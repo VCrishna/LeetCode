94ce51e5ddb16d29cc104955254928dda7f8ea3d
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-07-18 17:25:46 -0400

    Time: 67 ms (65.89%) | Memory: 120.3 MB (59.21%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,14 +1,27 @@
 class LRUCache {
-    private int capacity;
-    private Map<Integer, Node> cache;
-    private Node head;
-    private Node tail;
+
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    int capacity;
+    Map<Integer, Node> cache;
+    Node head;
+    Node tail;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
         cache = new HashMap<>();
-        head = new Node(-1, -1); // Dummy head node
-        tail = new Node(-1, -1); // Dummy tail node
+        head = new Node(0, 0); // Dummy head node
+        tail = new Node(0, 0); // Dummy tail node
         head.next = tail;
         tail.prev = head;
     }
@@ -16,7 +29,9 @@ class LRUCache {
     public int get(int key) {
         if (cache.containsKey(key)) {
             Node node = cache.get(key);
-            moveToHead(node); // Move the accessed node to the head of the list to update the access order
+            // Move the accessed node to the head of the list to update the access order
+            removeNode(node); // Remove the node from its current position in the list
+            addToHead(node); // Add the node to the head of the list
             return node.value;
         }
         return -1; // Key not found
@@ -26,7 +41,9 @@ class LRUCache {
         if (cache.containsKey(key)) {
             Node node = cache.get(key);
             node.value = value; // Update the value of the existing node
-            moveToHead(node); // Move the updated node to the head of the list to update the access order
+            // Move the updated node to the head of the list to update the access order
+            removeNode(node); // Remove the node from its current position in the list
+            addToHead(node); // Add the node to the head of the list
         } else {
             if (cache.size() == capacity) {
                 Node toRemove = tail.prev; // Get the node to be removed (least recently used)
@@ -39,113 +56,18 @@ class LRUCache {
         }
     }
 
-    private void moveToHead(Node node) {
-        removeNode(node); // Remove the node from its current position in the list
-        addToHead(node); // Add the node to the head of the list
-    }
-
-    private void removeNode(Node node) {
+    void removeNode(Node node) {
         node.prev.next = node.next; // Update the next pointer of the previous node
         node.next.prev = node.prev; // Update the previous pointer of the next node
     }
 
-    private void addToHead(Node node) {
+    void addToHead(Node node) {
         node.next = head.next; // Make the next pointer of the new node point to the current first node
         node.prev = head; // Make the previous pointer of the new node point to the head
         head.next.prev = node; // Make the previous pointer of the current first node point to the new node
         head.next = node; // Make the next pointer of the head point to the new node
     }
-
-    private class Node {
-        int key;
-        int value;
-        Node prev;
-        Node next;
-
-        Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
 }
-// class Node {
-//     Node prev;
-//     Node next;
-//     int key;
-//     int value;
-//     Node(int key, int value) {
-//         this.key = key;
-//         this.value = value;
-//     }
-// }
-// class LRUCache {
-//     int capacity;
-//     HashMap<Integer, Node> map;
-//     Node head;
-//     Node tail;
-//     public LRUCache(int capacity) {
-//         this.capacity = capacity;
-//         map = new HashMap<>(capacity);
-//         head = new Node(0, 0);
-//         tail = new Node(0, 0);
-//         head.next = tail;
-//         tail.prev = head;
-//     }
-//     public int get(int key) {
-//         if (!map.containsKey(key)) {
-//             return -1;
-//         }
-//         // move recently used node to the top of the linkedlist
-//         // simpley remove the node and add the node back at the head
-//         Node node = map.get(key);
-//         // Check if the node is already at the front of the linked list.
-//         if (node == head.next) {
-//             return node.value;
-//         }
-//         // Move the node to the front of the linked list.
-//         // inseting/adding back to the linkedlist at head
-//         removeNode(node);
-//         addNode(node);
-//         return node.value;
-//     }
-//     public void put(int key, int value) {
-//         // if key already present in the map
-//         // then we need to update value and move it to the top
-//         if (map.containsKey(key)) {
-//             Node node = map.get(key);
-//             node.value = value;
-//         }
-//         // If the cache is full, evict the least recently used node.
-//         if (capacity == map.size()) {
-//             // remove the least recently used node i.e., tail.prev
-//             removeNode(tail.prev);
-//         }
-//         // Add the new node to the front of the linked list.
-//         Node node = new Node(key, value);
-//         addNode(node);
-//     }
-//     public void addNode(Node newNode) {
-//         map.put(newNode.key,newNode);
-//         newNode.next = head.next;
-//         head.next.prev = newNode;
-//         head.next = newNode;
-//         newNode.prev = head;
-//     }
-//     public void removeNode(Node node) {
-//     map.remove(node.key);
-//     Node nodeNext = node.next;
-//     Node nodePrev = node.prev;
-//     nodePrev.next = nodeNext;
-//     nodeNext.prev = nodePrev;
-// }
-//     // public void removeNode(Node node) {
-//     //     map.remove(node.key);
-//     //     Node nodeNext = node.next;
-//     //     Node nodePrev = node.prev;
-//     //     nodePrev.next = nodeNext;
-//     //     nodeNext.prev = nodePrev;
-//     // }
-// }
 // /**
 //  * Your LRUCache object will be instantiated and called as such:
 //  * LRUCache obj = new LRUCache(capacity);
