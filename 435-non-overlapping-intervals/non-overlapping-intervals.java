9f46ddc80e7eac30b5d5164b3e00b68c64d467de
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-07-18 23:39:22 -0400

    Time: 85 ms (21.24%) | Memory: 100.6 MB (44.41%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/non-overlapping-intervals.java
@@ -1,15 +1,14 @@
 class Solution {
 
-    public int eraseOverlapIntervals(int[][] intervals) {
+    // My Method
+    public int eraseOverlapIntervalsK(int[][] intervals) {
         if (intervals.length <= 1) return 0;
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
-        System.out.println(Arrays.deepToString(intervals));
+        // System.out.println(Arrays.deepToString(intervals));
         int result = 0;
         Stack<int[]> stack = new Stack<>();
         for (int i = 0; i < intervals.length; i++) {
-            if (stack.isEmpty()) 
-                stack.push(intervals[i]); 
-            else {
+            if (stack.isEmpty()) stack.push(intervals[i]); else {
                 if (stack.peek()[1] > intervals[i][0]) {
                     result++;
                     continue;
@@ -20,4 +19,25 @@ class Solution {
         }
         return intervals.length - stack.size();
     }
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length <= 1) return 0;
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+        int end = intervals[0][1];
+        int result = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                result++;
+                // Update end time to the minimum end time between the current and previous interval
+                end = Math.min(end, intervals[i][1]);
+            } else end = intervals[i][1];
+        }
+
+        return result;
+    }
+    // LeetCode Soln
+    // public int eraseOverlapIntervals(int[][] intervals) {
+
+    // }
 }
