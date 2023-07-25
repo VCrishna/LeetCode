cf273fd9bdcc306b0d3782a16a95152a80cbcfda
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-07-25 17:05:18 -0400

    Time: 0 ms (100.00%) | Memory: 40.9 MB (85.79%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,5 +1,27 @@
 class Solution {
+    // 1-pass is essentially the 3-way quick partition method in quicksort. 
+    // check this gret video demo from Professor Robert Sedgewick
+    // https://www.youtube.com/watch?v=WBFzOYJ5ybM&t=177s
+    //-------------  SOLUTION 2: 1 SCAN ----------------//
+    // 3-way partition used in quick sort
     public void sortColors(int[] nums) {
+        int lt = 0, i = 0, gt = nums.length - 1;
+        while (i <= gt) {
+            if (nums[i] == 0) {
+                swap(nums, lt++, i++);
+            } else if (nums[i] == 2) {
+                swap(nums, i, gt--);
+            } else { // nums[i] == 1
+                i++;
+            }
+        }
+    }
+    private void swap(int[] nums, int p1, int p2) {
+        int temp = nums[p1];
+        nums[p1] = nums[p2];
+        nums[p2] = temp;
+    }
+    public void sortColors1(int[] nums) {
         for(int i=0;i+1<nums.length;i++){
             if(nums[i] > nums[i+1]){
                 int temp = nums[i];
@@ -7,9 +29,6 @@ class Solution {
                 nums[i+1] = temp;
                 i=-1;
             }
-            // else{
-            //     continue;
-            // }
         }
         
     }
