e81ecf23e6e164f9ef77d33833fcde6485df0150
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-07-25 17:00:45 -0400

    Time: 2 ms (7.42%) | Memory: 41 MB (73.61%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i=0;i+1<nums.length;i++){
+            if(nums[i] > nums[i+1]){
+                int temp = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = temp;
+                i=-1;
+            }
+            // else{
+            //     continue;
+            // }
+        }
+        
+    }
+}
