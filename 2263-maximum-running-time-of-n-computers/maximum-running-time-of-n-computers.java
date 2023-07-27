00a1428b99e501c65fcb7fad58d8512fd4e8cae0
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-07-27 16:06:39 -0400

    Time: 12 ms (100.00%) | Memory: 56.7 MB (70.54%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.java
@@ -1,30 +1,22 @@
 class Solution {
     public long maxRunTime(int n, int[] batteries) {
-        // Get the sum of all extra batteries.
-        Arrays.sort(batteries);
-        long extra = 0;
-        for (int i = 0; i < batteries.length - n; i++) {
-            extra += batteries[i];
+        long sumPower = 0;
+        for (int power : batteries)
+            sumPower += power;
+        long left = 1, right = sumPower / n;
+        
+        while (left < right){
+            long target = right - (right - left) / 2;
+            long extra = 0;
+            
+            for (int power : batteries)
+                extra += Math.min(power, target);
+
+            if (extra >= (long)(n * target))
+                left = target;
+            else
+                right = target - 1;
         }
-
-        // live stands for the n largest batteries we chose for n computers.
-
-        int[] live = Arrays.copyOfRange(batteries, batteries.length - n, batteries.length);
-
-        // We increase the total running time using 'extra' by increasing 
-        // the running time of the computer with the smallest battery.
-        for (int i = 0; i < n - 1; i++) {
-            // If the target running time is between live[i] and live[i + 1].
-            if (extra < (long)(i + 1) * (live[i + 1] - live[i])) {
-                return live[i] + extra / (long)(i + 1);
-            }
-
-            // Reduce 'extra' by the total power used.
-            extra -= (long)(i + 1) * (live[i + 1] - live[i]);
-        }
-
-        // If there is power left, we can increase the running time 
-        // of all computers.
-        return live[n - 1] + extra / n;
+        return left;
     }
 }
