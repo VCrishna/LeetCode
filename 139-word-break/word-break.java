88e98e2e88d1dcb1debc07b53ee89125701d7064
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-08-04 12:18:10 -0400

    Time: 1 ms (99.85%) | Memory: 40.9 MB (93.83%) - LeetSync

---
+++ 139-word-break/word-break.java
@@ -0,0 +1,27 @@
+class Solution {
+    private int[] memo;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        memo = new int[s.length()];
+        Arrays.fill(memo, -1);
+        return dp(s, wordDict, s.length()-1);
+
+    }
+    public boolean dp(String s, List<String> wordDict, int i) {
+        if(i < 0)
+            return true;
+        if(memo[i] != -1)
+            return memo[i] == 1;
+        for(String word : wordDict) {
+            if (i - word.length() + 1 < 0) {
+                continue;
+            }
+            if (s.substring(i - word.length() + 1, i + 1).equals(word) && 
+            dp(s, wordDict, i - word.length())) {
+                memo[i] = 1;
+                return true;
+            }
+        }
+        memo[i] = 0;
+        return false;
+    }
+}
