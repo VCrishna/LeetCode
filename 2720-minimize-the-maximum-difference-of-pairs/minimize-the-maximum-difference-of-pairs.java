e9a40ed375a605c3227caef8fc0c6139f107d539
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-08-09 20:15:19 -0400

    Time: 17 ms (93.02%) | Memory: 59.7 MB (10.85%) - LeetSync

--- 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.java
+++ 2720-minimize-the-maximum-difference-of-pairs/minimize-the-maximum-difference-of-pairs.java
@@ -1,35 +1,38 @@
 class Solution {
-    public int minimizeMax(int[] nums, int p) {
-        Arrays.sort(nums);
 
+    public int minimizeMax(int[] nums, int p) {
+        if (p==0) return 0;
+        Arrays.sort(nums);
         int left = 0;
         int right = nums[nums.length - 1] - nums[0];
-
         int result = right;
 
-        while(left <= right) {
-            // int middle_difference = left + (right - left) / 2;
-            int middle_difference = (left + right) / 2;
-            if(helper(nums, p, middle_difference)) {
-                result = middle_difference;
-                right = middle_difference - 1;
-            }
-            else {
-                left = middle_difference + 1;
+        while (left <= right) {
+            int threshold = left + (right - left) / 2;
+            if (isValid(nums, p, threshold)) {
+                result = threshold;
+                right = threshold - 1;
+            } else {
+                left = threshold + 1;
             }
         }
-
         return result;
-
     }
-    public boolean helper(int[] nums, int p, int diff) {
-        
-        for(int index = 1;index < nums.length; index++) {
-            if(nums[index] - nums[index-1] <= diff) {
-                p--;
+
+    public boolean isValid(int[] nums, int p, int threshold) {
+        int count = 0;
+        int index = 1;
+        while (index < nums.length) {
+            if(nums[index] - nums[index - 1] <= threshold) {
+                count++;
+                index += 2;
+            }
+            else{
                 index++;
             }
+            if (count == p) 
+                return true;
         }
-        return p<=0;
+        return false;
     }
 }
