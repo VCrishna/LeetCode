7e379cbac7fc4319ce9df79bce9c04afef6e83e5
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-08-10 16:36:20 -0400

    Time: 0 ms (100.00%) | Memory: 41 MB (42.36%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,39 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
+        // modified binary search to find the pivot
         int left = 0;
-        int right = nums.length-1;
-        // Finding the pivot
-        while(left < right){
-            int middle = left+(right-left)/2;
-            if(nums[middle] > nums[right]){
-                left=middle+1;
-            }else{
-                right = middle;
-            }
-        }
-        int start = left;
-        left = 0;
-        right = nums.length-1;
-        // Checking whether target is within the boundary
-        if(target >= nums[start] && target <= nums[right]){
-            left=start;
-        }else{
-            right=start;
-        }
-
-        // Regular Binary Search
-        while(left <= right){
-            int middleIndex = left + (right - left)/2;
-            int middleValue = nums[middleIndex];
-            if(middleValue == target)
-                return middleIndex;
-            else if(nums[middleIndex] < target){
-                left = middleIndex+1;
+        int right = nums.length - 1;
+        while(left < right) {
+            int midPoint = left + (right - left) / 2;
+            if (nums[midPoint] > nums[right]) {
+                left = midPoint + 1;
             }
             else{
-                right = middleIndex-1;
+                right = midPoint;
+            }
+        }
+        // regular binary search
+        int start = left;
+        left = 0;
+        right = nums.length - 1;
+        // Checking whether the target is within the boundaries
+        if(target >= nums[start] && target <= nums[right]) {
+            left = start;
+        } 
+        else {
+            right = start;
+        }
+        while(left <= right) {
+            int middle = left + (right - left) / 2;
+            if (nums[middle] == target)
+                return middle;
+            else if (nums[middle] < target) {
+                left = middle + 1;
+            }
+            else {
+                right = middle-1;
             }
         }
         return -1;
     }
 }
+        //  l   r
+// [4,5,6,7,0,1,2]
