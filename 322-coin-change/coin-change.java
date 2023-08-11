1998c6cc46273e86cbd8cc5d5287c41187fdb884
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-08-11 19:48:55 -0400

    Time: 14 ms (90.67%) | Memory: 43.7 MB (33.94%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,6 +1,5 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Arrays.sort(coins);
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
@@ -9,9 +8,6 @@ class Solution {
             for(int c:coins){
                 if(a-c >= 0) {
                     dp[a] = Math.min(dp[a], 1+dp[a-c]);
-                }
-                else {
-                    break;
                 }
             }
         }
