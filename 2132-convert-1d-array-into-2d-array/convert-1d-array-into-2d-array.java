219ca6ddee58569b2e8a9cb668f9e9003b39cbe6
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-08-15 09:18:29 -0400

    Time: 3 ms (99.15%) | Memory: 55.5 MB (9.63%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -1,14 +1,20 @@
 class Solution {
     public int[][] construct2DArray(int[] original, int m, int n) {
-        if(original.length != m*n)
+        // Base case: If the length of given original array is not 
+        // equal to the length of 2D array or m * n then we return empty array
+        if(original.length != m * n)
             return new int[0][0];
+        // index is used to keep track of position in original array
         int index = 0;
         int[][] result = new int[m][n];
         for(int i = 0; i < m; i++) {
+            // for each i (i.e., new row)
             int[] row = new int[n];
+            // adding n elements into the row
             for(int j = 0; j < n; j++) {
                 row[j] = original[index++];
             }
+            // adding newly created row into our 2D matrix
             result[i] = row;
         }
         return result;
