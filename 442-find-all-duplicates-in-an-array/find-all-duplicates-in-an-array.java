157ec1aed713bfe7d6458a86e95e3cb4f4b54494
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-08-16 15:35:28 -0400

    Time: 5 ms (95.32%) | Memory: 53.2 MB (76.33%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,14 +1,19 @@
+/**
+When we see k = | A[i] | for the first time, multiply A[k] by -1. 
+The negative sign on index k indicates k has been seen once. 
+As you progress through the array, you will find some j > i such that A[j] = k. 
+Now when you look at A[k], it is already negative (seen once). Thus j is a duplicate.
+ */
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         List<Integer> result = new ArrayList<>();
-        Set<Integer> set = new HashSet<>();
         for(int i = 0; i < nums.length; i++) {
-            if(!set.contains(nums[i])) {
-                set.add(nums[i]);
+            int index = Math.abs(nums[i])  - 1;
+            if(nums[index] < 0) {
+                result.add(index + 1);
             }
-            else{
-                result.add(nums[i]);
-            }
+            else
+                nums[index] *= -1;
         }
         return result;
     }
