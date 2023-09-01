1f64c4f965e46479db9274b3a7f099d13b49e6f5
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-08-31 23:40:14 -0400

    Time: 1 ms (78.48%) | Memory: 47.6 MB (38.02%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -83,14 +83,36 @@ class Solution {
 
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        for(int[] row : matrix) {
-            for(int col : row) {
-                maxHeap.add(col);
-                if(maxHeap.size() > k)
-                    maxHeap.poll();
+        // PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        // for(int[] row : matrix) {
+        //     for(int col : row) {
+        //         maxHeap.add(col);
+        //         if(maxHeap.size() > k)
+        //             maxHeap.poll();
+        //     }
+        // }
+        // return maxHeap.poll();
+        int n = matrix.length;
+        int left = matrix[0][0], right = matrix[n-1][n-1];
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0, j = n - 1;
+            
+            for (int i = 0; i < n; i++) {
+                while (j >= 0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                count += j + 1;
+            }
+            
+            if (count < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        return maxHeap.poll();
+        
+        return left;
     }
 }
