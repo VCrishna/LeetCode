2585c837786e8c401095e7f2f3cd540dd30a66e1
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-08-31 23:30:48 -0400

    Time: 16 ms (55.12%) | Memory: 46.6 MB (65.24%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,3 +1,86 @@
+/**
+ Approach 1 (Using Sorting) //it is the first approach that usually comes into mind
+ Time complexity : N * log(N);  //N == n^2
+ Auxiliary space complexity: O(N)
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int [] arr = new int[n*n];
+        int idx = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                arr[idx++] = matrix[i][j];
+            }
+        }
+        
+        Arrays.sort(arr);
+        
+        return arr[k - 1];
+    }
+}
+-------------------------------------------------------------------------------------------
+Approach 2 (Using Priority Queue) // By reading that we have to return kth of something,
+                                     this approach usually comes into mind
+Time Complexity: N * log(K) //N== n^2
+Auxiliary Space Complexity: O(K)
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+       PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int n = matrix.length;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                if(pq.size() < k){
+                    pq.add(matrix[i][j]);
+                }else{ //equal to k
+                    if(matrix[i][j] < pq.peek()){ //if incoming element is less than peek
+                        pq.poll();
+                        pq.add(matrix[i][j]);
+                    }
+                }
+            }
+        }
+        return pq.peek();
+    }
+}
+-------------------------------------------------------------------------------------------
+Approach 3: Binary Search
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+     int n = matrix.length;
+      int low = matrix[0][0];
+      int high = matrix[n - 1][n - 1];
+      
+      while(low < high) {
+        int mid = low + (high - low) / 2;
+        int count = lessEqual(matrix, mid);
+        
+        if(count < k) low = mid + 1;
+        else
+          high = mid;
+      }
+      return low;
+    }
+  public int lessEqual(int[][] matrix, int target) {
+    int count = 0, len = matrix.length;
+    int i = len - 1, j = 0;
+    
+    while(i >= 0 && j < len) {
+      if(matrix[i][j] > target) i --;
+      
+      else{
+        count = count + i + 1;
+        j ++;
+      }
+      
+    }
+      return count;
+  }
+}
+
+*/
+
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
