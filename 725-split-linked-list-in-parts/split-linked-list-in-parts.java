b4fbee3efaf34a727937066ab58cc9254af86f36
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-09-08 18:47:33 -0400

    Time: 2 ms (8.89%) | Memory: 43.7 MB (56.65%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.java
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.java
@@ -10,7 +10,9 @@
  */
 class Solution {
     public ListNode[] splitListToParts(ListNode head, int k) {
+        // result variable
         ListNode[] result = new ListNode[k];
+        // lst stores all the numbers of linked list
         List<Integer> lst = new ArrayList<>();
         ListNode temp = head;
         while (temp != null) {
@@ -18,17 +20,24 @@ class Solution {
             temp = temp.next;
         }
         // lst.forEach(System.out::println);
+        // Size of each partition
         int partitionSize = lst.size() / k;
+        // remainder is used to add extra elements into the sub lists
         int remainder = lst.size() % k;
+        // variable used to keep track of starting index for each sub list
         int startIndex = 0;
         List<List<Integer>> bigLst = new ArrayList<>();
+        // as we need to divide given linkedlist into k sublists so we iterate until k
         for (int i = 0; i < k; i++) {
+            // calculating end index
             int endIndex = startIndex + partitionSize + (i < remainder ? 1 : 0);
             // System.out.println(endIndex);
+            // adding sublist from start to end and updating start index
             bigLst.add(lst.subList(startIndex, endIndex));
             startIndex = endIndex;
         }
         // bigLst.forEach(System.out::println);
+        // creating linkedlist from each sublist and adding into result
         for (int i = 0; i < k; i++) {
             ListNode currentListNode = new ListNode(0);
             ListNode dummy = currentListNode;
