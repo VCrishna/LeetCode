cb99c3091360abfa0caad998e0a453e688eeafd4
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-09-10 08:51:04 -0400

    Time: 0 ms (100.00%) | Memory: 39.2 MB (71.95%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.java
@@ -7,20 +7,23 @@ class Solution {
     // d1 , p1, p2 , d2 --> invalid way --> d1 occured before p1 so it is invalid
     // only half of them occurs before d so we divide n * (n - 1) by 2 to get valid choices
     public int countOrders(int n) {
-        long mod = (long) (1e9 + 7);
+        // it is mentioned in the question that we need to divide result with mod 10 ^ 9 + 7 in each iteration
+        long modulo = (long) (1e9 + 7);
+        // result variable
         long result = 1;
+        // total number of slots available --> 2 * n (pickup, delivery)
         int slots = 2 * n;
+        // we need to run this loop until the slots are empty
         while(slots > 0) {
+            // calculating the valid no. of choices
+            // as mentioned above valid and invalid ways (p should occur before d)
             long valid_choices = slots * (slots - 1) / 2;
-            result = (result * valid_choices) % mod;
+            // calculating result by multiplying with valid choices and mod with modulo
+            result = (result * valid_choices) % modulo;
+            // for each iteration we decrease slots by 2 as (p,d)
             slots -= 2;
         }
-        // for (int i = 1; i <= n; i++) {
-        //     long valid_choices = slots * (slots - 1) / 2;
-        //     result = (result * valid_choices) % mod;
-        //     slots -= 2;
-        // }
-
+        // typecasting result to int
         return (int) result;
     }
 }
