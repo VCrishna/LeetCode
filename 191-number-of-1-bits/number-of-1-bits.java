88e71b5d33e82bfaef7aece384f93c20a49ef799
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-09-15 20:07:19 -0400

    Time: 0 ms (100.00%) | Memory: 39.8 MB (16.46%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,6 +1,14 @@
 public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
-        return Integer.bitCount(n);
+        // return Integer.bitCount(n);
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            int mask = 1 << i;
+            if ((n & mask) != 0) {
+                count++;
+            }
+        }
+        return count;
     }
 }
