143ec59f529994b8a52d916589df55d399428db0
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-09-17 15:01:41 -0400

    Time: 6 ms (51.28%) | Memory: 40.5 MB (51.10%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,23 +1,32 @@
 class Solution {
     public String countAndSay(int n) {
-        String current = "1";
-        for (int i = 0; i < n-1; i++) {
-            StringBuilder sb = new StringBuilder();
-            char ch = current.charAt(0);
+        String result = "1";
+        for (int i = 0; i < n - 1; i++) {
+            // first character of newly formed result string
+            char ch = result.charAt(0);
+            // as 1 character is used or first character is used so count is 1
             int count = 1;
-            for (int j = 1; j < current.length(); j++) {
-                if (ch != current.charAt(j)) {
+            StringBuilder sb = new StringBuilder();
+            // from start to the end of result string
+            for (int j = 1; j < result.length(); j++) {
+                // if the current string is not equal to the character at current index 
+                // then we append the count and character to our sb variable and
+                // assign it with new character and count to 0 and continue until the end of string
+                if (ch != result.charAt(j)) {
                     sb.append(count);
                     sb.append(ch);
+                    ch = result.charAt(j);
                     count = 0;
-                    ch = current.charAt(j);
                 }
+                // counting the characters
                 count++;
             }
+            // appending last character and its count
             sb.append(count);
             sb.append(ch);
-            current = sb.toString();
+            // updating the result with newly created string
+            result = sb.toString();
         }
-        return current;
+        return result;
     }
 }
