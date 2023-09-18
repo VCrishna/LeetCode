21f2df0fe3c2ca695ec38c7c68495f0b13cc724e
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-09-17 22:56:43 -0400

    Time: 1 ms (99.31%) | Memory: 44 MB (99.24%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -1,53 +1,42 @@
-class Pair{
-    // variable used to store index of row
-    int index;
-    // variable used to store count of 1's in the row
+class Pair {
     int count;
-    Pair(int a,int b){
-        this.index = a;
-        this.count = b;
+    int rowIndex;
+
+    Pair(int ri, int c) {
+        this.count = c;
+        this.rowIndex = ri;
     }
 }
+
 class Solution {
+
     public int[] kWeakestRows(int[][] mat, int k) {
-        // defining PriorityQueue
-        // PriorityQueue<Pair> pq = new PriorityQueue<>((p1, p2) -> {
-        //     if (p1.count != p2.count) {
-        //         return p1.count - p2.count; // Sort by count in ascending order
-        //     } else {
-        //         return p1.index - p2.index; // Maintain original order for rows with the same count
-        //     }
-        // });
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (p1, p2) -> p1.count != p2.count ? p1.count - p2.count : p1.index - p2.index
-            );
+        PriorityQueue<Pair> minHeap = new PriorityQueue<Pair>((p1, p2) -> p1.count != p2.count ? p1.count - p2.count : p1.rowIndex - p2.rowIndex);
 
-        // filling PriorityQueue with index of rows and number of ones present in each row
         for (int i = 0; i < mat.length; i++) {
-            pq.offer(new Pair(i,countOnes(mat[i])));
+            minHeap.add(new Pair(i, countOnes(mat[i])));
         }
-        
+        // while (!minHeap.isEmpty()) {
+        //     System.out.println(minHeap.poll().rowIndex);
+        // }
         int[] result = new int[k];
-
-        for(int i = 0;i<k;i++)
-            result[i]=pq.poll().index;
-    
+        for (int i = 0; i < k; i++) {
+            result[i] = minHeap.poll().rowIndex;
+        }
         return result;
     }
-    // binary search to find total number of ones present in each row
-    private int countOnes(int[] row) {
-        int lo = 0;
-        int hi = row.length;
-        
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
-            
-            if (row[mid] == 1)
-                lo = mid + 1;
-            else
-                hi = mid;
+
+    public int countOnes(int[] row) {
+        int left = 0;
+        int right = row.length;
+        while (left < right) {
+            int middleIndex = left + (right - left) / 2;
+            if (row[middleIndex] == 1) {
+                left = middleIndex + 1;
+            } else {
+                right = middleIndex;
+            }
         }
-        
-        return lo;
+        return left;
     }
 }
