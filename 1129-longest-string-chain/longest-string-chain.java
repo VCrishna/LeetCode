c62403eba162638cc9d52cb82f529a298fae39d2
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-09-23 18:12:42 -0400

    Time: 36 ms (74.43%) | Memory: 43.9 MB (20.93%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,22 +1,20 @@
 class Solution {
     public int longestStrChain(String[] words) {
         if (words.length == 1) return words.length;
+        int result = 0;
         Map<String, Integer> memo = new HashMap<>();
-        int count = 0;
-        Arrays.sort(words, (a, b) -> a.length() - b.length());
-        // Arrays.sort(words, Comparator.comparingInt(String::length));
-        memo.putIfAbsent(words[0], words[0].length());
+        Arrays.sort(words,(a,b) -> a.length() - b.length());
         for (String word : words) {
-            memo.put(word, 1);
+            int longest = 0;
             for (int i = 0; i < word.length(); i++) {
                 StringBuilder current = new StringBuilder(word);
-                String next = current.deleteCharAt(i).toString();
-                if (memo.containsKey(next)) {
-                    memo.put(word, Math.max(memo.get(word), memo.get(next) + 1));
-                }
+                String prev = current.deleteCharAt(i).toString();
+                longest = Math.max(longest, memo.getOrDefault(prev, 0) + 1);
             }
-            count = Math.max(count, memo.get(word));
+            memo.put(word, longest);
+            result = Math.max(result, longest);
         }
-        return count;
+
+        return result;
     }
 }
