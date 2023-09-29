74a7d9ad9f0bb2c482e6f648d4f3272001b01db6
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-09-28 22:26:29 -0400

    Time: 3 ms (43.67%) | Memory: 55.2 MB (79.93%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -3,14 +3,18 @@ class Solution {
         if (nums.length == 1) return true;
         boolean increasing = true;
         boolean decreasing = true;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i - 1] > nums[i]) increasing = false;
-
-            if (nums[i - 1] < nums[i]) decreasing = false;
+        for (int i = 0; i < nums.length - 1; i++) {
+            // checking if elements side by side or in increasing or decreasing in order
+            // if they are decreasing, then increasing is false
+            if (nums[i] > nums[i+1]) 
+                increasing = false;
+            // if they are increasing, then decreasing is false
+            if (nums[i] < nums[i+1]) 
+                decreasing = false;
         }
-        if (!increasing && !decreasing) {
-            return false;
-        }
-        return true;
+        // if (!increasing && !decreasing) {
+        //     return false;
+        // }
+        return increasing || decreasing;
     }
 }
