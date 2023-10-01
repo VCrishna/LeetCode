42730c6dac0c9dc58f5689953324b5888407b31d
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-09-30 20:32:13 -0400

    Time: 4 ms (87.56%) | Memory: 44.1 MB (64.33%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -2,10 +2,13 @@ class Solution {
     public String reverseWords(String s) {
         StringBuilder result = new StringBuilder();
 
-        for(String word : s.split(" ")) {
+        for (String word : s.split(" ")) {
             result.append(new StringBuilder(word).reverse().toString());
             result.append(" ");
         }
+        
+        // As we append space for every word in the string, we have also appended space after last word
+        // so last space is removed by calling stripTrailing method of string class
         return result.toString().stripTrailing();
     }
 }
