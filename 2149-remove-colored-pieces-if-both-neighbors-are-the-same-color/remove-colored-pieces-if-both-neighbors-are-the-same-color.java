0d745be7c3aa0708a2bcbb9fea7ce7748280a7e2
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-02 01:32:09 -0400

    Time: 7 ms (99.54%) | Memory: 44 MB (68.76%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.java
@@ -1,21 +1,33 @@
 class Solution {
     public boolean winnerOfGame(String colors) {
-        int aliceCount = 0, bobCount = 0;
-        int consecutiveA = 0, consecutiveB = 0;
-
+        // Count of sequences where Alice and Bob can remove characters
+        // Alice count -- incremented when alice removes character A
+        int aliceCount = 0, 
+            // Bob count -- incremented when bob removes character A
+            bobCount = 0,
+            // Current count of consecutive 'A's and 'B's
+            // Consecutive A's
+            consecutiveA_sCount = 0, 
+            // Consecutive B's
+            consecutiveB_sCount = 0;
+        // iterating over each character in the given colors string
         for(char ch : colors.toCharArray()) {
             if(ch == 'A') {
-                consecutiveA++;
-                consecutiveB = 0;
-                if(consecutiveA >= 3) aliceCount++;
+                // incrementing consecutive counts of A
+                consecutiveA_sCount++;
+                // resetting Consecutive B's count is A is occured
+                consecutiveB_sCount = 0;
+                // if A occurs 3 or more times consecutively in the colors string,
+                // then alice can remove, so we are incrementing alice's count
+                if(consecutiveA_sCount >= 3) aliceCount++;
             }
             else {
-                consecutiveB++;
-                consecutiveA = 0;
-                if(consecutiveB >= 3) bobCount++;
+                consecutiveB_sCount++;
+                consecutiveA_sCount = 0;// Reset consecutive A since we encountered a 'B'
+                if(consecutiveB_sCount >= 3) bobCount++;// If 3 or more 'B's in a row, Bob can remove one
             }
         }
 
-        return aliceCount > bobCount;
+        return aliceCount > bobCount; // If Alice can remove more 'A's than Bob can remove 'B's, she wins
     }
 }
