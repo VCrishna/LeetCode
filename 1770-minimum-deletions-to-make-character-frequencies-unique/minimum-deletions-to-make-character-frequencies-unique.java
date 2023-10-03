dac119869418a7e7bc949ebcfe2d70c7d10fa502
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-03 13:36:08 -0400

    Time: 48 ms (34.94%) | Memory: 44.2 MB (95.91%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,17 +1,21 @@
 class Solution {
     public int minDeletions(String s) {
+        // Map is used to store character and its frequency
         Map<Character, Integer> map = new HashMap<>();
+        // set is used to maintain the unique frequncies of each character
         Set<Integer> set = new HashSet<>();
-
+        // count number of deletions
         int deletions = 0;
-
         // calculating the frequencies of each character
         for (char ch : s.toCharArray()) {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-
+        // iterating over the values in the map
         for(int i : map.values()) {
-            
+            // if current frequncy of a character is > 0 && it is present in the set
+            // then we are decrementing it and increasing the count of deletions until the 
+            // frequency is not present in the set
+            // once we encounter a new frequency, we break this while loop and add it to the set
             while(i > 0 && set.contains(i)){
                 i--;
                 deletions++;
