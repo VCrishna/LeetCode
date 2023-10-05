b39051759aef003f1508e167b095c56da9b6a4d2
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-05 14:11:50 -0400

    Time: 1 ms (99.86%) | Memory: 48.5 MB (33.84%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,5 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int result = 0, count = 0;
+        for(int i : nums) {
+            if(count == 0)
+                result = i;
+            count += (i == result ? 1 : -1);
+        }
+        return result;
+    }
+    public int majorityElement_brute(int[] nums) {
         Map<Integer, Integer> myMap = new HashMap<Integer, Integer>();
         for (int num : nums) {
             myMap.put(num, myMap.getOrDefault(num, 0) + 1);
