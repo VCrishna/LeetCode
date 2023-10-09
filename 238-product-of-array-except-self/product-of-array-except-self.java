e07f7c98513cf1fac285803b330b9460abf85df7
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-09 13:01:10 -0400

    Time: 1 ms (100.00%) | Memory: 50.2 MB (99.55%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -8,12 +8,19 @@ class Solution {
         for (int i = 1; i < nums.length; i++) {
             left[i] = nums[i - 1] * left[i - 1];
         }
-        for (int i = nums.length - 2; i >= 0; i--) {
-            right[i] = right[i + 1] * nums[i + 1];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            result[i] = left[i] * right[i];
+        // for (int i = nums.length - 2; i >= 0; i--) {
+        //     right[i] = right[i + 1] * nums[i + 1];
+        // }
+        // for (int i = 0; i < nums.length; i++) {
+        //     result[i] = left[i] * right[i];
+        // }
+        // without using extra space
+        int mul = 1;
+        for(int i = nums.length - 1; i>= 0; i--) {
+            result[i] = left[i] * mul;
+            mul = mul * nums[i];
         }
         return result;
     }
 }
+
