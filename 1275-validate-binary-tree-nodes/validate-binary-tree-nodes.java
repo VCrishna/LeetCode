1a1b1d19c03dddde30006df10395e2c18c5a55f9
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-17 14:58:52 -0400

    Time: 12 ms (54.72%) | Memory: 45.5 MB (19.22%) - LeetSync

--- 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.java
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.java
@@ -1,43 +1,41 @@
-public class Solution {
+class Solution {
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
-        int[] parent = new int[n];
-        Arrays.fill(parent, -1);
-        
-        // Step 2: Identify children and set parent
-        for (int i = 0; i < n; i++) {
-            if (leftChild[i] != -1) {
-                if (parent[leftChild[i]] != -1) return false;  // node has two parents
-                parent[leftChild[i]] = i;
-            }
-            
-            if (rightChild[i] != -1) {
-                if (parent[rightChild[i]] != -1) return false;  // node has two parents
-                parent[rightChild[i]] = i;
-            }
+        Set<Integer> hasParent = new HashSet<>();
+
+        // adding left and right child's nodes into the set to remove duplicated
+        for(int i : leftChild) {
+            if(i != -1)
+                hasParent.add(i);
         }
-        
-        // Find root (node that isn't a child)
-        int rootCount = 0, root = -1;
-        for (int i = 0; i < n; i++) {
-            if (parent[i] == -1) {
-                rootCount++;
+        for(int i : rightChild) {
+            if(i != -1)
+                hasParent.add(i);
+        }
+
+        // finding parent node
+        int root = -1;
+        for(int i = 0; i < n; i++) {
+            if(!hasParent.contains(i)) {
                 root = i;
+                break;
             }
         }
-        if (rootCount != 1) return false;  // There should be exactly one root
-        
-        // Step 3: DFS to check for cycles and connectivity
+        // set used to keep track of visited nodes
         Set<Integer> visited = new HashSet<>();
-        dfs(root, visited, leftChild, rightChild);
-        
-        // Step 4: Check if it's a single tree
-        return visited.size() == n;
+        // calling dfs to find if these nodes are connected and they is no cycle exists in between these nodes
+        // also if all node are connected the visited hashset size will be equal to given n
+        return dfs(root, leftChild, rightChild, visited) && visited.size() == n;
     }
-    
-    private void dfs(int node, Set<Integer> visited, int[] leftChild, int[] rightChild) {
-        if (node == -1 || visited.contains(node)) return;
+
+    public boolean dfs(int node, int[] leftChild, int[] rightChild, Set<Integer> visited) {
+        if(node == -1) {
+            return true;
+        }
+        if(visited.contains(node)) {
+            return false;
+        }
         visited.add(node);
-        dfs(leftChild[node], visited, leftChild, rightChild);
-        dfs(rightChild[node], visited, leftChild, rightChild);
+        return dfs(leftChild[node], leftChild, rightChild, visited) &&
+               dfs(rightChild[node], leftChild, rightChild, visited);
     }
 }
