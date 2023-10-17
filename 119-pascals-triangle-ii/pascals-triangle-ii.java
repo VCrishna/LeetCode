a531260e1e24c32652bd0e23c6ad5ec2706117ad
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-16 21:04:10 -0400

    Time: 1 ms (79.47%) | Memory: 40.3 MB (44.44%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,11 +1,17 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> row = new ArrayList<>();
+        // adding 1 to the beginning of the row
         row.add(1);
+        // iterating from 1 to rowIndex
         for(int i = 1; i <= rowIndex; i++) {
+            // filling elements in between first and last element in the row
             for(int j = row.size() - 1; j > 0; j--) {
+                // getting last and before last element and placing at last index of the row
+                
                 row.set(j,(row.get(j)+row.get(j-1)));
             }
+            // adding 1 to the end of row
             row.add(1);
         }
         return row;
