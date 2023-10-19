8bb04de02e4da1d3a63963297a67cb03be09a378
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-18 21:52:15 -0400

    Time: 3 ms (27.60%) | Memory: 41.1 MB (11.39%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,35 +1,16 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
-        Stack<Character> s1 = new Stack<>();
-        Stack<Character> s2 = new Stack<>();
-        for(char c : s.toCharArray()) {
-            if(c == '#' && !s1.isEmpty()) {
-                s1.pop();
-            }
-            else if(c == '#')
-                continue;
-            else
-                s1.push(c);
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack();
+        for (char c: S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
         }
-        for(char c : t.toCharArray()) {
-            if(c == '#' && !s2.isEmpty()) {
-                s2.pop();
-            }
-            else if(c == '#')
-                continue;
-            else
-                s2.push(c);
-        }
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        for(char c : s1) {
-            sb1.append(c);
-        }
-        for(char c : s2) {
-            sb2.append(c);
-        }
-        System.out.println(sb1.toString());
-        System.out.println(sb2.toString());
-        return sb1.toString().equals(sb2.toString());
+        return String.valueOf(ans);
     }
 }
