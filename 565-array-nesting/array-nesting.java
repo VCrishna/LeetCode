75dd6e407907c0e29e406c85f66048fcca9f6963
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-20 19:25:05 -0400

    Time: 5 ms (63.76%) | Memory: 58.9 MB (32.75%) - LeetSync

--- 565-array-nesting/array-nesting.java
+++ 565-array-nesting/array-nesting.java
@@ -1,26 +1,19 @@
-class Solution {
-    boolean[] visited;
+public class Solution {
     public int arrayNesting(int[] nums) {
-        int length = Integer.MIN_VALUE;
-        visited = new boolean[nums.length];
-        Arrays.fill(visited, false);
+        boolean[] visited = new boolean[nums.length];
+        int res = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (visited[nums[i]]) continue; else {
-                Set<Integer> s = buildSet(nums, i, new HashSet<>());
-                length = Math.max(length, s.size());
+            if (!visited[i]) {
+                int start = nums[i], count = 0;
+                do {
+                    start = nums[start];
+                    count++;
+                    visited[start] = true;
+                }
+                while (start != nums[i]);
+                res = Math.max(res, count);
             }
         }
-        return length;
-    }
-
-    public Set<Integer> buildSet(int[] nums, int index, Set<Integer> set) {
-        if (set.contains(nums[index])) return set; else {
-            set.add(nums[index]);
-            visited[nums[index]] = true;
-            return buildSet(nums, nums[index], set);
-        }
+        return res;
     }
 }
-// [5,4,0,3,1,6,2]
-// n = 7
-// [0, 6]
