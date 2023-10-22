5fb0ba4df63c7872707732596dac8fac6e78483a
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-22 16:53:15 -0400

    Time: 8 ms (72.38%) | Memory: 59.2 MB (31.43%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.java
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.java
@@ -1,23 +1,48 @@
 class Solution {
-
     public int maximumScore(int[] nums, int k) {
+        // Initialize two pointers, left and right, both pointing to the index k.
         int left = k, right = k;
+
+        // Initialize the result with the value at index k.
+        // This is the initial score when considering only the single element at index k.
         int result = nums[k];
+
+        // Set the initial minimum to the value at index k.
+        // This keeps track of the minimum value in the current subarray.
         int current_min = nums[k];
+
+        // Continue the loop as long as there's room to expand the subarray to the left or right.
         while (left > 0 || right < nums.length - 1) {
-              if (left == 0) { // Only expand to the right if left is already at its boundary
+            // If the left pointer is already at the start of the array,
+            // we can only expand the subarray to the right.
+            if (left == 0) {
                 right++;
-            } else if (right == nums.length - 1) { // Only expand to the left if right is at its boundary
+            }
+            // If the right pointer is already at the end of the array,
+            // we can only expand the subarray to the left.
+            else if (right == nums.length - 1) {
                 left--;
-            } else if (nums[left - 1] > nums[right + 1]) {
+            }
+            // If the value to the left is greater than the value to the right,
+            // expand the subarray to the left to include the larger value.
+            else if (nums[left - 1] > nums[right + 1]) {
                 left--;
-            } else {
+            }
+            // Otherwise, expand the subarray to the right.
+            else {
                 right++;
             }
 
+            // Update the current minimum. It's either the previous current_min
+            // or the newly added number (either from the left or right).
             current_min = Math.min(current_min, Math.min(nums[left], nums[right]));
+
+            // Calculate the score for the current subarray and update the result if it's greater.
+            // The score is the current minimum value times the length of the subarray.
             result = Math.max(result, current_min * (right - left + 1));
         }
+
+        // Return the maximum score.
         return result;
     }
 }
