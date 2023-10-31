a6629ef254de01510470a967e5d2d45d6a40beed
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-31 12:46:38 -0400

    Time: 9 ms (53.46%) | Memory: 43.1 MB (46.92%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.java
+++ 1332-count-vowels-permutation/count-vowels-permutation.java
@@ -13,20 +13,34 @@ class Solution {
         oVowelPermutationCount[0] = 1L;
         uVowelPermutationCount[0] = 1L;
 
-        int MOD = 1000000007;
+        // int MOD = 1000000007;
+        int MOD = (int)1e9 + 7;
 
 
         for (int i = 1; i < n; i++) {
-            aVowelPermutationCount[i] = (eVowelPermutationCount[i - 1] + iVowelPermutationCount[i - 1] + uVowelPermutationCount[i - 1]) % MOD;
-            eVowelPermutationCount[i] = (aVowelPermutationCount[i - 1] + iVowelPermutationCount[i - 1]) % MOD;
-            iVowelPermutationCount[i] = (eVowelPermutationCount[i - 1] + oVowelPermutationCount[i - 1]) % MOD;
+            aVowelPermutationCount[i] = (eVowelPermutationCount[i - 1] + 
+                                         iVowelPermutationCount[i - 1] + 
+                                         uVowelPermutationCount[i - 1]) % MOD;
+
+            eVowelPermutationCount[i] = (aVowelPermutationCount[i - 1] + 
+                                         iVowelPermutationCount[i - 1]) % MOD;
+
+            iVowelPermutationCount[i] = (eVowelPermutationCount[i - 1] + 
+                                         oVowelPermutationCount[i - 1]) % MOD;
+
             oVowelPermutationCount[i] = iVowelPermutationCount[i - 1] % MOD;
-            uVowelPermutationCount[i] = (iVowelPermutationCount[i - 1] + oVowelPermutationCount[i - 1]) % MOD;
+
+            uVowelPermutationCount[i] = (iVowelPermutationCount[i - 1] + 
+                                         oVowelPermutationCount[i - 1]) % MOD;
         }
 
         long result = 0L;
 
-        result = (aVowelPermutationCount[n - 1] + eVowelPermutationCount[n - 1] + iVowelPermutationCount[n - 1] + oVowelPermutationCount[n - 1] + uVowelPermutationCount[n - 1]) % MOD;
+        result = (aVowelPermutationCount[n - 1] + 
+                  eVowelPermutationCount[n - 1] + 
+                  iVowelPermutationCount[n - 1] + 
+                  oVowelPermutationCount[n - 1] + 
+                  uVowelPermutationCount[n - 1]) % MOD;
 
 
         return (int)result;
