00a497a09009edf5127de208681dec08d0c9f7ce
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-10-31 13:33:19 -0400

    Time: 10 ms (51.19%) | Memory: 43.1 MB (46.92%) - LeetSync

--- 1332-count-vowels-permutation/count-vowels-permutation.java
+++ 1332-count-vowels-permutation/count-vowels-permutation.java
@@ -1,6 +1,30 @@
-class Solution {
-    public int countVowelPermutation(int n) {
+/**
+    'a' can only be followed by 'e'.
+    'e' can be followed by either 'a' or 'i'.
+    'i' can be followed by 'a', 'e', 'o', or 'u'.
+    'o' can be only be followed by 'i' or 'u'.
+    'u' can only be followed by 'a'.
+    
+    -> For 'a', a valid string of length i can only be formed by appending 'e' 
+       to a valid string of length i-1 that ends with 'e', 'i', or 'u'.
 
+    -> For 'e', a valid string of length i can be formed by appending 'a' or 'i' 
+       to a valid string of length i-1 that ends with 'a' or 'e'.
+
+    -> For 'i', a valid string of length i can be formed by appending 'a', 'e', 'o', or 'u' 
+       to a valid string of length i-1 that ends with 'a', 'e', 'i', 'o', or 'u'.
+
+    -> For 'o', a valid string of length i can only be formed by appending 'i' or 'u' 
+       to a valid string of length i-1 that ends with 'i' or 'o'.
+
+    -> For 'u', a valid string of length i can be formed by appending 'a' or 'i' 
+       to a valid string of length i-1 that ends with 'i' or 'u'.
+
+ */
+
+class Solution {
+
+    public int countVowelPermutation(int n) {
         long[] aVowelPermutationCount = new long[n];
         long[] eVowelPermutationCount = new long[n];
         long[] iVowelPermutationCount = new long[n];
@@ -13,36 +37,29 @@ class Solution {
         oVowelPermutationCount[0] = 1L;
         uVowelPermutationCount[0] = 1L;
 
-        // int MOD = 1000000007;
-        int MOD = (int)1e9 + 7;
-
+        int MOD = (int) 1e9 + 7;
 
         for (int i = 1; i < n; i++) {
-            aVowelPermutationCount[i] = (eVowelPermutationCount[i - 1] + 
-                                         iVowelPermutationCount[i - 1] + 
-                                         uVowelPermutationCount[i - 1]) % MOD;
-
-            eVowelPermutationCount[i] = (aVowelPermutationCount[i - 1] + 
-                                         iVowelPermutationCount[i - 1]) % MOD;
-
-            iVowelPermutationCount[i] = (eVowelPermutationCount[i - 1] + 
-                                         oVowelPermutationCount[i - 1]) % MOD;
-
-            oVowelPermutationCount[i] = iVowelPermutationCount[i - 1] % MOD;
-
-            uVowelPermutationCount[i] = (iVowelPermutationCount[i - 1] + 
-                                         oVowelPermutationCount[i - 1]) % MOD;
+            aVowelPermutationCount[i] = ( eVowelPermutationCount[i - 1] + 
+                                          iVowelPermutationCount[i - 1] + 
+                                          uVowelPermutationCount[i - 1]) % MOD;
+            eVowelPermutationCount[i] = ( aVowelPermutationCount[i - 1] + 
+                                          iVowelPermutationCount[i - 1]) % MOD;
+            iVowelPermutationCount[i] = ( eVowelPermutationCount[i - 1] + 
+                                          oVowelPermutationCount[i - 1]) % MOD;
+            oVowelPermutationCount[i] = ( iVowelPermutationCount[i - 1]) % MOD;
+            
+            uVowelPermutationCount[i] = ( oVowelPermutationCount[i - 1] + 
+                                          iVowelPermutationCount[i - 1]) % MOD;
         }
+        long result = 0l;
 
-        long result = 0L;
+        result = ( aVowelPermutationCount[n - 1] + 
+                   eVowelPermutationCount[n - 1] + 
+                   iVowelPermutationCount[n - 1] + 
+                   oVowelPermutationCount[n - 1] + 
+                   uVowelPermutationCount[n - 1]) % MOD;
 
-        result = (aVowelPermutationCount[n - 1] + 
-                  eVowelPermutationCount[n - 1] + 
-                  iVowelPermutationCount[n - 1] + 
-                  oVowelPermutationCount[n - 1] + 
-                  uVowelPermutationCount[n - 1]) % MOD;
-
-
-        return (int)result;
+        return (int) result;
     }
 }
