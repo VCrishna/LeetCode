74dd196c560c393cd5828a002740840a98646a1a
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-11-10 13:40:02 -0500

    Time: 112 ms (62.70%) | Memory: 113 MB (20.08%) - LeetSync

--- 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.java
+++ 1866-restore-the-array-from-adjacent-pairs/restore-the-array-from-adjacent-pairs.java
@@ -1,34 +1,53 @@
 class Solution {
     public int[] restoreArray(int[][] adjacentPairs) {
-        if(adjacentPairs.length == 1) 
-            return adjacentPairs[0];
+        // If there is only one pair, return it
+        if (adjacentPairs.length == 1) return adjacentPairs[0];
+
+        // Create a graph to represent the adjacent pairs
         Map<Integer, List<Integer>> graph = new HashMap<>();
-        for(int[] edge : adjacentPairs) {
+        for (int[] edge : adjacentPairs) {
             int x = edge[0];
             int y = edge[1];
-            if(!graph.containsKey(x)) {
+
+            // If x is not in the graph, add it with an empty list
+            if (!graph.containsKey(x)) {
                 graph.put(x, new ArrayList<>());
             }
-            if(!graph.containsKey(y)) {
+            // If y is not in the graph, add it with an empty list
+            if (!graph.containsKey(y)) {
                 graph.put(y, new ArrayList<>());
             }
+
+            // Add y to the neighbors of x, and add x to the neighbors of y
             graph.get(x).add(y);
             graph.get(y).add(x);
         }
+
+        // Find the root of the tree by identifying the node with only one neighbor
         int root = 0;
-        for(int num : graph.keySet()) {
-            if(graph.get(num).size() == 1) {
+        for (int num : graph.keySet()) {
+            if (graph.get(num).size() == 1) {
                 root = num;
                 break;
             }
         }
+
+        // Initialize the result array
         int[] result = new int[adjacentPairs.length + 1];
+
+        // Perform depth-first search to construct the result array
         dfs(graph, root, Integer.MAX_VALUE, result, 0);
+
+        // Return the result array
         return result;
     }
-    private void dfs(Map<Integer, List<Integer>> graph,int node, int prev, int[] ans, int i) {
+    // Depth-first search function to traverse the graph and construct the result array
+    private void dfs(Map<Integer, List<Integer>> graph, int node, int prev, int[] ans, int i) {
         ans[i] = node;
+
+        // Iterate through neighbors of the current node
         for (int neighbor : graph.get(node)) {
+            // If the neighbor is not the previous node, perform DFS on the neighbor
             if (neighbor != prev) {
                 dfs(graph, neighbor, node, ans, i + 1);
             }
