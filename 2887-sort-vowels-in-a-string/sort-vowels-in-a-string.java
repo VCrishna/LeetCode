4f08293c379f813b5bf1b12e9af91d9411b7646b
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-11-13 18:33:49 -0500

    Time: 68 ms (36.83%) | Memory: 46.2 MB (53.41%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,44 +1,21 @@
 class Solution {
-    // public String sortVowels(String s) {
-    //     List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
-
-    //     // Filter and collect vowels in a List
-    //     List<Character> vowelList = s.chars()
-    //             .mapToObj(c -> (char) c)
-    //             .filter(vowels::contains)
-    //             .sorted()
-    //             .collect(Collectors.toList());
-
-    //     // Build the result string
-    //     StringBuilder sb = new StringBuilder();
-    //     int vowelIndex = 0;
-    //     for (char c : s.toCharArray()) {
-    //         if (vowels.contains(c)) {
-    //             sb.append(vowelList.get(vowelIndex++));
-    //         } else {
-    //             sb.append(c);
-    //         }
-    //     }
-
-    //     return sb.toString();
-    // }
     public String sortVowels(String s) {
-        List<Integer> pq = new ArrayList<>();
-        List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
-        for (char c : s.toCharArray()) {
-            if (vowels.contains(c)) {
-                pq.add((int) c);
+        List<Character> vowels = Arrays.asList('a','A','e','E','i','I','o','O','u','U');
+        List<Integer> vowelsInS = new ArrayList<>();
+        for(char c : s.toCharArray()) {
+            if(vowels.contains(c)) {
+                vowelsInS.add((int)c);
             }
         }
-        Collections.sort(pq);
+        Collections.sort(vowelsInS);
         StringBuilder sb = new StringBuilder();
-        int index = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (vowels.contains(s.charAt(i))) {
-                int c = pq.get(index++);
+        int vowelIndex = 0;;
+        for(int i = 0; i < s.length(); i++) {
+            if(vowels.contains(s.charAt(i))) {
+                int c = vowelsInS.get(vowelIndex++);
                 sb.append((char)c);
-                
-            } else {
+            }
+            else {
                 sb.append(s.charAt(i));
             }
         }
