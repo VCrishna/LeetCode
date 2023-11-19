2d83599c0c4184d6968e51a3395721f599a503bb
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-11-18 20:53:20 -0500

    Time: 33 ms (92.11%) | Memory: 54.7 MB (48.03%) - LeetSync

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
@@ -1,6 +1,20 @@
 class Solution {
 
     public int reductionOperations(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        int distinctCount = 0;
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i-1])
+                distinctCount++;
+
+            result += distinctCount;
+        }
+
+        return result;
+    }
+    public int reductionOperations_BRUTE_FORCE(int[] nums) {
         int result = 0;
         Arrays.sort(nums);
         for (int i = nums.length - 2; i >= 0; i--) {
@@ -8,13 +22,8 @@ class Solution {
                 continue;
             }
             if (nums[i] < nums[i + 1]) {
-                // while (nums[i] < nums[i + 1]) {
-                //     nums[i + 1]--;
-                // }
-                // result++;
                 result += nums.length - 1 - i;
             }
-            // i = nums.length - 2;
         }
         return result;
     }
