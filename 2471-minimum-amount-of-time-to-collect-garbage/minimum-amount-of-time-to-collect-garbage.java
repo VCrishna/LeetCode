e02d80788ad2ffd25f4e8e9c7f25922a5ea55044
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-11-20 18:04:07 -0500

    Time: 2 ms (100.00%) | Memory: 60.8 MB (85.52%) - LeetSync

--- 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.java
@@ -2,37 +2,41 @@ class Solution {
     public int garbageCollection(String[] garbage, int[] travel) {
         int n = garbage.length;
         int result = 0;
-        for(int i = 0; i < n - 1; i++) {
+        // Total time taken to travel by 3 trucks to all the houses (considering all houses have all types of garbage )
+        for (int i = 0; i < n - 1; i++) {
             result += 3 * travel[i];
         }
-        for(int i = 0; i < n; i++) {
-            result += garbage[i].length();
+        // Total time taken to pick up all garbage from all houses (each pickup takes 1 min)
+        for (String s : garbage) {
+            result += s.length();
         }
-        for(int i = n - 1; i > 0; i--) {
-            if(!garbage[i].contains("G")) {
-                result -= travel[i-1];
-            }
-            else {
+        // finding the last house which contains G
+        // and subtracting any extra time we calculated in line 7
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("G")) {
+                result -= travel[i - 1];
+            } else {
                 break;
             }
         }
-        for(int i = n - 1; i > 0; i--) {
-            if(!garbage[i].contains("P")) {
-                result -= travel[i-1];
-            }
-            else {
+        // finding the last house which contains P
+        // and subtracting any extra time we calculated in line 7
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("P")) {
+                result -= travel[i - 1];
+            } else {
                 break;
             }
         }
-        for(int i = n - 1; i > 0; i--) {
-            if(!garbage[i].contains("M")) {
-                result -= travel[i-1];
-            }
-            else {
+        // finding the last house which contains M
+        // and subtracting any extra time we calculated in line 7
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("M")) {
+                result -= travel[i - 1];
+            } else {
                 break;
             }
         }
-
         return result;
     }
 }
