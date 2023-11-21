1b33de76243432fed4d8c1f986eaee80286803af
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-11-21 00:04:32 -0500

    Time: 4 ms (96.56%) | Memory: 44.1 MB (21.70%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
@@ -1,24 +1,36 @@
 class Solution {
+
+    // Efficient solution using sorting
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);  // Sort the array in ascending order
+        int steps = 0;  // Counter for the minimum moves
+        int i = 0;  // Start index
+        int j = nums.length - 1;  // End index
+
+        // Iterate until the two pointers meet in the middle
+        while (i < j) {
+            steps += (nums[j] - nums[i]);  // Add the difference between the larger and smaller elements
+            i++;  // Move the start pointer towards the center
+            j--;  // Move the end pointer towards the center
+        }
+
+        return steps;  // Return the total minimum moves
+    }
+
+    // Brute-force solution without sorting
     public int minMoves2_BRUTE_FORCE(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);
-        int steps = 0;
+        Arrays.sort(nums);  // Sort the array in ascending order
+        int steps = 0;  // Counter for the minimum moves
+
+        // Calculate the median (middle element) of the sorted array
         int median = (n % 2 == 0 ? (nums[n / 2] + nums[n / 2 - 1]) / 2 : nums[n / 2]);
-        for(int i : nums) {
+
+        // Iterate through the array and add the absolute differences between each element and the median
+        for (int i : nums) {
             steps += Math.abs(i - median);
         }
-        return steps;
-    }
-    public int minMoves2(int[] nums) {
-        Arrays.sort(nums);
-        int steps = 0;
-        int i = 0;
-        int j = nums.length - 1;
-        while(i < j) {
-            steps += (nums[j] - nums[i]);
-            i++;
-            j--;
-        }
-        return steps;
+
+        return steps;  // Return the total minimum moves
     }
 }
