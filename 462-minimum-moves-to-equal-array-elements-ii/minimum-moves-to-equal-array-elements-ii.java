1ad1956d502700b146ac9a0707a5c68e4cf30c5e
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-11-21 00:04:40 -0500

    Time: 5 ms (86.26%) | Memory: 44.8 MB (5.06%) - LeetSync

--- 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
@@ -2,26 +2,26 @@ class Solution {
 
     // Efficient solution using sorting
     public int minMoves2(int[] nums) {
-        Arrays.sort(nums);  // Sort the array in ascending order
-        int steps = 0;  // Counter for the minimum moves
-        int i = 0;  // Start index
-        int j = nums.length - 1;  // End index
+        Arrays.sort(nums); // Sort the array in ascending order
+        int steps = 0; // Counter for the minimum moves
+        int i = 0; // Start index
+        int j = nums.length - 1; // End index
 
         // Iterate until the two pointers meet in the middle
         while (i < j) {
-            steps += (nums[j] - nums[i]);  // Add the difference between the larger and smaller elements
-            i++;  // Move the start pointer towards the center
-            j--;  // Move the end pointer towards the center
+            steps += (nums[j] - nums[i]); // Add the difference between the larger and smaller elements
+            i++; // Move the start pointer towards the center
+            j--; // Move the end pointer towards the center
         }
 
-        return steps;  // Return the total minimum moves
+        return steps; // Return the total minimum moves
     }
 
     // Brute-force solution without sorting
     public int minMoves2_BRUTE_FORCE(int[] nums) {
         int n = nums.length;
-        Arrays.sort(nums);  // Sort the array in ascending order
-        int steps = 0;  // Counter for the minimum moves
+        Arrays.sort(nums); // Sort the array in ascending order
+        int steps = 0; // Counter for the minimum moves
 
         // Calculate the median (middle element) of the sorted array
         int median = (n % 2 == 0 ? (nums[n / 2] + nums[n / 2 - 1]) / 2 : nums[n / 2]);
@@ -31,6 +31,6 @@ class Solution {
             steps += Math.abs(i - median);
         }
 
-        return steps;  // Return the total minimum moves
+        return steps; // Return the total minimum moves
     }
 }
