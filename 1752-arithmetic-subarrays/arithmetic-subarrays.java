c5eb3c70bbc173ce4d3c01e37008ef25edcf61dc
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-11-22 20:37:41 -0500

    Time: 17 ms (77.20%) | Memory: 44.9 MB (9.62%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.java
@@ -2,12 +2,34 @@ class Solution {
 
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
         List<Boolean> result = new ArrayList<>();
+
+        for (int i = 0; i < l.length; i++) {
+            int[] newArray = Arrays.copyOfRange(nums, l[i], r[i] + 1);
+            result.add(checkHelper(newArray));
+        }
+
+        return result;
+    }
+
+    public boolean checkHelper(int[] newArray) {
+        Arrays.sort(newArray);
+        int diff1 = newArray[1] - newArray[0];
+        for (int j = 2; j < newArray.length; j++) {
+            int diff2 = newArray[j] - newArray[j - 1];
+            if (diff1 != diff2) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public List<Boolean> checkArithmeticSubarrays_BRUTE_FORCE(int[] nums, int[] l, int[] r) {
+        List<Boolean> result = new ArrayList<>();
         int n = nums.length;
         int m = l.length;
         for (int i = 0; i < m; i++) {
             int[] newArray = Arrays.copyOfRange(nums, l[i], r[i] + 1);
             Arrays.sort(newArray);
-            System.out.println(Arrays.toString(newArray));
             boolean arithSeq = true;
             int diff1 = newArray[1] - newArray[0];
             for (int j = 2; j < newArray.length; j++) {
