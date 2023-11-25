08e7056f050968a16a5b8579e9238aaecdb7b7b2
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-11-25 14:21:54 -0500

    Time: 4 ms (65.38%) | Memory: 59.7 MB (12.82%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,25 +1,31 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
         int[] result = new int[nums.length];
+
+        int totalSum = 0;
+        for (int num : nums) totalSum += num;
         int leftSum = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            // Calculate the right cumulative sum by 
-            // subtracting the current element and left cumulative sum
             int rightSum = totalSum - nums[i] - leftSum;
 
-            // Calculate the sum of absolute differences for the current element
             result[i] = (
-                i * nums[i] - leftSum + 
-                rightSum - (nums.length - i - 1) * nums[i]
+                nums[i] * i - leftSum + rightSum - (nums.length - i - 1) * nums[i]
             );
-
-            // Update the left cumulative sum by adding the current element
             leftSum += nums[i];
         }
+
+        return result;
+    }
+    public int[] getSumAbsoluteDifferences_BRUTE_FORCE(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                result[i] += Math.abs(nums[i]-nums[j]);
+            }
+        }
+
         return result;
     }
 }
