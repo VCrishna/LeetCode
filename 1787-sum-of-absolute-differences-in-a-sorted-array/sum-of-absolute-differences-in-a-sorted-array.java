bfed503b4e5fb74374187f9f3b558a72d6820fe0
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-11-25 14:28:59 -0500

    Time: 3 ms (99.36%) | Memory: 56.8 MB (86.54%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.java
@@ -1,16 +1,27 @@
 class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
         int[] result = new int[nums.length];
-
         int totalSum = 0;
+        // calculating total sum of elements in nums
         for (int num : nums) totalSum += num;
+        
         int leftSum = 0;
 
         for (int i = 0; i < nums.length; i++) {
+            // Calculating right sum by substracting totalSum, current element 
+            // and leftSum(sum of elements left to current element)    
             int rightSum = totalSum - nums[i] - leftSum;
-
+            // calculating sum of absolute difference of current element
             result[i] = (
-                nums[i] * i - leftSum + rightSum - (nums.length - i - 1) * nums[i]
+                // all the elements to the left of current element are smaller
+                // current element should be remove from all the elements present on the left side
+                // so nums[i] * i (i is the count or total number of elemnts occured so far)
+                // and this is subtracted from leftSum
+                nums[i] * i - leftSum + 
+                // all the elements to the right of the current element are greater
+                // so rightSum - (number of elements present to the right side of the current element) * nums[i]
+                // nums[i] this should be removed from all the elements present to the right of current element
+                rightSum - (nums.length - i - 1) * nums[i]
             );
             leftSum += nums[i];
         }
