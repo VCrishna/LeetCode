34c9033700b28a8dbf92c084c80e5636cd30d748
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-04 13:07:43 -0500

    Time: 3 ms (62.87%) | Memory: 43.6 MB (7.29%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -1,15 +1,30 @@
 class Solution {
     public String largestGoodInteger(String num) {
+        String maxGoodInteger = "";
+
+        for (int i = 0; i <= num.length() - 3; i++) {
+            String currentSubstr = num.substring(i, i + 3);
+            if (isGoodInteger(currentSubstr) && currentSubstr.compareTo(maxGoodInteger) > 0) {
+                maxGoodInteger = currentSubstr;
+            }
+        }
+
+        return maxGoodInteger;
+    }
+
+    private boolean isGoodInteger(String str) {
+        return str.charAt(0) == str.charAt(1) && str.charAt(1) == str.charAt(2);
+    }
+
+    public String largestGoodInteger_BRUTE_FORCE(String num) {
         StringBuilder sb = new StringBuilder();
         int currentMax = 0;
 
-        for(int i = 2; i < num.length(); i++) {
-            if(num.charAt(i) == num.charAt(i - 1) && num.charAt(i) == num.charAt(i - 2)) {
+        for (int i = 2; i < num.length(); i++) {
+            if (num.charAt(i) == num.charAt(i - 1) && num.charAt(i) == num.charAt(i - 2)) {
                 currentMax = Math.max(currentMax, Character.getNumericValue(num.charAt(i)));
-                // sb = new StringBuilder(num.substring(i-2,i + 1));
-                sb = new StringBuilder(currentMax+""+currentMax+""+currentMax);
-            }
-            else {
+                sb = new StringBuilder(currentMax + "" + currentMax + "" + currentMax);
+            } else {
                 continue;
             }
         }
