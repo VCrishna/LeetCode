0f1bf3d6a8916c2a0801c97ed22b75841744209e
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-06 12:24:30 -0500

    Time: 0 ms (100.00%) | Memory: 39.2 MB (43.33%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -1,13 +1,35 @@
 class Solution {
     public int totalMoney(int n) {
+      int weeks = n / 7;
+      // week 1
+      int low = 28;
+      int high = 28 + 7 * ( weeks - 1);
+      // int result = (low + high) * (weeks / 2);
+      int result = (weeks * (low + high) / 2);
+      int monday = weeks + 1;
+      for(int i = 0; i < n % 7; i++) {
+        result += i + monday;
+      }
+
+
+      return result;
+    }
+
+    public int totalMoney_BRUTE_FORCE(int n) {
         int day = 0;
         int deposit = 1;
         int result = 0;
         while (day < n) {
+            // everyday we deposit so we increase result by deposit amount
             result += deposit;
+            // for next day deposit should be increased by than the previous day
             deposit++;
+            // incrementing day by 1
             day++;
-            if(day % 7 == 0) {
+            // to reset the deposit
+            // checking if the day is first day of the week if so
+            if (day % 7 == 0) {
+              // updating the deposit
                 deposit = 1 + (day / 7);
             }
         }
