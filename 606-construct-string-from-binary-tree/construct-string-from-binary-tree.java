4480b6c642d459c4698348c0a28bde0ba73cbb26
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-11 14:40:46 -0500

    Time: 1 ms (100.00%) | Memory: 43.4 MB (82.65%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.java
@@ -14,24 +14,21 @@
  * }
  */
 class Solution {
+
     public String tree2str(TreeNode root) {
         StringBuilder sb = new StringBuilder();
-        dfs(root,sb);
+        dfs(root, sb);
         return sb.toString();
-        
     }
+
     public void dfs(TreeNode root, StringBuilder sb) {
-        if(root == null) {
-            return;
-        }
+        if (root == null) return;
         sb.append(root.val);
-        if(root.left == null && root.right == null) {
-            return;
-        }
+        if (root.left == null && root.right == null) return;
         sb.append("(");
         dfs(root.left, sb);
         sb.append(")");
-        if(root.right != null) {
+        if (root.right != null) {
             sb.append("(");
             dfs(root.right, sb);
             sb.append(")");
