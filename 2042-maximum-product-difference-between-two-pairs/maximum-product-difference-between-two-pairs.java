f002e7d346cfaceb4184d89490019b70d238ee5b
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-17 20:00:20 -0500

    Time: 4 ms (80.07%) | Memory: 44.5 MB (28.04%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.java
@@ -1,5 +1,30 @@
 class Solution {
     public int maxProductDifference(int[] nums) {
+        int biggest = 0;
+        int secondBiggest = 0;
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        
+        for (int i : nums) {
+            if (i > biggest) {
+                secondBiggest = biggest;
+                biggest = i;
+            }
+            else {
+                secondBiggest = Math.max(secondBiggest, i);
+            }
+            if (i < smallest) {
+                secondSmallest = smallest;
+                smallest = i;
+            }
+            else {
+                secondSmallest = Math.min(secondSmallest, i);
+            }
+        }
+        return biggest * secondBiggest - smallest * secondSmallest;
+    }
+
+    public int maxProductDifference_BRUTE_FORCE(int[] nums) {
         Arrays.sort(nums);
         return (nums[nums.length - 1] * nums[nums.length - 2]) - (nums[0] * nums[1]);
     }
