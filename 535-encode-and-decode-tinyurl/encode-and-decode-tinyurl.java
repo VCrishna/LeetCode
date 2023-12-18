e9fd6fdc5f25bfff3610292a1c0dacef54b201cc
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-18 08:13:38 -0500

    Time: 9 ms (9.04%) | Memory: 42.6 MB (12.38%) - LeetSync

--- 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.java
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.java
@@ -1,27 +1,45 @@
 public class Codec {
-    Map<String, String> encode;
-    Map<String, String> decode;
+    Map<Integer, String> encode;
     String base = "http://tinyurl.com/";
+    int i;
     public Codec() {
+        i = 1;
         encode = new HashMap<>();
-        decode = new HashMap<>();
     }
+
     // Encodes a URL to a shortened URL.
     public String encode(String longUrl) {
-        if(!encode.containsKey(longUrl)) {
-            String shortUrl = base + encode.size();
-            encode.put(longUrl, shortUrl);
-            decode.put(shortUrl, longUrl);
-        }
-        return encode.get(longUrl);
+        encode.put(i, longUrl);
+        return base + i++;
     }
 
     // Decodes a shortened URL to its original URL.
     public String decode(String shortUrl) {
-        return decode.get(shortUrl);
+        int key = Integer.parseInt(shortUrl.replace(base,""));
+        return encode.get(key);
     }
-}
+    // Map<String, String> encode;
+    // Map<String, String> decode;
+    // String base = "http://tinyurl.com/";
+    // public Codec() {
+    //     encode = new HashMap<>();
+    //     decode = new HashMap<>();
+    // }
+    // // Encodes a URL to a shortened URL.
+    // public String encode(String longUrl) {
+    //     if(!encode.containsKey(longUrl)) {
+    //         String shortUrl = base + encode.size();
+    //         encode.put(longUrl, shortUrl);
+    //         decode.put(shortUrl, longUrl);
+    //     }
+    //     return encode.get(longUrl);
+    // }
 
+    // // Decodes a shortened URL to its original URL.
+    // public String decode(String shortUrl) {
+    //     return decode.get(shortUrl);
+    // }
+}
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.decode(codec.encode(url));
