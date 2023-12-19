3c63ddd31caec56ae1ce5e1d8d07daa5669720dd
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-19 12:37:51 -0500

    Time: 6 ms (80.11%) | Memory: 44.8 MB (33.89%) - LeetSync

--- 661-image-smoother/image-smoother.java
+++ 661-image-smoother/image-smoother.java
@@ -1,13 +1,53 @@
 class Solution {
+
     public int[][] imageSmoother(int[][] img) {
+        int rows = img.length;
+        int columns = img[0].length;
+
+        // Iterating over each pixel in the image
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                int total = 0; // Accumulator for the sum of pixel values in the neighborhood
+                int count = 0; // Counter for the number of valid pixels in the neighborhood
+
+                // Iterating over the 3x3 neighborhood around the current pixel
+                // row - 1, row, row + 1
+                for (int i = row - 1; i < row + 2; i++) {
+                    // column - 1, column, column + 1
+                    for (int j = column - 1; j < column + 2; j++) {
+                        // Checking if the indices are within the bounds of the image
+                        if (i >= 0 && i < rows && j >= 0 && j < columns) {
+                            // Adding the pixel value to the total
+                            total += img[i][j] % 256; // Using modulo 256 to prevent overflow
+                            // Incrementing the count of valid pixels
+                            count++;
+                        }
+                    }
+                }
+
+                // Updating the original image with the smoothed value using bitwise operations
+                img[row][column] = img[row][column] ^ (total / count) << 8; // Storing smoothed value in the upper byte
+            }
+        }
+
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                img[row][column] = img[row][column] >> 8;
+            }
+        }
+
+        return img;
+    }
+
+    public int[][] imageSmoother_BRUTE_FORCE(int[][] img) {
         int rows = img.length;
         int columns = img[0].length;
         int[][] result = new int[rows][columns];
         // Iterating over each pixel in the image
         for (int row = 0; row < rows; row++) {
             for (int column = 0; column < columns; column++) {
-                int total = 0;// Accumulator for the sum of pixel values in the neighborhood
-                int count = 0;  // Counter for the number of valid pixels in the neighborhood
+                int total = 0; // Accumulator for the sum of pixel values in the neighborhood
+                int count = 0; // Counter for the number of valid pixels in the neighborhood
                 // Iterating over the 3x3 neighborhood around the current pixel
                 // row - 1
                 // row
@@ -18,8 +58,7 @@ class Solution {
                     // column + 1
                     for (int j = column - 1; j < column + 2; j++) {
                         // Checking if the indices are within the bounds of the image
-                        if (i < 0 || i == rows || j < 0 || j == columns) 
-                            continue;
+                        if (i < 0 || i == rows || j < 0 || j == columns) continue;
                         // Adding the pixel value to the total
                         total += img[i][j];
                         // Incrementing the count of valid pixels
@@ -30,7 +69,6 @@ class Solution {
                 result[row][column] = total / count;
             }
         }
-
         return result;
     }
 }
