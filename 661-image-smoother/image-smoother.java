e18336a18db6387f76fb4550d7014101f227bcf1
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-19 12:29:03 -0500

    Time: 5 ms (89.64%) | Memory: 44.5 MB (57.42%) - LeetSync

--- 661-image-smoother/image-smoother.java
+++ 661-image-smoother/image-smoother.java
@@ -3,27 +3,33 @@ class Solution {
         int rows = img.length;
         int columns = img[0].length;
         int[][] result = new int[rows][columns];
-
-        for(int row = 0; row < rows; row++) {
-            for(int column = 0; column < columns; column++) {
-                int total = 0;
-                int count = 0;
+        // Iterating over each pixel in the image
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                int total = 0;// Accumulator for the sum of pixel values in the neighborhood
+                int count = 0;  // Counter for the number of valid pixels in the neighborhood
+                // Iterating over the 3x3 neighborhood around the current pixel
                 // row - 1
                 // row
                 // row + 1
-                for(int i = row - 1; i < row + 2; i++) {
-                    for(int j = column - 1; j < column + 2; j++) {
-                        if(i < 0 || i == rows || j < 0 || j == columns)
+                for (int i = row - 1; i < row + 2; i++) {
+                    // column - 1
+                    // column
+                    // column + 1
+                    for (int j = column - 1; j < column + 2; j++) {
+                        // Checking if the indices are within the bounds of the image
+                        if (i < 0 || i == rows || j < 0 || j == columns) 
                             continue;
+                        // Adding the pixel value to the total
                         total += img[i][j];
+                        // Incrementing the count of valid pixels
                         count++;
                     }
                 }
-
-                result[row][column] = (int)Math.floor(total / count);
+                // Calculating the average value for the current pixel's neighborhood
+                result[row][column] = total / count;
             }
         }
-        
 
         return result;
     }
