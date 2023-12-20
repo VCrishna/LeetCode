5cd98e30662845ab8dc71cc5066145a70317d21a
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-20 00:22:09 -0500

    Time: 1 ms (100.00%) | Memory: 43.2 MB (65.94%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.java
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -1,8 +1,31 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
+        if (prices == null || prices.length < 2) {
+            return money;
+        }
+        int firstMin = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+        for (int p : prices) {
+            if (p < firstMin) {
+                secondMin = firstMin;
+                firstMin = p;
+            } else if (p < secondMin) {
+                secondMin = p;
+            }
+        }
+        int minCost = firstMin + secondMin;
+        if (minCost <= money) {
+            return money - minCost;
+        } else {
+            // Not enough money to buy the minimum cost chocolates
+            return money;
+        }
+    }
+
+    public int buyChoco_BRUTE_FORCE(int[] prices, int money) {
         Arrays.sort(prices);
         int minCost = prices[0] + prices[1];
-        if(minCost <= money) {
+        if (minCost <= money) {
             return money - minCost;
         }
         return money;
