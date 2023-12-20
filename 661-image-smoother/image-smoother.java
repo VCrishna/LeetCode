b7515c08164b234571cbfd859048422e7aa2df8d
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-20 16:22:55 -0500

    Time: 6 ms (67.06%) | Memory: 45.8 MB (5.30%) - LeetSync

--- 661-image-smoother/image-smoother.java
+++ 661-image-smoother/image-smoother.java
@@ -1,5 +1,29 @@
 class Solution {
+
     public int[][] imageSmoother(int[][] img) {
+        int rows = img.length;
+        int columns = img[0].length;
+        int[][] result = new int[rows][columns];
+
+        for (int row = 0; row < rows; row++) {
+            for (int column = 0; column < columns; column++) {
+                int total = 0;
+                int count = 0;
+
+                for (int i = row - 1; i < row + 2; i++) {
+                    for (int j = column - 1; j < column + 2; j++) {
+                        if (i < 0 || i == rows || j < 0 || j == columns) continue;
+                        total += img[i][j];
+                        count++;
+                    }
+                }
+                result[row][column] = total / count;
+            }
+        }
+        return result;
+    }
+
+    public int[][] imageSmoother_BIT_MANIPULATION(int[][] img) {
         int rows = img.length;
         int columns = img[0].length;
         // Iterating over each pixel in the image
