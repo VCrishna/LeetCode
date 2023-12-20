e421f56f0e615799aaa1b8392fad2181de8f3a9a
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-19 19:23:39 -0500

    Time: 12 ms (35.29%) | Memory: 45 MB (10.44%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.java
+++ 271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -1,34 +1,30 @@
 public class Codec {
+
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
-        StringBuilder sb = new StringBuilder();
-        for (String s : strs) {
-            sb.append(s.length() + "#" + s);
-        }
-        System.out.println(sb.toString());
-        return sb.toString();
+        StringBuilder encode = new StringBuilder();
+        for (String s : strs) encode.append(s.length() + "#" + s);
+        // System.out.println(encode.toString());
+        return encode.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        List<String> words = new ArrayList<>();
-        if(s.length() == 0 || s == null) return words;
+        List<String> decoded = new ArrayList<>();
         int i = 0;
-        while(i < s.length()) {
+
+        while (i < s.length()) {
             int j = i;
-            // finding delimitter
-            while(j < s.length()) {
-                if(s.charAt(j) == '#') {
-                    break;
-                }
+            while (j < s.length()) {
+                if (s.charAt(j) == '#') break;
                 j++;
             }
             int length = Integer.parseInt(s.substring(i, j));
-            words.add(s.substring(j+1, j+1+length));
+            decoded.add(s.substring(j + 1, j + 1 + length));
             i = j + 1 + length;
         }
 
-        return words;
+        return decoded;
     }
 }
 // Your Codec object will be instantiated and called as such:
