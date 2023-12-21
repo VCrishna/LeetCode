24c740a142424af515a281964a6f6f08f0910e98
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-21 12:03:18 -0500

    Time: 1 ms (97.49%) | Memory: 40.8 MB (5.36%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,12 +1,18 @@
 class Solution {
-        public int reverse(int x) {
-        String ans = x < 0 
-                ? new StringBuilder(String.valueOf(-x)).append("-").reverse().toString()
-                : new StringBuilder(String.valueOf(x)).reverse().toString();
-        try {
-            return Integer.parseInt(ans);
-        } catch (Exception e) {
-            return 0;
+    public int reverse(int x) {
+        int neg = x < 0 ? -1 : 1;
+
+        long reversed = 0l;
+
+        x = neg * x;
+
+        while (x > 0) {
+            int digit = x % 10;
+            x /= 10;
+            reversed = reversed * 10 + digit;
         }
+
+        if(reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) return 0;
+        return (int) reversed * neg;
     }
 }
