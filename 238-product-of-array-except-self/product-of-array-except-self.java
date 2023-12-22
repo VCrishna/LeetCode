a509b8a3fa367d47dfca37cc8b34a8a842e0c378
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-22 12:13:01 -0500

    Time: 2 ms (63.69%) | Memory: 52.6 MB (54.80%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,26 +1,25 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] left = new int[nums.length];
-        left[0] = 1;
-        int[] right = new int[nums.length];
-        right[right.length - 1] = 1;
-        int[] result = new int[nums.length];
-        for (int i = 1; i < nums.length; i++) {
-            left[i] = nums[i - 1] * left[i - 1];
+        int length = nums.length;
+        int[] leftProd = new int[length];
+        leftProd[0] = 1;
+        int[] rightProd = new int[length];
+        rightProd[length - 1] = 1;
+        int[] result = new int[length];
+
+        for(int i = 1; i < length; i++) {
+            leftProd[i] = leftProd[i - 1] * nums[i - 1];
+            
         }
-        // for (int i = nums.length - 2; i >= 0; i--) {
-        //     right[i] = right[i + 1] * nums[i + 1];
-        // }
-        // for (int i = 0; i < nums.length; i++) {
-        //     result[i] = left[i] * right[i];
-        // }
-        // without using extra space
-        int mul = 1;
-        for(int i = nums.length - 1; i>= 0; i--) {
-            result[i] = left[i] * mul;
-            mul = mul * nums[i];
+
+        for(int i = length - 2; i >= 0; i--) {
+            rightProd[i] = rightProd[i + 1] * nums[i + 1];
         }
+
+        for(int i = 0; i < length; i++) {
+            result[i] = leftProd[i] * rightProd[i];
+        }
+
         return result;
     }
 }
-
