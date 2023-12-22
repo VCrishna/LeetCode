c91e65d1c5f3d9dba937d7d214f51c44f7166410
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-22 12:20:46 -0500

    Time: 1 ms (100.00%) | Memory: 54.7 MB (5.42%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,23 +1,28 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int length = nums.length;
+        
         int[] leftProd = new int[length];
         leftProd[0] = 1;
         int[] rightProd = new int[length];
         rightProd[length - 1] = 1;
         int[] result = new int[length];
 
-        for(int i = 1; i < length; i++) {
+        for (int i = 1; i < length; i++) {
             leftProd[i] = leftProd[i - 1] * nums[i - 1];
-            
         }
 
-        for(int i = length - 2; i >= 0; i--) {
-            rightProd[i] = rightProd[i + 1] * nums[i + 1];
-        }
-
-        for(int i = 0; i < length; i++) {
-            result[i] = leftProd[i] * rightProd[i];
+        // for(int i = length - 2; i >= 0; i--) {
+        //     rightProd[i] = rightProd[i + 1] * nums[i + 1];
+        // }
+        // for(int i = 0; i < length; i++) {
+        //     result[i] = leftProd[i] * rightProd[i];
+        // }
+        
+        int mul = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            result[i] = leftProd[i] * mul;
+            mul = mul * nums[i];
         }
 
         return result;
