d0bd53b3fd38334aa89ca7bc0c7f3b29154e72b8
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-23 13:15:03 -0500

    Time: 0 ms (100.00%) | Memory: 41.2 MB (14.48%) - LeetSync

--- 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.java
+++ 1065-binary-string-with-substrings-representing-1-to-n/binary-string-with-substrings-representing-1-to-n.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean queryString(String s, int n) {
-        for(int i = 1; i <=n; i++) {
-            String str = Integer.toBinaryString(i);
-            int a = s.indexOf(str);
-            if(a == -1) return false;
+        for(int i = 1; i <= n; i++) {
+            String binaryString = Integer.toBinaryString(i);
+            int index = s.indexOf(binaryString);
+            if(index == -1) 
+                return false;
         }
         return true;
-   }
+    }
 }
