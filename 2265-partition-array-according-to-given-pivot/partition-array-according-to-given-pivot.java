c815e0ac81d01b65077226b16c886f1d7b54610b
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-23 15:58:20 -0500

    Time: 6 ms (33.29%) | Memory: 68.1 MB (5.49%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,23 +1,41 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int[] result = new int[nums.length];
-        int index = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] < pivot) {
-                result[index++] = nums[i];
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < pivot) {
+                result[left++] = nums[i];
+            }
+            if(nums[nums.length - 1 - i] > pivot) {
+                result[right--] = nums[nums.length - 1 - i];
             }
         }
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] == pivot) {
-                result[index++] = nums[i];
-            }
+        while(left <= right) {
+            result[left++] = pivot;
+            result[right--] = pivot;
         }
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] > pivot) {
-                result[index++] = nums[i];
-            }
-        }
+        return result;
+    }
 
+    public int[] pivotArray_BRUTE_FORCE(int[] nums, int pivot) {
+        int[] result = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < pivot) {
+                result[index++] = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == pivot) {
+                result[index++] = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > pivot) {
+                result[index++] = nums[i];
+            }
+        }
         return result;
     }
 }
