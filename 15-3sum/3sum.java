7f83c779d9f3ba1a6bf8d934390882031e50540b
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-25 12:29:49 -0500

    Time: 31 ms (67.77%) | Memory: 51.4 MB (27.33%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,6 +4,7 @@ class Solution {
         Arrays.sort(nums);
 
         for(int i = 0; i < nums.length; i++) {
+            // avoiding duplicates for first elemet
             if(i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
                 int first = nums[i];
                 int left = i + 1;
@@ -13,13 +14,15 @@ class Solution {
                     int sum = nums[left] + nums[right];
                     if(first + sum == 0) {
                         result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        // avoiding duplicates at left
                         while (left < right && nums[left] == nums[left + 1]) {
                             left++;
                         }
+                        // avoiding duplicates at right
                         while (left < right && nums[right] == nums[right - 1]) {
                             right--;
                         }
-
+                        // regular increment of pointers
                         left++;
                         right--;
                     }
