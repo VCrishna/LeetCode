420bab993249483e727ef463d557539a89d30bec
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-26 11:25:19 -0500

    Time: 0 ms (100.00%) | Memory: 42.3 MB (6.33%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -5,16 +5,18 @@ class Solution {
 
         int left = 0;
         int right = rows * columns - 1;
-        while(left <= right) {
-            int middleIndex = left + (right - left)/2;
-            int middleValue = matrix[middleIndex / columns][middleIndex % columns];
-            if (target == middleValue)
-                return true;
-            if (target < middleValue)
-                right = middleIndex - 1;
-            else
-                left = middleIndex + 1;
 
+        while(left <= right) {
+            int middleIndex = right + (left - right) / 2;
+            int middleValue = matrix[middleIndex / columns][middleIndex % columns];
+            if(middleValue == target) 
+                return true;
+            if(middleValue > target) {
+                right = middleIndex - 1;
+            }
+            else {
+                left =  middleIndex + 1;
+            }
         }
         return false;
     }
