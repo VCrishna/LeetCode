098ca8437826d6ccf7dcda6d963fb28cfeef8737
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-26 14:05:06 -0500

    Time: 1 ms (100.00%) | Memory: 47.3 MB (5.02%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -15,8 +15,14 @@ class Solution {
                 right--;
             }
         }
-
-
         return result;
+    }
+    public int[] sortedSquares_BRUTE_FORCE(int[] nums) {
+        int[] squareArray = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            squareArray[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(squareArray);
+        return squareArray;
     }
 }
