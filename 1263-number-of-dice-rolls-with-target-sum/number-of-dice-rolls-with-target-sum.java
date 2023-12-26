358ac1155a0aa48186188206744a652a831641b6
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-25 23:50:27 -0500

    Time: 10 ms (68.33%) | Memory: 42.4 MB (20.64%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -2,27 +2,31 @@ class Solution {
     int MOD = (int) 1e9 + 7;
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp = new int[n + 1][target + 1];
-        for (int[] d : dp) {
-            Arrays.fill(d, -1);
+
+        for (int[] smallDP : dp) {
+            Arrays.fill(smallDP, -1);
         }
-        return helper(n, k, target, dp);
+
+        return dfs(n, k, target, dp);
     }
-    public int helper(int n, int k, int target, int[][] dp) {
-        if(n < 0 || target < 0) {
+
+    public int dfs(int n, int k, int target, int[][] dp) {
+        if (n < 0 || target < 0) {
             return 0;
         }
-        if(n == 0 && target == 0) {
+        if (n == 0 && target == 0) {
             return 1;
         }
-        if(dp[n][target] != -1)
+        if (dp[n][target] != -1) {
             return dp[n][target];
-        int totalWays = 0;
-        
-        for (int i = 1; i <= k; i++) {
-            if(i <= target) {
-                totalWays = (totalWays + helper(n - 1, k, target - i, dp)) % MOD;
-            }
         }
+
+        int totalWays = 0;
+
+        for(int i = 1; i <= k; i++) {
+            totalWays = (totalWays + dfs(n - 1, k, target - i, dp)) % MOD;
+        }
+
 
         dp[n][target] = totalWays;
         return totalWays;
