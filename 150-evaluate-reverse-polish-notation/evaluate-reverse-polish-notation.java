dbaf40dc95c9919cd4ec87166c341e9e59dbd7a1
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-28 16:36:50 -0500

    Time: 8 ms (15.54%) | Memory: 44.8 MB (6.83%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -2,6 +2,41 @@ class Solution {
 
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (!"+-*/".contains(token)) {
+                stack.push(Integer.valueOf(token));
+                continue;
+            }
+
+            int number2 = stack.pop();
+            int number1 = stack.pop();
+
+            int result = 0;
+
+            switch (token) {
+                case "+":
+                    result = number1 + number2;
+                    break;
+                case "-":
+                    result = number1 - number2;
+                    break;
+                case "*":
+                    result = number1 * number2;
+                    break;
+                case "/":
+                    result = number1 / number2;
+                    break;
+            }
+
+            stack.push(result);
+        }
+
+        return stack.pop();
+    }
+
+    public int evalRPN_BRUTE_FORCE(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
         for (String s : tokens) {
             if (s.equals("+")) {
                 int num1 = stack.isEmpty() ? 0 : stack.pop();
