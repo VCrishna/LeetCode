dfc0bc42e05c3cd3e4a8d6df55306501b383aa3f
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-28 16:45:26 -0500

    Time: 1 ms (88.49%) | Memory: 43.7 MB (25.18%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,18 +1,36 @@
+// class Solution {
+//     public List<String> generateParenthesis(int n) {
+//         List<String> result = new ArrayList<>();
+//         backtrack(n, n, n, "", result);
+//         return result;
+//     }
+
+//     public void backtrack(int n, int left, int right, String currentString, List<String> result) {
+//         if (left == 0 && right == 0) {
+//             result.add(currentString);
+//             return;
+//         }
+//         if (left > 0)
+//             backtrack(n, left - 1, right, currentString + "(", result);
+//         if (right > left)
+//             backtrack(n, left, right - 1, currentString + ")", result);
+//     }
+// }
+
 class Solution {
+
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        backtrack(n, n, n, "", result);
+        generate(result, "", n, n);
         return result;
     }
 
-    public void backtrack(int n, int left, int right, String currentString, List<String> result) {
+    public void generate(List<String> result, String str, int left, int right) {
         if (left == 0 && right == 0) {
-            result.add(currentString);
+            result.add(str);
             return;
         }
-        if (left > 0) 
-            backtrack(n, left - 1, right, currentString + "(", result);
-        if (right > left) 
-            backtrack(n, left, right - 1, currentString + ")", result);
+        if (left > 0) generate(result, str + "(", left - 1, right);
+        if (right > left) generate(result, str + ")", left, right - 1);
     }
 }
