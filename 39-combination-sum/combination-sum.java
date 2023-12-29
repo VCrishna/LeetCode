d7533fcd36b07faa3dae38bac39715946171933f
VCrishna <67335876+VCrishna@users.noreply.github.com>
2023-12-29 15:46:36 -0500

    Time: 9 ms (6.69%) | Memory: 44.6 MB (9.74%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,21 +2,29 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> currentList = new ArrayList<>();
-        
-        // call backtrack funtion
-        backtrack(0, currentList, 0, target, candidates, result);
+
+        // calling backtrack
+        backtrack(
+            target,
+            0,
+            0,
+            currentList,
+            result,
+            candidates          
+        );
 
         return result;
     }
 
-    public void backtrack(
+    public void backtrack (
+        int target,
         int currentSum,
-        List<Integer> currentList, 
-        int currentIndex, 
-        int target, 
-        int[] candidates,
-        List<List<Integer>> result
+        int currentIndex,
+        List<Integer> currentList,
+        List<List<Integer>> result,
+        int[] candidates
     ) {
+        
         if(currentSum == target) {
             result.add(new ArrayList(currentList));
             return;
@@ -24,24 +32,29 @@ class Solution {
         if(currentIndex >= candidates.length || currentSum > target) {
             return;
         }
-        
-        // considering the element
+
+        // consider current value
+        // add the value into currentList
         currentList.add(candidates[currentIndex]);
         backtrack(
-            currentSum + candidates[currentIndex], 
-            currentList, 
-            currentIndex, 
             target,
-            candidates,
-            result);
-        // not considering the element
+            currentSum + candidates[currentIndex],
+            currentIndex,
+            currentList,
+            result,
+            candidates          
+        );
+
+        // donot consider the current value
+        // clean after backtrack - remove element from currentList
         currentList.remove(currentList.get(currentList.size() - 1));
         backtrack(
-            currentSum, 
-            currentList, 
-            currentIndex + 1, 
             target,
-            candidates,
-            result);
+            currentSum,
+            currentIndex + 1,
+            currentList,
+            result,
+            candidates          
+        );
     }
 }
