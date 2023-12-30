502e86e441aeadc3be83afbbec72c93721805194
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2023-12-30 00:24:07 -0500

    Time: 5 ms (22.54%) | Memory: 43.5 MB (20.34%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,36 +1,44 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<Integer> currentList = new ArrayList<>();
         List<List<Integer>> result = new ArrayList<>();
+        List<Integer> currentList = new ArrayList<>();
         Arrays.sort(candidates);
-        backtrack(target, 0, currentList, result, candidates);
+        backtrack(
+            candidates,
+            target,
+            result,
+            currentList,
+            0
+        );
 
         return result;
     }
-    public void backtrack (
-        int target,
-        int currentIndex,
-        List<Integer> currentList,
-        List<List<Integer>> result,
-        int[] candidates        
-    ) {
-        if(target == 0) {
-            result.add(new ArrayList<>(currentList));
-            return;
-        }
-        if(target < 0 || currentIndex >= candidates.length) {
-            return;
-        }
+    public void backtrack(
+            int[] candidates, 
+            int target, 
+            List<List<Integer>> result, 
+            List<Integer> currentList, 
+            int currentIndex) {
+                if(target == 0) {
+                    result.add(new ArrayList<>(currentList));
+                }
+                else if(target < 0 || currentIndex >= candidates.length) {
+                    return;
+                }
+                for(int i = currentIndex; i < candidates.length; i++) {
+                    if(i > currentIndex && candidates[i] == candidates[i - 1]) {
+                        continue;
+                    }
+                    currentList.add(candidates[i]);
+                    backtrack(
+                        candidates,
+                        target - candidates[i],
+                        result,
+                        currentList,
+                        i + 1
+                    );
+                    currentList.remove(currentList.size() - 1);
+                }
 
-        for(int i = currentIndex; i < candidates.length; i++) {
-            if(i > currentIndex && candidates[i] == candidates[i - 1]) {
-                continue;
             }
-            currentList.add(candidates[i]);
-            // i + 1
-            backtrack(target - candidates[i], i + 1, currentList, result, candidates);
-            currentList.remove(currentList.size() - 1);
-        }
-
-    }
 }
