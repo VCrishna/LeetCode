2c0f973ffb48b862ed73e6fe78bc80a58da55ccc
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2023-12-30 00:16:17 -0500

    Time: 3 ms (32.28%) | Memory: 44.6 MB (13.36%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,32 +2,41 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> currentList = new ArrayList<>();
-
-        // calling backtrack function
-        backtrack(target, 0, currentList, result, candidates);
+        backtrack(
+            result,
+            candidates,
+            target,
+            currentList, 
+            0,
+            target
+        );
         return result;
     }
-
-    public void backtrack (
-        int target,
-        int currentIndex,
-        List<Integer> currentList,
-        List<List<Integer>> result,
-        int[] candidates        
+    public void backtrack(
+            List<List<Integer>> result,
+            int[] candidates, 
+            int target, 
+            List<Integer> currentList, 
+            int currentIndex,
+            int currentSum
     ) {
-        if(target == 0) {
-            result.add(new ArrayList<>(currentList));
+        if(currentSum == 0) {
+            result.add(new ArrayList(currentList));
+        }
+        if(currentSum < 0 || currentIndex >= candidates.length) {
             return;
         }
-        if(target < 0 || currentIndex >= candidates.length) {
-            return;
-        }
-
         for(int i = currentIndex; i < candidates.length; i++) {
             currentList.add(candidates[i]);
-            backtrack(target - candidates[i], i, currentList, result, candidates);
+            backtrack(
+                result,
+                candidates,
+                target,
+                currentList, 
+                i,
+                currentSum - candidates[i]
+            );
             currentList.remove(currentList.size() - 1);
         }
-
     }
 }
