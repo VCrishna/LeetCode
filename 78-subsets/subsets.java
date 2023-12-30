54adb859d05ede67833b257a1988754f86ad1d4e
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2023-12-29 22:56:08 -0500

    Time: 13 ms (5.82%) | Memory: 42.6 MB (12.97%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        backtrack(nums, result, new ArrayList<>(), 0);
+
+        return result;
+    }
+
+    public void backtrack (
+        int[] nums, 
+        List<List<Integer>> result,
+        List<Integer> currentList,
+        int currentIndex) {
+            if(!result.contains(currentList)) {
+                result.add(new ArrayList<>(currentList));
+            }
+            for(int i = currentIndex; i < nums.length; i++) {
+                currentList.add(nums[i]);
+                backtrack(nums, result, currentList, i + 1);
+                currentList.remove(currentList.size() - 1);
+            }
+
+    }
+}
