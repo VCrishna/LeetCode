07a944f9eb99468a65a1a1010b20d1493c2f79ad
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2023-12-30 21:28:48 -0500

    Time: 126 ms (9.84%) | Memory: 58.5 MB (23.90%) - LeetSync

---
+++ 883-car-fleet/car-fleet.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        if (position.length == 1) return 1;
+        // position, speed
+        int[][] combined = new int[position.length][2];
+        for (int i = 0; i < position.length; i++) {
+            combined[i][0] = position[i];
+            combined[i][1] = speed[i];
+        }
+        Arrays.sort(combined, (a, b) -> a[0] - b[0]);
+
+        // for (int[] i : combined) {
+        //     System.out.println(Arrays.toString(i));
+        // }
+        Stack<Double> stack = new Stack<>();
+        for(int i = combined.length - 1; i >= 0; i--) {
+            // time = distance ÷ speed
+            double currentTime = (double) (target - combined[i][0]) / combined[i][1];
+            if(!stack.isEmpty() && currentTime <= stack.peek()) {
+                continue;
+            }
+            else {
+                stack.push(currentTime);
+            }
+        }
+        return stack.size();
+    }
+}
