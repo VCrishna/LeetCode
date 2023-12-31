180d7bab09f8985a4ddaf825caf57c289632d6d7
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2023-12-30 21:03:31 -0500

    Time: 170 ms (13.41%) | Memory: 85 MB (48.60%) - LeetSync

--- 1366-first-unique-number/first-unique-number.java
+++ 1366-first-unique-number/first-unique-number.java
@@ -1,54 +1,25 @@
 class FirstUnique {
-    // Map<Integer, Integer> map;
-    // PriorityQueue<Integer> minHeap;
-    // public FirstUnique(int[] nums) {
-    //     map = new HashMap<>();
-    //     minHeap = new PriorityQueue<>(
-    //         (a,b) -> map.get(a) - map.get(b)
-    //     );
-    //     for(int i : nums) 
-    //         map.put(i , map.getOrDefault(i , 0) + 1);
-    //     for(int i : nums) minHeap.add(i);
-    // }
-    
-    // public int showFirstUnique() {
-    //     while(!minHeap.isEmpty() && map.get(minHeap.peek()) > 1) {
-    //         minHeap.poll();
-    //     }
-    //     return minHeap.isEmpty() ? -1 : minHeap.peek();
-    // }
-    
-    // public void add(int value) {
-    //     map.put(value , map.getOrDefault(value , 0) + 1);
-    //     if (map.get(value) == 1) {
-    //         minHeap.offer(value);
-    //     }
-    // }
-    private Map<Integer, Integer> frequencyMap;
-    private Queue<Integer> uniqueQueue;
-
+    Map<Integer, Integer> map;
+    Queue<Integer> queue;
     public FirstUnique(int[] nums) {
-        frequencyMap = new HashMap<>();
-        uniqueQueue = new LinkedList<>();
-
-        for (int num : nums) {
-            add(num);
+        map = new HashMap<>();
+        queue = new LinkedList<>();
+        for(int i : nums) {
+            add(i);
         }
     }
-
+    
     public int showFirstUnique() {
-        while (!uniqueQueue.isEmpty() && frequencyMap.get(uniqueQueue.peek()) > 1) {
-            uniqueQueue.poll();
+        while(!queue.isEmpty() && map.get(queue.peek()) > 1) {
+            queue.poll();
         }
-
-        return uniqueQueue.isEmpty() ? -1 : uniqueQueue.peek();
+        return queue.isEmpty() ? -1 : queue.peek();
     }
-
+    
     public void add(int value) {
-        frequencyMap.put(value, frequencyMap.getOrDefault(value, 0) + 1);
-        
-        if (frequencyMap.get(value) == 1) {
-            uniqueQueue.offer(value);
+        map.put(value, map.getOrDefault(value, 0) + 1);
+        if(map.get(value) == 1) {
+            queue.offer(value);
         }
     }
 }
