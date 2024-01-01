9fa7bd985af8143ed904a1289a90d33df6b0e69e
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-01 16:09:07 -0500

    Time: 11 ms (6.93%) | Memory: 41.6 MB (12.19%) - LeetSync

--- 1283-reformat-date/reformat-date.java
+++ 1283-reformat-date/reformat-date.java
@@ -1,4 +1,5 @@
 class Solution {
+
     public String reformatDate(String date) {
         Map<String, String> months = new HashMap<>();
         months.put("Jan", "01");
@@ -15,12 +16,8 @@ class Solution {
         months.put("Dec", "12");
         StringBuilder reformatedDate = new StringBuilder();
         String[] dateStrs = date.split(" ");
-        String day = dateStrs[0].length() > 3 ? dateStrs[0].substring(0,2) : "0"+dateStrs[0].substring(0,1);
-        reformatedDate.append(
-            dateStrs[2] + "-" + 
-            months.get(dateStrs[1]) + "-" +
-            day
-            );
+        String day = dateStrs[0].length() > 3 ? dateStrs[0].substring(0, 2) : "0" + dateStrs[0].substring(0, 1);
+        reformatedDate.append(dateStrs[2] + "-" + months.get(dateStrs[1]) + "-" + day);
 
         return reformatedDate.toString();
     }
