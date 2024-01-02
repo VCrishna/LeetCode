cf216ba39125c40181c3df1a3b20366ad5e50509
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-01 23:18:10 -0500

    Time: 3 ms (77.12%) | Memory: 44.9 MB (18.64%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -3,13 +3,14 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         // number and its frequency
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int i : nums) {
             map.put(i, map.getOrDefault(i, 0) + 1);
             int row = map.get(i);
+            // Ensuring that the result list has enough rows
             if(result.size() < row) {
                 result.add(new ArrayList<>());
             }
+            // Adding the number to the appropriate frequency group
             result.get(row - 1).add(i);
         }
 
