cf9815560a08b94df1b2942820832fd8e0b51946
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-02 18:09:27 -0500

    Time: 16 ms (5.09%) | Memory: 41.6 MB (14.19%) - LeetSync

--- 355-design-twitter/design-twitter.java
+++ 355-design-twitter/design-twitter.java
@@ -2,9 +2,9 @@ class Twitter {
     // User - follower list
     // userid, followers set
     // Map<User, Set<Users>>
-    // userid, [timestamp, tweet]
-    // Map<Integer, List<int[]> tweetMap;
     Map<Integer, Set<Integer>> users;
+    // userid, List[timestamp, tweet]
+    // Map<Integer, List<int[]> tweetMap;
     Map<Integer, List<int[]>> tweetMap;
     int time;
 
@@ -15,24 +15,28 @@ class Twitter {
     }
 
     public void postTweet(int userId, int tweetId) {
-        int[] tweet = new int[] { time, tweetId };
+        // creating new tweet object, incrementing time for each new tweet
+        int[] tweet = new int[] { time++, tweetId };
+        // checking if userId is not present in the tweetMap
         if (!tweetMap.containsKey(userId)) {
             tweetMap.put(userId, new ArrayList<>());
         }
+        // adding tweet to the specific user
         tweetMap.get(userId).add(tweet);
-        time++;
     }
 
     public List<Integer> getNewsFeed(int userId) {
         List<Integer> feed = new ArrayList<>();
+        // getting all the user followers
         Set<Integer> followers = users.getOrDefault(userId, new HashSet<>());
+        // as we also want to show user tweets as well so we are adding userId into the set
         followers.add(userId);
+        // maxHeap 
         PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[0] - a[0]);
 
         for (int user : followers) {
             if (tweetMap.containsKey(user)) {
-                List<int[]> lst = tweetMap.get(user);
-                lst.forEach(x -> maxHeap.offer(x));
+                tweetMap.get(user).forEach(x -> maxHeap.offer(x));
             }
         }
 
@@ -52,7 +56,8 @@ class Twitter {
     }
 
     public void unfollow(int followerId, int followeeId) {
-        if (users.containsKey(followerId)) users.get(followerId).remove(followeeId);
+        if (users.containsKey(followerId)) 
+            users.get(followerId).remove(followeeId);
     }
 }
 /**
