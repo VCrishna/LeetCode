950ef9493b9b27c7326f56e73026de7c2a550c85
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-03 15:40:25 -0500

    Time: 0 ms (100.00%) | Memory: 40.4 MB (33.98%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0 || nums == null) {
+        if (nums.length == 0 || nums == null) {
             return 0;
         }
-        if(nums.length == 1) {
+        if (nums.length == 1) {
             return nums[0];
         }
-        if(nums.length == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-        int[] dp = new int[nums.length + 1];
+        int[] dp = new int[nums.length];
+        
+        // base cases for the first two houses
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
-        for(int i = 2; i < nums.length; i++) {
+        for (int i = 2; i < nums.length; i++) {
+            // For each house i, calculate the maximum amount that can be robbed:
+            // Either by robbing the current house and adding the maximum amount from two houses back (dp[i - 2]),
+            // or by skipping the current house and taking the maximum amount from the previous house (dp[i - 1])
             dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
         }
+        
         return dp[nums.length - 1];
     }
 }
