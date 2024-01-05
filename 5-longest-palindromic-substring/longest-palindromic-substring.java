e94aa650d1f6d755ad8d0d87f3e02c838cd63058
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-05 01:21:00 -0500

    Time: 22 ms (56.49%) | Memory: 45.3 MB (21.52%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,38 +1,39 @@
 class Solution {
-
     public String longestPalindrome(String s) {
-        int start = 0;
-        int end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int oddLength = expand(i, i, s);
-            // if (oddLength > end - start + 1) {
-            //     int dist = oddLength / 2;
-            //     start = i - dist;
-            //     end = i + dist;
-            // }
+        String result = "";
+        int resultLength = 0;
 
-            int evenLength = expand(i, i + 1, s);
-            // if (evenLength > end - start + 1) {
-            //     int dist = (evenLength / 2) - 1;
-            //     start = i - dist;
-            //     end = i + 1 + dist;
-            // }
-            int len = Math.max(evenLength, oddLength);
-            if (len > end - start) {
-                start = i - ((len - 1) / 2);
-                end = i + (len / 2);
+        for(int i = 0; i < s.length(); i++) {
+            // odd length
+            int left = i;
+            int right = i;
+
+            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                if(right - left + 1 > resultLength) {
+                    result = s.substring(left, right + 1);
+                    resultLength = right - left + 1;
+                }
+
+                left--;
+                right++;
+            }
+
+            // even length
+            left = i;
+            right = i + 1;
+
+            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                if(right - left + 1 > resultLength) {
+                    result = s.substring(left, right + 1);
+                    resultLength = right - left + 1;
+                }
+
+                left--;
+                right++;
             }
         }
 
-        return s.substring(start, end + 1);
-    }
 
-    private int expand(int left, int right, String s) {
-        if (s == null || left > right) return 0;
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            left--;
-            right++;
-        }
-        return right - left - 1;
+        return result;
     }
 }
