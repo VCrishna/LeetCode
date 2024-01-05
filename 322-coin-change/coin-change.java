7bf21dbdcd5a1e118310245a4257997dfc303260
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-04 22:07:51 -0500

    Time: 15 ms (77.29%) | Memory: 44.1 MB (34.65%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,16 +1,30 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        // Creating an array dp to store the minimum number of coins needed for each amount
         int[] dp = new int[amount + 1];
+
+        // Initialize the dp array with a value larger than any possible number of coins
+        // i.e., amount + 1
         Arrays.fill(dp, amount + 1);
+
+        // Base case: The minimum number of coins needed to make up amount 0 is 0
         dp[0] = 0;
-        
-        for(int a = 1; a < amount + 1; a++) {
-            for(int c:coins){
-                if(a-c >= 0) {
-                    dp[a] = Math.min(dp[a], 1+dp[a-c]);
+
+        // Iterate through each amount from 1 to the target amount
+        for (int amt = 1; amt <= amount; amt++) {
+            // Iterate through each coin denomination
+            for (int coin : coins) {
+                // Check if using the current coin doesn't exceed the target amount
+                if (amt - coin >= 0) {
+                    // Update dp[amt] with the minimum number of coins needed
+                    dp[amt] = Math.min(dp[amt], 1 + dp[amt - coin]);
                 }
             }
         }
+
+        // If dp[amount] is still greater than amount + 1, 
+        // it means it wasn't updated, and there's no valid solution
+        // else return dp[amount]
         return dp[amount] != amount + 1 ? dp[amount] : -1;
     }
 }
