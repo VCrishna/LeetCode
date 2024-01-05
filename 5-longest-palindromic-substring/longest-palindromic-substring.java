6362e0dc2fd5984fb7359771d0955cb0c6fe1c26
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-05 01:22:24 -0500

    Time: 21 ms (59.53%) | Memory: 45.3 MB (19.77%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,39 +1,45 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        String result = "";
-        int resultLength = 0;
 
-        for(int i = 0; i < s.length(); i++) {
-            // odd length
+    public String longestPalindrome(String s) {
+        String result = ""; // Variable to store the longest palindrome substring
+        int resultLength = 0; // Variable to store the length of the longest palindrome substring
+
+        for (int i = 0; i < s.length(); i++) {
+            // Odd length palindrome
             int left = i;
             int right = i;
 
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-                if(right - left + 1 > resultLength) {
+            // Expandinging around the center (i) for odd length palindromes.
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                // If the current palindrome is longer than the previous result, update the result.
+                if (right - left + 1 > resultLength) {
                     result = s.substring(left, right + 1);
                     resultLength = right - left + 1;
                 }
 
+                // Expanding the palindrome by moving the pointers outward.
                 left--;
                 right++;
             }
 
-            // even length
+            // Even length palindrome
             left = i;
             right = i + 1;
 
-            while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-                if(right - left + 1 > resultLength) {
+            // Expanding around the center (i, i+1) for even length palindromes.
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+                // If the current palindrome is longer than the previous result, update the result.
+                if (right - left + 1 > resultLength) {
                     result = s.substring(left, right + 1);
                     resultLength = right - left + 1;
                 }
 
+                // Expanding the palindrome by moving the pointers outward.
                 left--;
                 right++;
             }
         }
 
-
-        return result;
+        return result; // Return the longest palindrome substring.
     }
 }
