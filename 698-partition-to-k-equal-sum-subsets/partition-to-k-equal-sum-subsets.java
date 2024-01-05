48a6c164b05a3c3c083fbb9b551935b0760524bd
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-04 21:53:20 -0500

    Time: 1567 ms (22.55%) | Memory: 40.9 MB (43.15%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.java
@@ -1,77 +1,56 @@
 class Solution {
 
     public boolean canPartitionKSubsets(int[] nums, int k) {
+        // Calculating the total sum of the array elements
         int totalSum = 0;
         for (int i : nums) {
             totalSum += i;
         }
+
+        // If the total sum is not divisible evenly by k, return false
         if (totalSum % k != 0) {
-            return false;
+            return false; // Not possible to distribute elements into k subsets with equal sums
         }
+
+        // Calculating the target sum that each subset should have
         int targetSum = totalSum / k;
+
+        // Creating an array to keep track of whether an element has been visited or not
+        // as we don't want to use same element twice
         boolean[] visited = new boolean[nums.length];
 
+        // Starting the backtracking process to find k subsets with equal sums
         return backtrack(nums, k, 0, 0, targetSum, visited);
     }
 
     public boolean backtrack(int[] nums, int k, int currentIndex, int currentSubSetSum, int targetSum, boolean[] visited) {
+        // If k becomes 0, it means all subsets have been formed, and the function returns true
         if (k == 0) {
-            return true;
+            return true; // Valid partitioning found
         }
+
+        // If the current subset sum equals the target sum, initiate the next subset by making a recursive call
         if (currentSubSetSum == targetSum) {
             return backtrack(nums, k - 1, 0, 0, targetSum, visited);
         }
 
+        // Iterate through the array from the current index and try to include each element in the current subset
         for (int i = currentIndex; i < nums.length; i++) {
+            // Check if the element hasn't been visited and adding it to the current subset won't exceed the target sum
             if (!visited[i] && currentSubSetSum + nums[i] <= targetSum) {
+                // Mark the element as visited
                 visited[i] = true;
+
+                // Make a recursive call to explore further possibilities
                 if (backtrack(nums, k, i + 1, currentSubSetSum + nums[i], targetSum, visited)) {
-                    return true;
+                    return true; // If a valid partitioning is found, return true
                 }
+
+                // Backtrack by marking the current element as not visited
                 visited[i] = false;
             }
         }
-        return false;
+
+        return false; // If no valid partitioning is found, return false
     }
 }
-/**
-class Solution {
-    public boolean canPartitionKSubsets(int[] nums, int k) {
-         int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
-        }
-        
-        if (totalSum % k != 0) {
-            return false;
-        }
-        
-        int targetSum = totalSum / k;
-        boolean[] visited = new boolean[nums.length];
-
-        return backtrack(nums, k, 0, 0, targetSum, visited);
-    }
-
-    public boolean backtrack(int[] nums, int k, int startIndex, int currentSum, int targetSum, boolean[] visited) {
-        if (k == 0) {
-            return true; // All subsets have been formed
-        }
-        
-        if (currentSum == targetSum) {
-            return backtrack(nums, k - 1, 0, 0, targetSum, visited);
-        }
-        
-        for (int i = startIndex; i < nums.length; i++) {
-            if (!visited[i] && currentSum + nums[i] <= targetSum) {
-                visited[i] = true;
-                if (backtrack(nums, k, i + 1, currentSum + nums[i], targetSum, visited)) {
-                    return true;
-                }
-                visited[i] = false;
-            }
-        }
-        
-        return false;
-    }
-}
- */
