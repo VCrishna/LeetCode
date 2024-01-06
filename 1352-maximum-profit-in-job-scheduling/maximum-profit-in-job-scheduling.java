b8574eb436c71e4f207a09203365595876e4421f
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-06 01:37:09 -0500

    Time: 81 ms (27.26%) | Memory: 56.2 MB (42.70%) - LeetSync

---
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+        int[][] jobs = new int[startTime.length][3];
+        for (int i = 0; i < startTime.length; i++) {
+            jobs[i] = new int[] { startTime[i], endTime[i], profit[i] };
+        }
+        Arrays.sort(jobs, (a, b) -> a[1] - b[1]);
+        TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+        int maxProfit = 0;
+        for (int[] job : jobs) {
+            Integer entryTillStartTime = treeMap.floorKey(job[0]);
+            int maxProfitTillStartTime = entryTillStartTime == null ? 0 : treeMap.get(entryTillStartTime);
+            maxProfit = Math.max(maxProfit, maxProfitTillStartTime + job[2]);
+            treeMap.put(job[1], maxProfit);
+        }
+
+        return maxProfit;
+    }
+}
