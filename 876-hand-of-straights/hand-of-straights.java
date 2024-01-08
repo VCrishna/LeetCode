d2e806e4b96b1f01ff61618b60db6f5b59a565a4
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-08 14:15:01 -0500

    Time: 64 ms (18.89%) | Memory: 45.6 MB (18.74%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,27 +1,39 @@
 class Solution {
+
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if(hand.length % groupSize != 0) {
+        // If the total number of cards is not divisible by the groupSize, it's not possible.
+        if (hand.length % groupSize != 0) {
             return false;
         }
+
+        // TreeMap to store the count of each unique card value
         TreeMap<Integer, Integer> map = new TreeMap<>();
-        for(int i : hand) {
-            map.put(i, map.getOrDefault(i, 0) + 1);
+        for (int i = 0; i < hand.length; i++) {
+            map.put(hand[i], map.getOrDefault(hand[i], 0) + 1);
         }
+        // Process until TreeMap is empty
+        while (map.size() > 0) {
+            // Find the smallest value in the TreeMap
+            int minValue = map.firstKey();
 
-        while(map.size() > 0) {
-            int minHand = map.firstKey();
-
-            for(int i = minHand; i < minHand + groupSize; i++){
-                if(!map.containsKey(i)) {
+            // Iterate over groupSize consecutive cards starting from minValue
+            for (int card = minValue; card < minValue + groupSize; card++) {
+                // Check if the current card exists in the TreeMap
+                if (!map.containsKey(card)) {
                     return false;
                 }
-                map.put(i, map.getOrDefault(i, 0) - 1);
-                if(map.get(i) == 0) {
-                    map.remove(i);
+
+                // Update the count for the current card in the TreeMap
+                map.put(card, map.get(card) - 1);
+
+                // Remove the card from the TreeMap if the count becomes zero
+                if (map.get(card) == 0) {
+                    map.remove(card);
                 }
             }
         }
-
         return true;
     }
 }
+// hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+// hand = [1,2,2,3,3,4,6,7,8]
