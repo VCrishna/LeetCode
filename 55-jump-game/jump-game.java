924cf462e857214dc25ebf388bb2f8b8d57d8c8c
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-08 18:17:50 -0500

    Time: 1 ms (99.99%) | Memory: 45.4 MB (32.15%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,12 +1,21 @@
 class Solution {
+
     public boolean canJump(int[] nums) {
+        // Initialize the goal as the last index of the array
         int goal = nums.length - 1;
 
-        for(int i = nums.length - 2; i >= 0; i--) {
+        // Iterate backward through the array starting 
+        // from the second-to-last index
+        for (int i = nums.length - 2; i >= 0; i--) {
+            // Check if the current index plus its value 
+            // can reach the current goal
             if (nums[i] + i >= goal) {
+                // If it can, update the goal to the current index
                 goal = i;
             }
         }
+
+        // Check if the goal is at the start of the array (index 0)
         return goal == 0;
     }
 }
