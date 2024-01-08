d3d8bef723d98ba8af3c88e8ced1294ac4ed5314
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-08 00:49:42 -0500

    Time: 65 ms (17.69%) | Memory: 44.8 MB (55.77%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -8,12 +8,25 @@ class Solution {
         for (int i = 0; i < hand.length; i++) {
             map.put(hand[i], map.getOrDefault(hand[i], 0) + 1);
         }
+        // Process until TreeMap is empty
         while (map.size() > 0) {
+            // Find the smallest value in the TreeMap
             int minValue = map.firstKey();
+
+            // Iterate over groupSize consecutive cards starting from minValue
             for (int card = minValue; card < minValue + groupSize; card++) {
-                if (!map.containsKey(card)) return false;
+                // Check if the current card exists in the TreeMap
+                if (!map.containsKey(card)) {
+                    return false;
+                }
+
+                // Update the count for the current card in the TreeMap
                 map.put(card, map.get(card) - 1);
-                if (map.get(card) == 0) map.remove(card);
+
+                // Remove the card from the TreeMap if the count becomes zero
+                if (map.get(card) == 0) {
+                    map.remove(card);
+                }
             }
         }
         return true;
