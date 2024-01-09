12b7bf83b3f129c7e52908261e14605d63064807
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-09 12:03:19 -0500

    Time: 0 ms (100.00%) | Memory: 42.2 MB (11.13%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,39 +1,25 @@
 class Solution {
+
     public void rotate(int[][] matrix) {
         int rows = matrix.length;
         int columns = matrix[0].length;
+
+        // Transpose Matrix
         for (int row = 0; row < rows; row++) {
-            for (int column = row; column < rows; column++) {
+            for (int column = row; column < columns; column++) {
                 int temp = matrix[row][column];
                 matrix[row][column] = matrix[column][row];
                 matrix[column][row] = temp;
             }
         }
+
+        // swapping with two pointers
         for (int row = 0; row < rows; row++) {
-            for (int column = 0; column < rows / 2; column++) {
+            for (int column = 0; column < columns / 2; column++) {
                 int temp = matrix[row][column];
-                matrix[row][column] = matrix[row][rows - 1 - column];
-                matrix[row][rows - 1 - column] = temp;
+                matrix[row][column] = matrix[row][columns - column - 1];
+                matrix[row][columns - column - 1] = temp;
             }
         }
     }
 }
-// class Solution {
-//     public void rotate(int[][] matrix) {
-//         if(matrix.length == 0 || matrix.length != matrix[0].length)
-//             return;
-//         int rows=matrix.length;
-//         for(int layer=0;layer<rows/2; layer++){
-//             int first=layer;
-//             int last = rows - 1 - layer;
-//             for(int i=first;i<last;i++){
-//                 int temp = i-first;
-//                 int top=matrix[first][i];
-//                 matrix[first][i] = matrix[last - temp][first];
-//                 matrix[last - temp][first] = matrix[last][last - temp];
-//                 matrix[last][last - temp] = matrix[i][last];
-//                 matrix[i][last]=top;
-//             }
-//         }
-//     }
-// }
