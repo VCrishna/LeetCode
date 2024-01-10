41e8e09ba16b94858018b1928adb7aed697a0847
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-10 18:41:00 -0500

    Time: 3 ms (84.34%) | Memory: 42.2 MB (68.36%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-
         int sLength = s.length();
-        boolean[] memo = new boolean[sLength + 1];
-        memo[sLength] = true;
+        boolean[] dp = new boolean[sLength + 1];
+        dp[sLength] = true;
 
-        for (int i = sLength - 1; i >= 0; i--) {
+        for(int i = sLength - 1; i >= 0; i--) {
             for (String word : wordDict) {
-                int wordLen = word.length();
-                // Check if the current substring equals the word
-                if (i + wordLen <= sLength && s.substring(i, i + wordLen).equals(word)) {
-                    memo[i] = memo[i + wordLen];
+                int wordLength = word.length();
+                if(
+                    i + wordLength <= sLength && s.substring(i, i + wordLength).equals(word)
+                ) {
+                    dp[i] = dp[i + wordLength];
                 }
-                // Break the inner loop if memo[i] is already true
-                if (memo[i]) {
+                if(dp[i]) {
                     break;
                 }
             }
         }
-        return memo[0];
+
+
+        return dp[0];
     }
 }
