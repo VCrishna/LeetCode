5025eb87bbe9d5ee0551bb429d67b30a8e6f8340
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-10 18:58:13 -0500

    Time: 1 ms (99.34%) | Memory: 45.1 MB (30.32%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -3,15 +3,18 @@ class Solution {
         int result = 0;
         int left = 0;
         int right = 0;
+        // Contining until the right boundary reaches the last index
         while (right < nums.length - 1) {
             int farthest = 0;
-            for(int i = left; i < right + 1; i++) {
+
+            // Iterate through the current jump range to find the farthest reachable index
+            for (int i = left; i < right + 1; i++) {
                 farthest = Math.max(farthest, i + nums[i]);
             }
-            left = right + 1;
-            right = farthest;
-            result++;
 
+            left = right + 1; // Move the left boundary to the right boundary of the current jump
+            right = farthest; // Update the right boundary with the farthest reachable index
+            result++; // Increment the result (number of jumps)
         }
         return result;
     }
