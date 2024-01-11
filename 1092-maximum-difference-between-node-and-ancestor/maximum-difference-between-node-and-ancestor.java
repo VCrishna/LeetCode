341dc4513e04033ac56fe415073d876fba46d2c8
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-11 17:44:53 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (17.52%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -1,55 +1,34 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
-    int result = 0;
-
+    int result;
     public int maxAncestorDiff(TreeNode root) {
-        // dfs (root, min, max)
+        result = 0;
+        // dfs - root, min, max
         dfs(root, root.val, root.val);
         return result;
-        // return findMaxDifference(root, root.val, root.val);
     }
-
     public void dfs(TreeNode root, int min, int max) {
-        if (root == null) {
+        if(root == null) {
             return;
         }
-        // result
-        result = Math.max(result, Math.max(Math.abs(min - root.val), Math.abs(max - root.val)));
-        // update min and max for the child nodes
+        result = Math.max(result, Math.max(Math.abs(root.val - min), Math.abs(root.val - max)));
         min = Math.min(min, root.val);
         max = Math.max(max, root.val);
         dfs(root.left, min, max);
         dfs(root.right, min, max);
     }
-
-    public int findMaxDifference(TreeNode root, int min, int max) {
-        if (root == null) {
-            return 0;
-        }
-
-        min = Math.min(min, root.val);
-        max = Math.max(max, root.val);
-
-        int diff = Math.abs(max - min);
-
-        int left = findMaxDifference(root.left, min, max);
-        int right = findMaxDifference(root.right, min, max);
-
-        return Math.max(diff, Math.max(left, right));
-    }
-
 }
