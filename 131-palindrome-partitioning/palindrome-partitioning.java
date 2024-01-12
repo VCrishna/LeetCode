9207c7ad8ed480eecc4fd2e0d805d93d33b617eb
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-12 00:45:32 -0500

    Time: 7 ms (78.97%) | Memory: 56.8 MB (29.21%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -7,24 +7,30 @@ class Solution {
         return result;
     }
 
+    // Depth-First Search method
     public void dfs(
-        String s,
-        List<List<String>> result,
-        List<String> partition,
-        int index
-    ) {
-        if(index >= s.length()) {
+            String s,
+            List<List<String>> result,
+            List<String> partition,
+            int index) {
+        // If the current index exceeds the string length, 
+        // add the current partition to the result
+        if (index >= s.length()) {
             result.add(new ArrayList<>(partition));
             return;
         }
-        for(int i = index; i < s.length(); i++) {
-            if(isPalindrome(s, index, i)) {
+        // Iterating through the string starting from the current index
+        for (int i = index; i < s.length(); i++) {
+            // Checking if the substring from the current index to i is a palindrome
+            if (isPalindrome(s, index, i)) {
+                // Adding the palindrome substring to the partition list
                 partition.add(s.substring(index, i + 1));
+                // Recursively calling dfs with the next index
                 dfs(s, result, partition, i + 1);
+                // Backtrack: Removing the last added substring to explore other possibilities
                 partition.remove(partition.size() - 1);
             }
         }
-
     }
 
     public boolean isPalindrome(String s, int left, int right) {
