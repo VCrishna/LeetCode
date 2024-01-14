6aa6bb41365916b513fce360fb27ce9f6c8e2b1f
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-14 14:34:00 -0500

    Time: 0 ms (100.00%) | Memory: 42.6 MB (6.31%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,5 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Making a copy of the first m elements of nums1.
+        int[] nums1Copy = new int[m];
+        for (int i = 0; i < m; i++) {
+            nums1Copy[i] = nums1[i];
+        }
+        int p1 = 0;
+        int p2 = 0;
+
+        // Comparing elements from nums1Copy and nums2 and write the smallest to nums1.
+        for (int p = 0; p < m + n; p++) {
+            // We also need to ensure that p1 and p2 aren't over the boundaries
+            // of their respective arrays.
+            if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
+                nums1[p] = nums1Copy[p1++];
+            } else {
+                nums1[p] = nums2[p2++];
+            }
+        }
+    }
+
+    public void merge_BRUTE_FORCE(int[] nums1, int m, int[] nums2, int n) {
         for (int i = 0; i < n; i++) {
             nums1[i + m] = nums2[i];
         }
