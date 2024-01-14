51fd7d2199a66a79b52b81a5ad3558a52ae3a502
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-14 16:30:09 -0500

    Time: 9 ms (38.38%) | Memory: 54.9 MB (70.55%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,30 +1,40 @@
 class Solution {
     public boolean canReach(int[] arr, int start) {
         int n = arr.length;
+        
+        // Array to track visited indices
         boolean[] visited = new boolean[n];
-
+        
         Queue<Integer> queue = new LinkedList<>();
         queue.add(start);
 
+        // Performing BFS traversal
         while (!queue.isEmpty()) {
             int current = queue.poll();
+
+            // Marking the current index as visited
             visited[current] = true;
 
+            // If the value at the current index is zero, returning true
             if (arr[current] == 0) {
                 return true;
             }
 
+            // Calculating the indices to the left and right based on the current index and array value
             int left = current - arr[current];
             int right = current + arr[current];
 
+            // Enqueue the left index if it is valid and not visited
             if (left >= 0 && !visited[left]) {
                 queue.add(left);
             }
+            
+            // Enqueue the right index if it is valid and not visited
             if (right < n && !visited[right]) {
                 queue.add(right);
             }
         }
-
+        // If no zero is reached during BFS traversal, returning false
         return false;
     }
 }
