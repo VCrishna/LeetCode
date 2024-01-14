0be074fb7de175b33029563624772a8048f87dfb
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-14 14:13:48 -0500

    Time: 0 ms (100.00%) | Memory: 42.1 MB (47.91%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,9 +10,15 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // Creating a dummy node as the head of the merged list
         ListNode mergedHead = new ListNode(0);
+        // Creating a temporary pointer to traverse and build the merged list
         ListNode temp = mergedHead;
+
+        // Continuing the loop until either list1 or list2 becomes null
         while (list1 != null && list2 != null) {
+            // Comparing values of nodes in list1 and list2, 
+            // and link the smaller value node to the merged list
             if (list1.val < list2.val) {
                 temp.next = list1;
                 list1 = list1.next;
@@ -20,16 +26,23 @@ class Solution {
                 temp.next = list2;
                 list2 = list2.next;
             }
+            // Moving the temporary pointer to the last node in the merged list
             temp = temp.next;
         }
+
+        // Checking if there are remaining nodes in list1 and link them to the merged list
         if (list1 != null) {
             temp.next = list1;
             list1 = list1.next;
         }
+
+        // Checking if there are remaining nodes in list2 and link them to the merged list
         if (list2 != null) {
             temp.next = list2;
             list2 = list2.next;
         }
+
+        // Returning the head of the merged list (skip the dummy node)
         return mergedHead.next;
     }
 }
