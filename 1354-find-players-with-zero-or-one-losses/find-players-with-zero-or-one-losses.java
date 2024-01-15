09dd58f23cf5086020b93c3bbdc2c42e99a7407b
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-15 10:05:09 -0500

    Time: 92 ms (39.47%) | Memory: 102.7 MB (35.97%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.java
@@ -1,29 +1,24 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
         Set<Integer> players = new HashSet<>();
-        Map<Integer, Integer> winnerMap = new HashMap<>();
         Map<Integer, Integer> loserMap = new HashMap<>();
         for (int[] match : matches) {
             players.add(match[0]);
             players.add(match[1]);
-            winnerMap.put(match[0], winnerMap.getOrDefault(match[0], 0) + 1);
             loserMap.put(match[1], loserMap.getOrDefault(match[1], 0) + 1);
         }
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = new ArrayList<>();
         for(int x : players) {
-            if(!loserMap.containsKey(x) && winnerMap.get(x) > 0) {
+            if(!loserMap.containsKey(x)) {
                 list1.add(x);
             }
             else if(loserMap.get(x) == 1) {
                 list2.add(x);
             }
         }
-        // winnerMap.forEach((x, y) -> System.out.println(x + " " + y));
-        // loserMap.forEach((x, y) -> System.out.println(x + " " + y));
         Collections.sort(list1);
         Collections.sort(list2);
         return Arrays.asList(list1, list2);
     }
 }
-// [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
