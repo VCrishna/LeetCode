c93196327314138ba9ce86c94d2b1fb16e6440ce
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-15 15:01:53 -0500

    Time: 0 ms (100.00%) | Memory: 41.4 MB (24.94%) - LeetSync

--- 1119-robot-bounded-in-circle/robot-bounded-in-circle.java
+++ 1119-robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -1,26 +1,33 @@
 class Solution {
     public boolean isRobotBounded(String instructions) {
+        // Initial position of the robot
         int x = 0;
         int y = 0;
+        
+        // Initial direction of the robot (facing upward)
         int dirX = 0;
         int dirY = 1;
 
+        // Iterating through the instructions
         for (char direction : instructions.toCharArray()) {
             if (direction == 'G') {
+                // Moving forward based on the current direction
                 x = x + dirX;
                 y = y + dirY;
             } else if (direction == 'L') {
+                // Turning left by swapping and negating direction components
                 int temp = dirX;
-                dirX = -1 * dirY;
+                dirX = -dirY;
                 dirY = temp;
             } else {
+                // Turning right by swapping and negating direction components
                 int temp = dirX;
                 dirX = dirY;
-                dirY = -1 * temp;
+                dirY = -temp;
             }
         }
 
+        // Checking if the robot is back to the starting position or not facing upward
         return (x == 0 && y == 0) || (dirX != 0 || dirY != 1);
     }
-
 }
