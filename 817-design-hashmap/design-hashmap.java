4441280150b1c5568fe0f521ddd0e38ea4ee7f48
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-15 12:15:17 -0500

    Time: 31 ms (45.81%) | Memory: 54.9 MB (18.71%) - LeetSync

--- 817-design-hashmap/design-hashmap.java
+++ 817-design-hashmap/design-hashmap.java
@@ -1,19 +1,20 @@
 class MyHashMap {
-    int map[];
+    int[] map;
     public MyHashMap() {
-        map = new int[10000000];
+        map = new int[1000001];
+        Arrays.fill(map,-1);
     }
-
+    
     public void put(int key, int value) {
-        map[key] = value + 1;
+        map[key] = value;
     }
-
+    
     public int get(int key) {
-        return map[key] - 1;
+        return map[key];
     }
-
+    
     public void remove(int key) {
-        map[key] = 0;
+        map[key] = -1;
     }
 }
 
