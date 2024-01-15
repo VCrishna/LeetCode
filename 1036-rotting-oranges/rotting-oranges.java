be42679109ee2ee195f87f2dc904b68997b50589
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-15 18:55:00 -0500

    Time: 15 ms (5.13%) | Memory: 45.3 MB (5.39%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,9 +1,11 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-
+        // Set to store coordinates of fresh oranges
         Set<String> fresh = new HashSet<>();
-        Set<String> rotten = new HashSet<>();
+        // Set to store coordinates of rotten oranges
+        Set<String> rotten = new HashSet();
 
+        // Populating fresh and rotten sets based on the initial state of the grid
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 2) {
@@ -14,10 +16,17 @@ class Solution {
             }
         }
 
+        // Variable to keep track of minutes elapsed
         int minutes = 0;
+        // Array to represent possible directions (up, down, left, right)
         int[][] directions = { { 0, 1 }, { 1, 0 }, { -1, 0 }, { 0, -1 } };
+
+        // Continuing the process until there are fresh oranges
         while (fresh.size() > 0) {
+            // Set to store newly infected oranges in the current minute
             Set<String> infected = new HashSet<>();
+
+            // Iterating through each rotten orange and try to infect adjacent fresh oranges
             for (String s : rotten) {
                 String[] indices = s.split(",");
                 int i = Integer.parseInt(indices[0]);
@@ -26,18 +35,27 @@ class Solution {
                 for (int[] direction : directions) {
                     int nextI = i + direction[0];
                     int nextJ = j + direction[1];
+
+                    // If the adjacent cell has a fresh orange, mark it as infected
                     if (fresh.contains(nextI + "," + nextJ)) {
-                        fresh.remove(nextI+ "," + nextJ);
-                        infected.add(nextI+ "," + nextJ);
+                        fresh.remove(nextI + "," + nextJ);
+                        infected.add(nextI + "," + nextJ);
                     }
                 }
             }
+
+            // If no new oranges are infected, it means there are isolated fresh oranges
             if (infected.size() == 0) {
                 return -1;
             }
+
+            // Update the set of rotten oranges for the next minute
             rotten = infected;
+            // Increment the minute counter
             minutes++;
         }
+
+        // Return the total minutes required to infect all fresh oranges
         return minutes;
     }
 }
