15837028caa1d9c818ef9fc07a4bc29fb1054809
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-15 22:08:11 -0500

    Time: 3 ms (89.82%) | Memory: 42.7 MB (73.55%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -2,25 +2,25 @@
 // Memory Usage: 42.5 MB, less than 92.71% of Java online submissions for Intersection of Two Arrays II.
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        // Sort both the arrays first...
+        // Sort both the arrays first
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        // Create an array list...
+        // Create an array list
         ArrayList<Integer> arr = new ArrayList<Integer>();
         // Use two pointers i and j for the two arrays and initialize both with zero.
         int i = 0, j = 0;
         while (i < nums1.length && j < nums2.length) {
-            // If nums1[i] is less than nums2[j]...
-            // Leave the smaller element and go to next(greater) element in nums1...
+            // If nums1[i] is less than nums2[j]
+            // Leave the smaller element and go to next(greater) element in nums1
             if (nums1[i] < nums2[j]) {
                 i++;
             }
-            // If nums1[i] is greater than nums2[j]...
-            // Go to next(greater) element in nums2 array...
+            // If nums1[i] is greater than nums2[j]
+            // Go to next(greater) element in nums2 array
             else if (nums1[i] > nums2[j]) {
                 j++;
             }
-            // If both the elements intersected...
+            // If both the elements intersected
             // Add this element to arr & increment both i and j.
             else {
                 arr.add(nums1[i]);
@@ -28,7 +28,7 @@ class Solution {
                 j++;
             }
         }
-        // Create a output list to store the output...
+        // Create a output list to store the output
         int[] output = new int[arr.size()];
         int k = 0;
         while (k < arr.size()) {
