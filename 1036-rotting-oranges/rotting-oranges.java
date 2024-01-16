adb8e1ef9899d1cd64d1007fe2ce962c96aefda3
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-15 19:05:33 -0500

    Time: 14 ms (5.13%) | Memory: 45 MB (5.39%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,4 +1,66 @@
 class Solution {
+    public int orangesRotting_QUEUE_DFS(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        // Queue to store the coordinates of rotten oranges
+        Queue<int[]> queue = new LinkedList<>();
+
+        // Variable to count the number of fresh oranges
+        int fresh = 0;
+
+        // Iterating through the grid to populate the queue and count fresh oranges
+        for (int i = 0; i < m; i += 1) {
+            for (int j = 0; j < n; j += 1) {
+                if (grid[i][j] == 2) {
+                    // Add coordinates of rotten oranges to the queue
+                    queue.offer(new int[] { i, j });
+
+                }
+                else if (grid[i][j] == 1) {
+                    // Count fresh oranges
+                    fresh += 1;
+                }
+            }
+        }
+
+        // Variable to keep track of minutes
+        int count = 0;
+
+        // Array to represent possible directions (up, down, left, right)
+        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+        // Continuing the process until the queue is empty 
+        // or there are no more fresh oranges
+        while (!queue.isEmpty() && fresh != 0) {
+            // Increment the minute counter
+            count += 1;
+            int sz = queue.size();
+
+            // Processing each rotten orange in the current minute
+            for (int i = 0; i < sz; i += 1) {
+                // Retrieving the coordinates of a rotten orange
+                int[] rotten = queue.poll();
+                int r = rotten[0], c = rotten[1];
+
+                // Exploring adjacent cells (up, down, left, right) of the current rotten orange
+                for (int[] dir : dirs) {
+                    int x = r + dir[0], y = c + dir[1];
+
+                    // Check if the adjacent cell is within the grid boundaries and 
+                    // contains a fresh orange
+                    if (0 <= x && x < m && 0 <= y && y < n && grid[x][y] == 1) {
+                        grid[x][y] = 2; // Mark the fresh orange as rotten
+                        queue.offer(new int[] { x, y }); // Add the coordinates to the queue
+                        fresh -= 1; // Decrement the count of fresh oranges
+                    }
+                }
+            }
+        }
+
+        // Return the total minutes required to infect all fresh oranges or -1 if there
+        // are isolated fresh oranges
+        return fresh == 0 ? count : -1;
+    }
+
     public int orangesRotting(int[][] grid) {
         // Set to store coordinates of fresh oranges
         Set<String> fresh = new HashSet<>();
