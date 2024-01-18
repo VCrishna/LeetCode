22a35c542d2357aaef7f16e8837525128eef7d13
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-18 13:23:23 -0500

    Time: 2 ms (87.98%) | Memory: 48.1 MB (38.13%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -17,44 +17,60 @@ class Solution {
         ListNode firstHalf = head;
         ListNode secondHalf = head;
 
-        // Move secondHalf to the middle of the list
+        // Moving secondHalf to the middle of the list
         while (secondHalf != null && secondHalf.next != null) {
             firstHalf = firstHalf.next;
             secondHalf = secondHalf.next.next;
         }
 
-        // Reverse the secondHalf
+        // Reversing the secondHalf
         ListNode reversedSecondHalf = reverse(firstHalf.next);
 
-        // Break the list after the firstHalf
+        // Breaking the list after the firstHalf
         firstHalf.next = null;
 
-        // Merge the firstHalf and reversedSecondHalf
+        // Merging the firstHalf and reversedSecondHalf
         merge(head, reversedSecondHalf);
     }
 
     public ListNode reverse(ListNode head) {
+        // Initializing two pointers: 'prev' to track the reversed list and
+        // 'current' to traverse the original list
         ListNode prev = null;
         ListNode current = head;
+
+        // Traversing the original list
         while (current != null) {
+            // Saving the next node in the original list
             ListNode next = current.next;
+
+            // Reversing the link by pointing 'current.next' to the previous node
             current.next = prev;
+
+            // Moving 'prev' and 'current' pointers to the next nodes
             prev = current;
             current = next;
         }
+
+        // 'prev' now points to the head of the reversed list
         return prev;
     }
 
     public void merge(ListNode firstHalf, ListNode reversedSecondHalf) {
+        // Merging the two halves by alternating nodes from each half
         while (firstHalf != null && reversedSecondHalf != null) {
+            // Saving the next nodes in both halves
             ListNode firstHalfNext = firstHalf.next;
             ListNode reversedSecondHalfNext = reversedSecondHalf.next;
 
+            // Updating the links to connect nodes from the two halves
             firstHalf.next = reversedSecondHalf;
             firstHalf = firstHalfNext;
 
+            // Moving to the next nodes in both halves
             reversedSecondHalf.next = firstHalfNext;
             reversedSecondHalf = reversedSecondHalfNext;
         }
     }
+
 }
