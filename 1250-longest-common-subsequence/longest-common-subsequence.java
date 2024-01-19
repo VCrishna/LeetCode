0a158cca47313711e56eec350b13afd00b8fe0be
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-18 23:54:58 -0500

    Time: 19 ms (91.12%) | Memory: 50.5 MB (50.14%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -13,13 +13,31 @@ class Solution {
                     // Characters match, extend the common subsequence
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    // Characters don't match, choose the maximum length from the previous subproblems
+                    // Characters don't match, choose the maximum length from the previous
+                    // subproblems
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        // The bottom-right cell of the dp array contains the length of the longest common subsequence
+        // The bottom-right cell of the dp array contains the length of the longest
+        // common subsequence
         return dp[m][n];
+    }
+
+    public int longestCommonSubsequence_BOTTOM_UP(String text1, String text2) {
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+
+        for (int i = text1.length() - 1; i >= 0; i--) {
+            for (int j = text2.length() - 1; j >= 0; j--) {
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 }
