c639c78ddb0ce2bd59974ac019d1ab07a89da99e
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-20 12:49:07 -0500

    Time: 0 ms (100.00%) | Memory: 45.5 MB (32.38%) - LeetSync

--- 280-wiggle-sort/wiggle-sort.java
+++ 280-wiggle-sort/wiggle-sort.java
@@ -1,14 +1,21 @@
 class Solution {
     public void wiggleSort(int[] nums) {
-        for(int i = 1; i < nums.length; i++) {
-            if(i % 2 == 1 && nums[i] < nums[i - 1]) {
-                // swap
+        // Iterate through the array starting from the second element
+        for (int i = 1; i < nums.length; i++) {
+            // Checking if the current index is odd and the 
+            // current value is less than the previous value
+            if (i % 2 == 1 && nums[i] < nums[i - 1]) {
+                // Swapping the current value with the previous value 
+                // to ensure values at odd indices are greater
                 int temp = nums[i];
                 nums[i] = nums[i - 1];
                 nums[i - 1] = temp;
             }
-            if(i % 2 == 0 && nums[i] > nums[i - 1]) {
-                // swap
+            // Checking if the current index is even and the 
+            // current value is greater than the previous value
+            if (i % 2 == 0 && nums[i] > nums[i - 1]) {
+                // Swapping the current value with the previous value 
+                // to ensure values at even indices are smaller
                 int temp = nums[i];
                 nums[i] = nums[i - 1];
                 nums[i - 1] = temp;
