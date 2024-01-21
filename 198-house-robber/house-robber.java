a0cfa3b1640056c3ad99f74dbd2a90fc86eff7ae
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-20 19:39:33 -0500

    Time: 0 ms (100.00%) | Memory: 41.2 MB (17.21%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,5 @@
 class Solution {
-    public int rob(int[] nums) { 
-        int rob1 = 0;
-        int rob2 = 0;
-        int sum = 0;
-
-        for(int i = 0; i < nums.length; i++) {
-            int temp = Math.max(nums[i] + rob2, rob1);
-            rob2  = rob1;
-            rob1 = temp;
-        }
-
-        return rob1;
-    }
-    public int rob_EXTRA_SPACE(int[] nums) {
+    public int rob(int[] nums) {
         if (nums.length == 0 || nums == null) {
             return 0;
         }
@@ -20,17 +7,11 @@ class Solution {
             return nums[0];
         }
         int[] dp = new int[nums.length];
-        
-        // base cases for the first two houses
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < nums.length; i++) {
-            // For each house i, calculate the maximum amount that can be robbed:
-            // Either by robbing the current house and adding the maximum amount from two houses back (dp[i - 2]),
-            // or by skipping the current house and taking the maximum amount from the previous house (dp[i - 1])
-            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        for(int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i -1]);
         }
-        
         return dp[nums.length - 1];
     }
 }
