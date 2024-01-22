3ead2a97bd27a590ab737a48649dece1019d5b56
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-22 14:59:07 -0500

    Time: 4 ms (34.85%) | Memory: 45.9 MB (12.06%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,5 +1,19 @@
 public class Solution {
     public int[] findErrorNums(int[] nums) {
+        int duplicateNumber = -1, missingNumber = 1;
+        for (int n: nums) {
+            if (nums[Math.abs(n) - 1] < 0)
+                duplicateNumber = Math.abs(n);
+            else
+                nums[Math.abs(n) - 1] *= -1;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > 0)
+                missingNumber = i + 1;
+        }
+        return new int[]{duplicateNumber, missingNumber};
+    }
+    public int[] findErrorNums_MAP(int[] nums) {
         Map<Integer, Integer> map = new HashMap();
         int duplicate = -1, missingNumber = 1;
         for (int n : nums) {
