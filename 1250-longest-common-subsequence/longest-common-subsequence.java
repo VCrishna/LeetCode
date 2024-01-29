dfc0914ed184b942c1477b8f4dab01bbcdbc5acf
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-29 08:04:12 -0500

    Time: 20 ms (79.87%) | Memory: 51.1 MB (15.74%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,27 +1,19 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
+        int length1 = text1.length();
+        int length2 = text2.length();
 
-        // Create a 2D array to store the length of the longest common subsequence
-        int[][] dp = new int[m + 1][n + 1];
+        int[][] dpLCS = new int[length1 + 1][length2 + 1];
 
-        // Fill the dp array using dynamic programming
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= length1; i++) {
+            for (int j = 1; j <= length2; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    // Characters match, extend the common subsequence
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dpLCS[i][j] = 1 + dpLCS[i - 1][j - 1];
                 } else {
-                    // Characters don't match, choose the maximum length from the previous
-                    // subproblems
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dpLCS[i][j] = Math.max(dpLCS[i - 1][j], dpLCS[i][j - 1]);
                 }
             }
         }
-
-        // The bottom-right cell of the dp array contains the length of the longest
-        // common subsequence
-        return dp[m][n];
+        return dpLCS[length1][length2];
     }
 }
