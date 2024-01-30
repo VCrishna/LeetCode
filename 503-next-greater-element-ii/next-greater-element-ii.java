78ecc341ee25bfc6a3e683f050346808a04c5f4f
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-29 23:35:45 -0500

    Time: 10 ms (81.98%) | Memory: 46.8 MB (5.14%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,19 +1,33 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
+        // Initializing the result array with -1 for each element
         int[] result = new int[nums.length];
         Arrays.fill(result, -1);
 
+        // Creating a stack to track indices of elements 
+        // for which the next greater element is not found yet
         Stack<Integer> stack = new Stack();
-
+        
+        // Traversing the array twice to handle circular nature
         for (int i = 0; i < nums.length * 2; i++) {
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % nums.length]) {
-                result[stack.pop()] = nums[i % nums.length];
+            // Getting the actual element 
+            // considering the circular nature of the array
+            int num = nums[i % nums.length];
+            
+            // While the stack is not empty and the current element is 
+            // greater than the element at the top of the stack
+            while (!stack.isEmpty() && nums[stack.peek()] < num) {
+                // Updating the result array with the next greater element for the element at the top of the stack
+                result[stack.pop()] = num;
             }
+            
+            // If the current iteration is within the original array length, push the current index onto the stack
             if (i < nums.length) {
                 stack.push(i);
             }
         }
 
+        // Return the modified result array containing the next greater element for each element in the input array
         return result;
     }
 }
