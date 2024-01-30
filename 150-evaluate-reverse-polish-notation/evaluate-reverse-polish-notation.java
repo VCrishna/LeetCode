94a2436bc26caf91da2460bb2952f6d76ceb0d87
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-29 23:09:06 -0500

    Time: 5 ms (97.05%) | Memory: 44.6 MB (44.79%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,62 +1,30 @@
 class Solution {
-
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack();
 
-        for (String token : tokens) {
-            if (!"+-*/".contains(token)) {
-                stack.push(Integer.valueOf(token));
+        for(String token : tokens) {
+            if(!"+-*/".contains(token)) {
+                stack.push(Integer.parseInt(token));
                 continue;
             }
-
-            int number2 = stack.pop();
-            int number1 = stack.pop();
-
-            int result = 0;
-
-            switch (token) {
+            int num2 = stack.pop();
+            int num1 = stack.pop();
+            int sol = 0;
+            switch(token) {
                 case "+":
-                    result = number1 + number2;
+                    sol = num1 + num2;
                     break;
                 case "-":
-                    result = number1 - number2;
+                    sol = num1 - num2;
                     break;
                 case "*":
-                    result = number1 * number2;
+                    sol = num1 * num2;
                     break;
                 case "/":
-                    result = number1 / number2;
+                    sol = num1 / num2;
                     break;
             }
-
-            stack.push(result);
-        }
-
-        return stack.pop();
-    }
-
-    public int evalRPN_BRUTE_FORCE(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
-        for (String s : tokens) {
-            if (s.equals("+")) {
-                int num1 = stack.isEmpty() ? 0 : stack.pop();
-                int num2 = stack.isEmpty() ? 0 : stack.pop();
-                stack.push(num1 + num2);
-            } else if (s.equals("-")) {
-                int num1 = stack.isEmpty() ? 0 : stack.pop();
-                int num2 = stack.isEmpty() ? 0 : stack.pop();
-                stack.push(num2 - num1);
-            } else if (s.equals("*")) {
-                int num1 = stack.isEmpty() ? 0 : stack.pop();
-                int num2 = stack.isEmpty() ? 0 : stack.pop();
-                stack.push(num1 * num2);
-            } else if (s.equals("/")) {
-                int num1 = stack.isEmpty() ? 0 : stack.pop();
-                int num2 = stack.isEmpty() ? 0 : stack.pop();
-                stack.push(num2 / num1);
-            } else {
-                stack.push(Integer.parseInt(s));
-            }
+            stack.push(sol);
         }
         return stack.pop();
     }
