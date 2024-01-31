7ced3d46d07fbc02bdb241c36cc8f84c57a76f37
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-31 11:04:30 -0500

    Time: 62 ms (63.75%) | Memory: 62.4 MB (26.83%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,8 +1,10 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int[] result = new int[temperatures.length];
+
         Stack<Integer> stack = new Stack<>();
-        for (int currentDay = 0; currentDay < temperatures.length; currentDay++) {
+
+        for(int currentDay = 0; currentDay < temperatures.length; currentDay++) {
             while (!stack.isEmpty() && temperatures[currentDay] > temperatures[stack.peek()]) {
                 int previousDay = stack.pop();
                 result[previousDay] = currentDay - previousDay;
