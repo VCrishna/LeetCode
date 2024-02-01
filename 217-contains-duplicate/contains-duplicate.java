a40f5841e4d9609c49331a4851ec24ad737154c6
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-01 12:13:28 -0500

    Time: 7 ms (98.49%) | Memory: 61.2 MB (5.69%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,10 +1,24 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums); 
+    // time - O(n log n)
+    // space - O(1)
+    public boolean containsDuplicate_SORTING(int[] nums) {
+        Arrays.sort(nums);
         for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i+1]) {
-                    return true;
-                }
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // time - O(n)
+    // space - O(n)
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
         }
         return false;
     }
