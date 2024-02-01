9b8ffea25a837059ac298ec7dfb0277f858f3d85
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-31 19:20:22 -0500

    Time: 24 ms (68.69%) | Memory: 58.9 MB (21.37%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -1,21 +1,40 @@
 class Solution {
     public int[][] divideArray(int[] nums, int k) {
+        // Checking if the length of nums is divisible by 3
         if (nums.length % 3 != 0) {
+            // If not, returning an empty 2D array
             return new int[0][0];
         }
+
+        // Initializing a 2D array to store the result
         int[][] result = new int[nums.length / 3][3];
+
+        // Initializing an index to keep track of the 
+        // current position in the result array
         int index = 0;
+
+        // Sorting the nums array in ascending order
+        // to facilitate the division of the array
         Arrays.sort(nums);
+
+        // Iterating through nums array in steps of 3
         for (int i = 0; i < nums.length; i += 3) {
+            // Checking if the conditions for dividing the array are satisfied
             if (nums[i + 1] - nums[i] <= k && nums[i + 2] - nums[i] <= k) {
+                // If conditions are met, populating the current row in the result array
                 result[index][0] = nums[i];
                 result[index][1] = nums[i + 1];
                 result[index][2] = nums[i + 2];
+
+                // Incrementing the index for the next row in the result array
                 index++;
             } else {
+                // If conditions are not met, returning an empty 2D array
                 return new int[0][0];
             }
         }
+
+        // Returning the populated result array
         return result;
     }
 
