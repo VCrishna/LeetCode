fd0192df7dc4eb0d52fce1f9f41b5ff740db887f
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-01-31 19:17:49 -0500

    Time: 23 ms (93.20%) | Memory: 59.4 MB (10.03%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.java
@@ -4,6 +4,26 @@ class Solution {
             return new int[0][0];
         }
         int[][] result = new int[nums.length / 3][3];
+        int index = 0;
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i += 3) {
+            if (nums[i + 1] - nums[i] <= k && nums[i + 2] - nums[i] <= k) {
+                result[index][0] = nums[i];
+                result[index][1] = nums[i + 1];
+                result[index][2] = nums[i + 2];
+                index++;
+            } else {
+                return new int[0][0];
+            }
+        }
+        return result;
+    }
+
+    public int[][] divideArray_BRUTE_FORCE(int[] nums, int k) {
+        if (nums.length % 3 != 0) {
+            return new int[0][0];
+        }
+        int[][] result = new int[nums.length / 3][3];
         List<int[]> lst = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i += 3) {
@@ -14,7 +34,7 @@ class Solution {
             }
         }
         int index = 0;
-        for(int[] i : lst) {
+        for (int[] i : lst) {
             result[index++] = i;
         }
         return result;
