f0090b1e1d7b906c39e68755dd2246bec762e0ab
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-01 16:16:50 -0500

    Time: 31 ms (68.91%) | Memory: 51.2 MB (68.31%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,18 +1,19 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
+        
         Arrays.sort(nums);
 
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             // avoiding duplicates for first elemet
-            if(i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
                 int first = nums[i];
                 int left = i + 1;
                 int right = nums.length - 1;
 
-                while(left < right) {
+                while (left < right) {
                     int sum = nums[left] + nums[right];
-                    if(first + sum == 0) {
+                    if (first + sum == 0) {
                         result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                         // avoiding duplicates at left
                         while (left < right && nums[left] == nums[left + 1]) {
@@ -25,11 +26,9 @@ class Solution {
                         // regular increment of pointers
                         left++;
                         right--;
-                    }
-                    else if (first + sum > 0) {
+                    } else if (first + sum > 0) {
                         right--;
-                    }
-                    else {
+                    } else {
                         left++;
                     }
                 }
