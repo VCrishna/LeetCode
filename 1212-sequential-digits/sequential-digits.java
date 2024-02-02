dcf9c5bf8f16d69d1738565ec69577b5d4a482f8
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-01 19:53:25 -0500

    Time: 0 ms (100.00%) | Memory: 40.7 MB (66.49%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -1,18 +1,24 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
-        int[] allPossibleElement = {
-                12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456, 567, 678, 789, 1234,
-                2345, 3456, 4567, 5678, 6789, 12345, 23456, 34567, 45678, 56789, 123456,
-                234567, 345678, 456789, 1234567, 2345678, 3456789, 12345678, 23456789,
-                123456789
+        // Array with all possible sequential digits
+        int[] allPossibleSequences = {
+                12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456, 567, 678, 789,
+                1234, 2345, 3456, 4567, 5678, 6789, 12345, 23456, 34567, 45678,
+                56789, 123456, 234567, 345678, 456789, 1234567, 2345678, 3456789,
+                12345678, 23456789, 123456789
         };
 
         List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < allPossibleElement.length; i++) {
-            if (low <= allPossibleElement[i] && allPossibleElement[i] <= high) {
-                result.add(allPossibleElement[i]);
+
+        // Iterating through all possible sequential digits
+        for (int i = 0; i < allPossibleSequences.length; i++) {
+            // Checking if the current sequence is within the specified range [low, high]
+            if (low <= allPossibleSequences[i] && allPossibleSequences[i] <= high) {
+                // If within the range, adding it to the result list
+                result.add(allPossibleSequences[i]);
             }
         }
+
         return result;
     }
 }
