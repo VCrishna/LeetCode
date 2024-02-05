a54803da7f92184238adc0ac4ca959963cefd0af
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-04 21:17:36 -0500

    Time: 1 ms (100.00%) | Memory: 43.9 MB (96.05%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,5 +1,24 @@
 class Solution {
     public int firstUniqChar(String s) {
+        // Stores lowest index / first index
+        int result  = Integer.MAX_VALUE;
+        // Iterating from a to z which is 26 which makes it constant
+        for (char c = 'a'; c <= 'z'; c++) {
+            // indexOf will return first index of alphabet 
+            // lastIndexOf will return last index
+            // if both are equal then it has occured only once.
+            // through this we will get all index's which are occured once
+            // but our answer is lowest index
+            int index = s.indexOf(c);
+            if (index != -1 && index == s.lastIndexOf(c)) {
+                result  = Math.min(result , index);
+            }
+        }
+        // If result  remain's Integer.MAX_VALUE then their is no unique character
+        return result  == Integer.MAX_VALUE ? -1 : result;
+    }
+
+    public int firstUniqChar_INT_ARRAY(String s) {
         int countArray[] = new int[26];
         for (char c : s.toCharArray()) {
             countArray[c - 'a']++;
@@ -9,6 +28,20 @@ class Solution {
                 return i;
             }
         }
+        return -1;
+    }
+
+    public int firstUniqChar_MAP(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
         return -1;
     }
 }
