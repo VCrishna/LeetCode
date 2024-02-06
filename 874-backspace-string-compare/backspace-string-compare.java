e08ecc7bf52951190f9a194adb6933b22c391ccc
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-06 16:00:50 -0500

    Time: 0 ms (100.00%) | Memory: 41.5 MB (62.94%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,5 +1,42 @@
 class Solution {
-    public boolean backspaceCompare(String s, String t) {
+    public boolean backspaceCompare(String S, String T) {
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
+
+        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
+            while (i >= 0) { // Find position of next possible char in build(S)
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else
+                    break;
+            }
+            while (j >= 0) { // Find position of next possible char in build(T)
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else
+                    break;
+            }
+            // If two actual characters are different
+            if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
+                return false;
+            // If expecting to compare char vs nothing
+            if ((i >= 0) != (j >= 0))
+                return false;
+            i--;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean backspaceCompare_BRUTE_FORCE(String s, String t) {
         Stack<Character> st = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '#') {
