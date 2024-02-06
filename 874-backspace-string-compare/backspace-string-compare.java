1f20de9b101e3a22ffdc4a72fb90113f7e748d0c
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-06 16:19:02 -0500

    Time: 0 ms (100.00%) | Memory: 41.5 MB (51.29%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,38 +1,64 @@
 class Solution {
     public boolean backspaceCompare(String S, String T) {
+        // Initialize pointers for traversing the strings from the end
         int i = S.length() - 1, j = T.length() - 1;
+        // Variables to track the number of backspaces encountered in each string
         int skipS = 0, skipT = 0;
 
-        while (i >= 0 || j >= 0) { // While there may be chars in build(S) or build (T)
-            while (i >= 0) { // Find position of next possible char in build(S)
+        // Iterate until there are characters left in either string
+        while (i >= 0 || j >= 0) {
+            // Find the position of the next possible character in S after skipping  backspaces
+            while (i >= 0) {
                 if (S.charAt(i) == '#') {
+                    // If encountering a backspace, 
+                    // increment skipS and move to the previous  character
                     skipS++;
                     i--;
                 } else if (skipS > 0) {
+                    // If encountering a character to be skipped due to a previous backspace,
+                    // decrement skipS
                     skipS--;
                     i--;
-                } else
+                } else {
+                    // If encountering an actual character, break out of the loop
                     break;
+                }
             }
-            while (j >= 0) { // Find position of next possible char in build(T)
+
+            // Find the position of the next possible character in T after skipping
+            // backspaces
+            while (j >= 0) {
                 if (T.charAt(j) == '#') {
+                    // If encountering a backspace, increment skipT and move to the previous
+                    // character
                     skipT++;
                     j--;
                 } else if (skipT > 0) {
+                    // If encountering a character to be skipped due to a previous backspace,
+                    // decrement skipT
                     skipT--;
                     j--;
-                } else
+                } else {
+                    // If encountering an actual character, break out of the loop
                     break;
+                }
             }
-            // If two actual characters are different
+
+            // If there are characters left in both strings and they are different, return
+            // false
             if (i >= 0 && j >= 0 && S.charAt(i) != T.charAt(j))
                 return false;
-            // If expecting to compare char vs nothing
+
+            // If there are characters left in one string but not the other, return false
             if ((i >= 0) != (j >= 0))
                 return false;
+
+            // Move to the previous characters in both strings
             i--;
             j--;
         }
+
+        // If no differences are found, return true
         return true;
     }
 
