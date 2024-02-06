74b559c520cd7171e2558a6649814a785fd2205d
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-06 14:44:34 -0500

    Time: 9 ms (64.05%) | Memory: 57.3 MB (60.35%) - LeetSync

--- 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.java
+++ 1713-dot-product-of-two-sparse-vectors/dot-product-of-two-sparse-vectors.java
@@ -1,12 +1,39 @@
 class SparseVector {
+    Map<Integer, Integer> map;
+
+    SparseVector(int[] nums) {
+        map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                map.put(i, nums[i]);
+            }
+        }
+    }
+
+    // Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int result = 0;
+
+        for(int i : map.keySet()) {
+            if(vec.map.containsKey(i)) {
+                result += vec.map.get(i) * map.get(i);
+            }
+        }
+
+        return result;
+    }
+}
+
+class SparseVector_BRUTE_FORCE {
 
     private int[] array;
 
-    SparseVector(int[] nums) {
+    SparseVector_BRUTE_FORCE(int[] nums) {
         array = nums;
     }
 
-    public int dotProduct(SparseVector vec) {
+    // Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector_BRUTE_FORCE vec) {
         int result = 0;
 
         for (int i = 0; i < array.length; i++) {
@@ -15,7 +42,6 @@ class SparseVector {
         return result;
     }
 }
-
 // Your SparseVector object will be instantiated and called as such:
 // SparseVector v1 = new SparseVector(nums1);
 // SparseVector v2 = new SparseVector(nums2);
