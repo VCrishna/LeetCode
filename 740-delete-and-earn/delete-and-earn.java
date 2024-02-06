7cafcee23b889167d467cc583bbb6c5cccfde524
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-06 12:28:56 -0500

    Time: 16 ms (20.51%) | Memory: 44.9 MB (34.78%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,24 +1,34 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
+        // Creating a frequency map to count occurrences of each number
         Map<Integer, Integer> freqMap = new HashMap<>();
-        Set s = new HashSet<>();
+        // Using a set to store distinct numbers
+        Set<Integer> s = new HashSet<>();
+        
+        // Populating the frequency map and the set
         for (int i : nums) {
             s.add(i);
             freqMap.put(i, freqMap.getOrDefault(i, 0) + 1);
         }
+        
+        // Converting the set to a sorted list for easier traversal
         List<Integer> lst = new ArrayList<>(s);
         Collections.sort(lst);
 
+        // Initializing an array to store the maximum points earned at each step
         int[] dp = new int[lst.size()];
+        // Base case: the maximum points earned by taking the first number
         dp[0] = lst.get(0) * freqMap.get(lst.get(0));
 
+        // Iterating through the sorted list of numbers
         for (int i = 1; i < lst.size(); i++) {
             int currentNum = lst.get(i);
             int currentSum = currentNum * freqMap.get(currentNum);
+            
             // If the current number is adjacent to the previous one
             if (lst.get(i - 1) == currentNum - 1) {
-                // Skipping current number, 
-                // and take the max between not taking it and taking it
+                // Skiping current number, 
+                // and taking the max between not taking it and taking it
                 dp[i] = Math.max(dp[i - 1], (i >= 2 ? dp[i - 2] : 0) + currentSum);
             } else {
                 // If not adjacent, taking current number
@@ -26,6 +36,7 @@ class Solution {
             }
         }
 
+        // Returning the maximum points earned after traversing through all numbers
         return dp[lst.size() - 1];
     }
 }
