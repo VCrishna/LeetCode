b8ebbc080241581136ac2ea7f1da0f636b011d9e
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-07 22:23:50 -0500

    Time: 33 ms (70.20%) | Memory: 42.4 MB (79.98%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,17 +1,28 @@
 class Solution {
     public int numSquares(int n) {
+        // Create an array to store the minimum number of 
+        // perfect squares needed to sum up to each index
         int[] dp = new int[n + 1];
+
+        // Initialize dp[0] to 0
         dp[0] = 0;
+
+        // Fill the rest of the array with a value 
+        // greater than n to indicate uncalculated values
         Arrays.fill(dp, 1, n + 1, n + 1);
-        for (int target = 1; target < n + 1; target++) {
-            for (int s = 1; s * s < target + 1; s++) {
+
+        // Iterate through each target sum from 1 to n
+        for (int target = 1; target <= n; target++) {
+            // Iterate through each square number from 1 to the current target
+            for (int s = 1; s * s <= target; s++) {
                 int square = s * s;
-                if (target - square < 0) {
-                    break;
-                }
+                // Update dp[target] with the minimum of its 
+                // current value and 1 plus the value at dp[target - square]
                 dp[target] = Math.min(dp[target], 1 + dp[target - square]);
             }
         }
+
+        // Return the minimum number of perfect squares needed to sum up to n
         return dp[n];
     }
 }
