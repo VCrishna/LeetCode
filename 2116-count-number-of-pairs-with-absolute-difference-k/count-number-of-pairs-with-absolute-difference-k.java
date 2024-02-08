ef71a1c3ad1736e26543c6d0ed11b2e42e72af8f
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-07 19:01:21 -0500

    Time: 4 ms (94.32%) | Memory: 44.3 MB (11.40%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -1,10 +1,27 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
         int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(Math.abs(nums[i] - nums[j]) == k) {
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i] + k)) {
+                result += map.get(nums[i] + k);
+            }
+            if (map.containsKey(nums[i] - k)) {
+                result += map.get(nums[i] - k);
+            }
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        return result;
+    }
+
+    public int countKDifference_BRUTE_FORCE(int[] nums, int k) {
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) == k) {
                     result++;
                 }
             }
