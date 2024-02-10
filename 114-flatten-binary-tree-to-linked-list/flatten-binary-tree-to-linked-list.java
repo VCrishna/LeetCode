3d84a1e1eec362fb0c2670b813677336cc1d10c6
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-09 19:18:11 -0500

    Time: 0 ms (100.00%) | Memory: 41.9 MB (61.40%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -1,36 +1,44 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public void flatten(TreeNode root) {
+        // Base case: If the root is null, return as there's nothing to flatten
         if (root == null) {
             return;
         }
 
+        // Flatten the left subtree recursively
         flatten(root.left);
+
+        // Flatten the right subtree recursively
         flatten(root.right);
 
+        // Save the right subtree of the current node
         TreeNode rightSubtree = root.right;
 
+        // Make the left subtree the right subtree of the current node
         root.right = root.left;
         root.left = null;
 
+        // Find the end of the current flattened subtree
         while (root.right != null) {
             root = root.right;
         }
 
+        // Attach the right subtree to the end of the current flattened subtree
         root.right = rightSubtree;
     }
 }
