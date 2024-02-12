fb6f5bceeb148c6926ed38eb4eed55fc026bfd86
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-11 20:29:18 -0500

    Time: 3 ms (64.59%) | Memory: 50.7 MB (29.63%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,26 +1,38 @@
 class Solution {
+    // Using Boyer-Moore Voting Algorithm
     public int majorityElement(int[] nums) {
-        int count = 0;
-        Integer majorityEle = null;
+        int count = 0; // Initialize a counter for tracking occurrences
+        Integer majorityEle = null; // Initialize a variable to store the majority element
 
-        for(int num : nums) {
-            if(count == 0) {
-                majorityEle = num;
+        // Iterating through the array
+        for (int num : nums) {
+            if (count == 0) { // If count is 0, meaning no majority candidate yet
+                majorityEle = num; // Set the current number as the potential majority candidate
             }
+            // Updating the count based on whether the current number is equal to the
+            // potential majority candidate
             count += (num == majorityEle) ? 1 : -1;
         }
-
-        return majorityEle;
+        // At the end of the loop, 
+        // majorityEle should hold the majority element (if it exists)
+        return majorityEle; // Return the majority element
     }
-    public int majorityElement_EXTRA_SPACE(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
 
+    // Using HashMap to count occurrences (Extra Space)
+    public int majorityElement_EXTRA_SPACE(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(); // Create a HashMap to count occurrences
+
+        // Counting occurrences of each number in the array
         for (int i : nums)
             map.put(i, map.getOrDefault(i, 0) + 1);
-        
-        for(int i : nums) {
-            if(map.get(i) > nums.length / 2) return i;
+
+        // Iterating through the numbers in the array
+        for (int i : nums) {
+            // Checking if the occurrence of the current number is 
+            // greater than half of the array length
+            if (map.get(i) > nums.length / 2)
+                return i; // Return the number as the majority element
         }
-        return 0;
+        return 0; // If no majority element found, return 0
     }
 }
