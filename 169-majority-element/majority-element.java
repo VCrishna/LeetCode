e90009687c5d54864d279a3a818432d5d1f91f9f
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-11 20:21:59 -0500

    Time: 12 ms (33.06%) | Memory: 48.6 MB (85.54%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,33 +1,12 @@
 class Solution {
-    // The Boyer-Moore Majority Vote Algorithm
     public int majorityElement(int[] nums) {
-        // 'result' will hold the current candidate for majority element.
-        int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : nums)
+            map.put(i, map.getOrDefault(i, 0) + 1);
         
-        // 'count' is a counter for the majority element.
-        int count = 0;
-
-        // Iterate through each number in the 'nums' array.
         for(int i : nums) {
-            // If count is zero, then set 'i' as the new candidate for majority element.
-            if(count == 0)
-                result = i;
-
-            // Increment the counter if the current number is the same as the current majority candidate, 
-            // otherwise decrement it.
-            count += (i == result ? 1 : -1);
-        }
-
-        // After iterating through all numbers, 'result' will have the majority element.
-        return result;
-    }
-
-    public int majorityElement_brute(int[] nums) {
-        Map<Integer, Integer> myMap = new HashMap<Integer, Integer>();
-        for (int num : nums) {
-            myMap.put(num, myMap.getOrDefault(num, 0) + 1);
-            if (myMap.get(num) > nums.length / 2)
-                return num;
+            if(map.get(i) > nums.length / 2) return i;
         }
         return 0;
     }
