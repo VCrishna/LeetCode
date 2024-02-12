567083cf21412cb4f7c5be164668d97c97ba359e
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-11 20:26:48 -0500

    Time: 2 ms (78.11%) | Memory: 50.1 MB (62.08%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,5 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer majorityEle = null;
+
+        for(int num : nums) {
+            if(count == 0) {
+                majorityEle = num;
+            }
+            count += (num == majorityEle) ? 1 : -1;
+        }
+
+        return majorityEle;
+    }
+    public int majorityElement_EXTRA_SPACE(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int i : nums)
