240d0c19038f91fbc4bd3f6c33a977114d57d0a3
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-13 16:10:11 -0500

    Time: 29 ms (60.57%) | Memory: 44.8 MB (92.66%) - LeetSync

--- 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,18 +1,30 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
+        // Initialize a StringBuilder to manipulate the string efficiently
         StringBuilder sb = new StringBuilder(s);
+
+        // Create an array count to keep track of the count of consecutive occurrences
+        // of characters
         int count[] = new int[sb.length()];
+
+        // Iterate through the characters of the string
         for (int i = 0; i < sb.length(); ++i) {
+            // If the current character is different from the previous one
+            // or it's the first character
             if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
-                count[i] = 1;
+                count[i] = 1; // Reset the count to 1
             } else {
-                count[i] = count[i - 1] + 1;
+                count[i] = count[i - 1] + 1; // Increment the count of consecutive occurrences
+                // If the count becomes equal to k, it means k consecutive duplicates are found
                 if (count[i] == k) {
+                    // Remove these k characters from the string using StringBuilder's delete method
                     sb.delete(i - k + 1, i + 1);
+                    // Adjust the loop counter i to handle the removed characters
                     i = i - k;
                 }
             }
         }
+        // Return the modified string
         return sb.toString();
     }
 }
