6f5d4516b9e0d3c50422c3fa1c32d5e76ad9bbde
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-13 18:27:38 -0500

    Time: 0 ms (100.00%) | Memory: 41.6 MB (90.72%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,12 +3,11 @@ class Solution {
         // modified binary search to find the pivot
         int left = 0;
         int right = nums.length - 1;
-        while(left < right) {
+        while (left < right) {
             int midPoint = left + (right - left) / 2;
             if (nums[midPoint] > nums[right]) {
                 left = midPoint + 1;
-            }
-            else{
+            } else {
                 right = midPoint;
             }
         }
@@ -17,25 +16,23 @@ class Solution {
         left = 0;
         right = nums.length - 1;
         // Checking whether the target is within the boundaries
-        if(target >= nums[start] && target <= nums[right]) {
+        if (target >= nums[start] && target <= nums[right]) {
             left = start;
-        } 
-        else {
+        } else {
             right = start;
         }
-        while(left <= right) {
+        while (left <= right) {
             int middle = left + (right - left) / 2;
             if (nums[middle] == target)
                 return middle;
             else if (nums[middle] < target) {
                 left = middle + 1;
-            }
-            else {
-                right = middle-1;
+            } else {
+                right = middle - 1;
             }
         }
         return -1;
     }
 }
-        //  l   r
+// l r
 // [4,5,6,7,0,1,2]
