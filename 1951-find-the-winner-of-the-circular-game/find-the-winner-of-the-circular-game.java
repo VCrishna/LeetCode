29702f7dd52f9fc9067303d466f2c01216551d87
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-14 17:10:35 -0500

    Time: 0 ms (100.00%) | Memory: 40.8 MB (43.10%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -1,26 +1,45 @@
+// class Solution {
+//     public int findTheWinner(int n, int k) {
+//         // Creating a queue to represent the circle of players
+//         Queue<Integer> queue = new LinkedList<>();
+
+//         // Initializing the queue with player numbers from 1 to n
+//         for (int i = 1; i <= n; i++) {
+//             queue.add(i);
+//         }
+
+//         // Continue the elimination process until only one player remains
+//         while (queue.size() != 1) {
+//             // Rotate the queue (k-1) times, simulating 
+//             // the count to (k) and eliminating the player
+//             for (int i = 1; i <= k - 1; i++) {
+//                 queue.add(queue.poll());
+//             }
+
+//             // Eliminating the k-th player
+//             queue.poll();
+//         }
+
+//         // Returning the player number of the remaining player (the winner)
+//         return queue.poll();
+//     }
+// }
 class Solution {
     public int findTheWinner(int n, int k) {
-        // Creating a queue to represent the circle of players
-        Queue<Integer> queue = new LinkedList<>();
-
-        // Initializing the queue with player numbers from 1 to n
-        for (int i = 1; i <= n; i++) {
-            queue.add(i);
+        // Start the game with n friends and k steps
+        return playGame(n, k);
+    }
+    
+    private int playGame(int n, int k) {
+        if (n == 1) {
+            // If there's only one friend left, they are the winner
+            return 1;
+        } else {
+            // Recursively play the game with n-1 friends (excluding the friend who loses)
+            // Start the count from the friend immediately clockwise of the friend who loses,
+            // and wrap around the circle if necessary
+            return (playGame(n - 1, k) + k - 1) % n + 1;
         }
-
-        // Continue the elimination process until only one player remains
-        while (queue.size() != 1) {
-            // Rotate the queue (k-1) times, simulating 
-            // the count to (k) and eliminating the player
-            for (int i = 1; i <= k - 1; i++) {
-                queue.add(queue.poll());
-            }
-
-            // Eliminating the k-th player
-            queue.poll();
-        }
-
-        // Returning the player number of the remaining player (the winner)
-        return queue.poll();
     }
 }
+
