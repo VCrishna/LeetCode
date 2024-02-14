2a36ff9d6a91d72d31da1978a8a81d5c6aa6396e
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-13 22:01:43 -0500

    Time: 1 ms (43.61%) | Memory: 40.8 MB (94.02%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,27 +1,58 @@
 class Solution {
-    public boolean checkValidString(String s) {
-        int leftMin = 0;
-        int leftMax = 0;
+    // public boolean checkValidString(String s) {
+    //     int leftMin = 0;
+    //     int leftMax = 0;
 
-        for (char c : s.toCharArray()) {
+    //     for (char c : s.toCharArray()) {
+    //         if (c == '(') {
+    //             leftMin++;
+    //             leftMax++;
+    //         } else if (c == ')') {
+    //             leftMin--;
+    //             leftMax--;
+    //         } else {
+    //             leftMin--;
+    //             leftMax++;
+    //         }
+    //         if (leftMax < 0) {
+    //             return false;
+    //         }
+    //         if (leftMin < 0) {
+    //             leftMin = 0;
+    //         }
+    //     }
+
+    //     return leftMin == 0;
+    // }
+    public boolean checkValidString(String s) {
+        Stack<Integer> leftParentheses = new Stack<>();
+        Stack<Integer> asterisks = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             if (c == '(') {
-                leftMin++;
-                leftMax++;
-            } else if (c == ')') {
-                leftMin--;
-                leftMax--;
-            } else {
-                leftMin--;
-                leftMax++;
-            }
-            if (leftMax < 0) {
-                return false;
-            }
-            if (leftMin < 0) {
-                leftMin = 0;
+                leftParentheses.push(i);
+            } else if (c == '*') {
+                asterisks.push(i);
+            } else { // c == ')'
+                if (!leftParentheses.isEmpty()) {
+                    leftParentheses.pop();
+                } else if (!asterisks.isEmpty()) {
+                    asterisks.pop();
+                } else {
+                    return false;
+                }
             }
         }
-
-        return leftMin == 0;
+        
+        while (!leftParentheses.isEmpty() && !asterisks.isEmpty()) {
+            if (leftParentheses.peek() > asterisks.peek()) {
+                return false;
+            }
+            leftParentheses.pop();
+            asterisks.pop();
+        }
+        
+        return leftParentheses.isEmpty();
     }
 }
