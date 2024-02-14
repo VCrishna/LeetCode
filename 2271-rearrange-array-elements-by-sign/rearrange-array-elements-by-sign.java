2925655a858d00aac0bfd5a67ff3f20d6246523f
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-13 19:20:54 -0500

    Time: 3 ms (100.00%) | Memory: 76.9 MB (57.70%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,24 +1,40 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        int[] result = new int[nums.length];
+        int even = 0;
+        int odd = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result[even] = nums[i];
+                even += 2;
+            } else {
+                result[odd] = nums[i];
+                odd += 2;
+            }
+        }
+
+        return result;
+    }
+
+    public int[] rearrangeArray_EXTRA_SPACE(int[] nums) {
         List<Integer> positive = new ArrayList();
         List<Integer> negative = new ArrayList();
 
-        for(int i : nums) {
-            if(i < 0) {
+        for (int i : nums) {
+            if (i < 0) {
                 negative.add(i);
-            }
-            else {
+            } else {
                 positive.add(i);
             }
         }
         int[] result = new int[nums.length];
         int even = 0;
         int odd = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(i % 2 == 0) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
                 result[i] = positive.get(even++);
-            }
-            else {
+            } else {
                 result[i] = negative.get(odd++);
             }
         }
