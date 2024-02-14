5a889b7e2c59b51e5169ba5473295d210caaa1f5
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-14 14:14:31 -0500

    Time: 1 ms (79.13%) | Memory: 41.8 MB (46.27%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -8,16 +8,15 @@ class Solution {
 
         for (int i = 2; i <= s.length(); i++) {
             int oneDigit = Integer.parseInt(s.substring(i - 1, i));
-            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+            int twoDigit = Integer.parseInt(s.substring(i - 2, i));
 
             if (oneDigit > 0 && oneDigit <= 9) {
                 dp[i] += dp[i - 1];
             }
-            if (twoDigits >= 10 && twoDigits <= 26) {
+            if (twoDigit >= 10 && twoDigit <= 26) {
                 dp[i] += dp[i - 2];
             }
         }
-
         return dp[s.length()];
     }
 }
