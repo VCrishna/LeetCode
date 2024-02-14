39ee0c72bbfd169a44621945cce704df21a520b7
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-14 07:20:52 -0500

    Time: 1 ms (100.00%) | Memory: 44.2 MB (77.18%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,27 +1,80 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        // Base condition
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return mergeKLists(lists, 0, lists.length - 1);
+    }
+
+    private ListNode mergeKLists(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        // Mid of list of lists
+        int mid = start + (end - start) / 2;
+        // Recursive call for left sublist
+        ListNode left = mergeKLists(lists, start, mid);
+        // Recursive call for right sublist
+        ListNode right = mergeKLists(lists, mid + 1, end);
+        // Merge the left and right sublist
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        // Create a dummy node
+        ListNode head = new ListNode(-1);
+        // Temp node
+        ListNode temp = head;
+        // Loop until any of the list becomes null
+        while (left != null && right != null) {
+            // Choose the value from the left and right which is smaller
+            if (left.val < right.val) {
+                temp.next = left;
+                left = left.next;
+            } else {
+                temp.next = right;
+                right = right.next;
+            }
+            temp = temp.next;
+        }
+        // Take all nodes from left list if remaining
+        while (left != null) {
+            temp.next = left;
+            left = left.next;
+            temp = temp.next;
+        }
+        // Take all nodes from right list if remaining
+        while (right != null) {
+            temp.next = right;
+            right = right.next;
+            temp = temp.next;
+        }
+        return head.next;
+    }
+
+    public ListNode mergeKLists_EXTRA_SPACE(ListNode[] lists) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(
-            (a, b) -> a - b
-        );
-        for(ListNode list : lists) {
-            while(list != null) {
+                (a, b) -> a - b);
+        for (ListNode list : lists) {
+            while (list != null) {
                 minHeap.add(list.val);
                 list = list.next;
             }
         }
         ListNode dummy = new ListNode(0);
         ListNode temp = dummy;
-        while(!minHeap.isEmpty()) {
+        while (!minHeap.isEmpty()) {
             temp.next = new ListNode(minHeap.remove());
             temp = temp.next;
         }
