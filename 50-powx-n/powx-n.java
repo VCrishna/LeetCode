5a178acd4941a9a56764c4366563f53f2f4516db
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-14 20:18:52 -0500

    Time: 0 ms (100.00%) | Memory: 42.4 MB (31.52%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,16 +1,27 @@
 class Solution {
+    // Method to calculate x raised to the power of n
     public double myPow(double x, int n) {
-        double power = power(x,n);
+        double power = power(x, n);
+        // Return the result; if n is negative, return the reciprocal of power
         return n < 0 ? 1.0 / power : power;
     }
+
+    // Recursive method to calculate x raised to the power of n
     public double power(double x, int n) {
-        if(x==0)
+        // Base case: if x is 0, return 0
+        if (x == 0)
             return 0;
-        if(n==0)
+        // Base case: if n is 0, return 1
+        if (n == 0)
             return 1;
+
         double result = 1.0;
-        result = power(x,n/2);
+
+        // Recursively calculate the power using divide and conquer technique
+        result = power(x, n / 2);
+        // Square the result
         result *= result;
+        // If n is odd, multiply the result by x
         return n % 2 == 0 ? result : x * result;
     }
 }
