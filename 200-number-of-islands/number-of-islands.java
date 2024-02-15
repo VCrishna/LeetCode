8f933a15cb042d1807412ddb2215a7031d1b4184
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-14 19:35:38 -0500

    Time: 3 ms (88.59%) | Memory: 49.5 MB (45.50%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -7,7 +7,7 @@ class Solution {
         // Iterating through each cell in the grid
         for (int row = 0; row < rows; row++) {
             for (int column = 0; column < columns; column++) {
-                // If the current cell is part of an island ('1'), 
+                // If the current cell is part of an island ('1'),
                 // perform DFS to mark the entire island
                 if (grid[row][column] == '1') {
                     dfs(grid, row, column, rows, columns);
@@ -18,10 +18,11 @@ class Solution {
         }
         return result;
     }
-    // Depth-First Search (DFS) method to mark the 
+
+    // Depth-First Search (DFS) method to mark the
     // entire island connected to the given cell
     public void dfs(char[][] grid, int row, int column, int rows, int columns) {
-        // Base case: Check if the current cell 
+        // Base case: Check if the current cell
         // is out of bounds or not part of an island
         if (row < 0 || row >= rows ||
                 column < 0 || column >= columns ||
@@ -30,7 +31,7 @@ class Solution {
         }
         // Marking the current cell as visited by changing '1' to '0'
         grid[row][column] = '0';
-        
+
         // Recursively performing DFS on adjacent cells in all four directions
         dfs(grid, row + 1, column, rows, columns);
         dfs(grid, row - 1, column, rows, columns);
