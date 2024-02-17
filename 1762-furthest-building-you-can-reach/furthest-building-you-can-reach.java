92eb9250590d387c7deaa283922dfaee805e7953
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-17 14:46:06 -0500

    Time: 15 ms (91.86%) | Memory: 57.3 MB (73.95%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
@@ -1,28 +1,44 @@
 class Solution {
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        // Creating a priority queue to store the height differences
+        // between consecutive buildings
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        for(int i = 1; i < heights.length; i++) {
-            int diff = heights[i] - heights[i-1];
-            if(diff > 0) {
-                if(minHeap.size() < ladders) {
-                    minHeap.add(diff);
+        // Iterating through the heights array starting from the second building
+        for (int i = 1; i < heights.length; i++) {
+            // Calculating the height difference between the current and previous buildings
+            int diff = heights[i] - heights[i - 1];
+
+            // If the height difference is positive (meaning we need to use bricks or
+            // ladders to climb), process it
+            if (diff > 0) {
+                // If we have available ladders, we can climb without using bricks
+                if (minHeap.size() < ladders) {
+                    minHeap.add(diff); // Storing the height difference in the priority queue
                 }
+                // If we've used all ladders, we need to use bricks to climb
                 else {
-                    int brks = diff;
-                    // optimize previous ladder use
-                    if(minHeap.size() > 0 && minHeap.peek() < diff) {
-                        brks = minHeap.remove();
-                        minHeap.add(diff);
+                    int brks = diff; // Initializing bricks needed to climb as the height difference
+                    // If there are previously used ladders that can be optimized by using
+                    // bricks instead, remove the smallest height difference from the priority queue
+                    if (minHeap.size() > 0 && minHeap.peek() < diff) {
+                        brks = minHeap.remove(); // Removing the smallest height difference from the queue
+                        minHeap.add(diff); // Adding the current height difference to the queue
                     }
-                    if(bricks - brks >= 0) {
+                    // If we have enough bricks to climb, 
+                    // subtracting the bricks needed from the total available bricks
+                    if (bricks - brks >= 0) {
                         bricks -= brks;
                     }
+                    // If we don't have enough bricks to climb, 
+                    // return the index of the previous building
                     else {
                         return i - 1;
                     }
                 }
             }
         }
+        // If we reach the end of the heights array, 
+        // return the index of the last building
         return heights.length - 1;
     }
 }
