cb1730436e67959d86fd4948ea49f4c3c31607fb
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-02-23 09:14:11 -0500

    Time: 3 ms (71.20%) | Memory: 53.8 MB (51.82%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -5,6 +5,28 @@ class Solution {
             return -1;
         }
 
+        int[] indegrees = new int[N + 1];
+        int[] outdegrees = new int[N + 1];
+
+        for (int[] relation : trust) {
+            outdegrees[relation[0]]++;
+            indegrees[relation[1]]++;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (indegrees[i] == N - 1 && outdegrees[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int findJudge_ARRAY(int N, int[][] trust) {
+
+        if (trust.length < N - 1) {
+            return -1;
+        }
+
         int[] trustScores = new int[N + 1];
 
         for (int[] relation : trust) {
