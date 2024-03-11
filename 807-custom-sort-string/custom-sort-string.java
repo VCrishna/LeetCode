22a6d69ad3b674a3172a1dd9e4d6de2441651a74
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-03-10 20:49:48 -0400

    Time: 2 ms (62.70%) | Memory: 40.9 MB (98.09%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.java
+++ 807-custom-sort-string/custom-sort-string.java
@@ -1,6 +1,46 @@
 class Solution {
     public String customSortString(String order, String s) {
         // StringBuilder to store the sorted string
+        StringBuilder result = new StringBuilder();
+        // Map to store the frequency of each character in the input string 's'
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        // Counting the frequency of each character in the input string 's'
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        // Iterating through each character in the 'order' string
+        for (int i = 0; i < order.length(); i++) {
+            char currentChar = order.charAt(i);
+            // Checking if the current character exists in the frequency map
+            if (freqMap.containsKey(currentChar)) {
+                // While there are occurrences of the current character in the 'order' 
+                // string ppend them to the result string, and decrement the 
+                // count of occurrences in the frequency map
+                while (freqMap.get(currentChar) > 0) {
+                    result.append(currentChar);
+                    freqMap.put(currentChar, freqMap.get(currentChar) - 1);
+                }
+            }
+        }
+
+        // Appending remaining characters (not found in the 'order' string) 
+        // to the result string
+        for (char c : freqMap.keySet()) {
+            int count = freqMap.get(c);
+            while (count > 0) {
+                result.append(c);
+                count--;
+            }
+        }
+
+        // Returning the sorted string
+        return result.toString();
+    }
+
+    public String customSortString_MY_METHOD(String order, String s) {
+        // StringBuilder to store the sorted string
         StringBuilder sb = new StringBuilder();
 
         // List to store characters in 's' that match the order specified
