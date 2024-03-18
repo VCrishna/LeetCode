e7127bd424be8feb4ff65181beff1f1a14481759
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-03-18 16:17:01 -0400

    Time: 51 ms (98.23%) | Memory: 68.8 MB (54.30%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -2,24 +2,28 @@ import java.util.Arrays;
 
 class Solution {
     public int findMinArrowShots(int[][] points) {
-        if (points.length == 0) 
+        // Base case: if there are no points, return 0 as no arrows are needed
+        if (points.length == 0)
             return 0;
-        
+
         // Sort the points based on their end positions
         Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
-        
+
         int arrows = 1; // Initialize the number of arrows needed to 1
         int end = points[0][1]; // Initialize the end position with the end of the first point
-        
+
+        // Iterate through the sorted points starting from the second point
         for (int i = 1; i < points.length; i++) {
-            // If the current balloon doesn't intersect with the previous one
-            // Increment the arrow count and update the end position
+            // If the start position of the current balloon is greater than the end
+            // position:
             if (points[i][0] > end) {
-                arrows++;
-                end = points[i][1];
+                // It means a new arrow is needed:
+                arrows++; // Increment the arrow count
+                end = points[i][1]; // Update the end position to the end of the current balloon
             }
         }
-        
+
+        // Return the final arrow count
         return arrows;
     }
 }
