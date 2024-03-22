f59a96abb50ad597360381856e1bf6fcb01daffe
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-03-21 20:07:41 -0400

    Time: 4 ms (82.66%) | Memory: 67.6 MB (57.54%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,42 +1,51 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        // Initializing two pointers slow and fast to traverse the list
         ListNode slow = head, fast = head;
-        while(fast != null && fast.next != null) {
+
+        // Moving fast pointer two steps ahead and slow pointer
+        // one step ahead until fast reaches end or null
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
+
+        // Reversing the second half of the list starting from slow pointer
         slow = reverse(slow);
-        fast = head;
-        while(fast.next != null) {
-            if(fast.val != slow.val) {
-                return false;
+        fast = head; // Reseting fast pointer to the beginning of the list
+
+        // Traversing both halves simultaneously and check for palindrome
+        while (fast.next != null) {
+            if (fast.val != slow.val) {
+                return false; // If values don't match, it's not a palindrome
             }
             fast = fast.next;
             slow = slow.next;
         }
-        return true;
+        return true; // If all values match, it's a palindrome
     }
 
+    // Function to reverse a linked list
     public ListNode reverse(ListNode current) {
-        ListNode prev = null;
-        while(current != null) {
-            ListNode next = current.next;
-            // reversing the link between current element and previous element
+        ListNode prev = null; // Initializing previous node as null
+        while (current != null) {
+            ListNode next = current.next; // Storing next node
+            // Reversing the link between current element and previous element
             current.next = prev;
-            // moving prev and current pointers forward
+            // Moving prev and current pointers forward
             prev = current;
             current = next;
         }
-        return prev;
+        return prev; // Returning the new head of the reversed list
     }
 }
