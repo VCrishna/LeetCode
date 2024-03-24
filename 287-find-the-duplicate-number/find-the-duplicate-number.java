2cbde0335fd7d4d29eef6267b151bb58da4c42b2
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-03-23 20:10:36 -0400

    Time: 3 ms (89.42%) | Memory: 58.4 MB (37.91%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -3,31 +3,32 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int index = Math.abs(nums[i]);
             if (nums[index] < 0) {
-                nums[index] *= -1;
-                return index;
+                nums[index] *= -1; // Marking the duplicate by negating the value
+                return index; // Returning the duplicate number
             }
-            nums[index] *= -1;
+            nums[index] *= -1; // Marking the visited number by negating the value
         }
-        return 0;
+        return 0; // Default return value if no duplicate found (assuming 0 is not in the array)
     }
 
     public int findDuplicate_SET(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int i : nums) {
             if (set.contains(i)) {
-                return i;
+                return i; // Returning the duplicate number found in the set
             }
-            set.add(i);
+            set.add(i); // Adding each element to the set
         }
-        return 0;
+        return 0; // Default return value if no duplicate found (assuming 0 is not in the array)
     }
 
     public int findDuplicate_SORTING(int[] nums) {
-        Arrays.sort(nums);
+        Arrays.sort(nums); // Sorting the array in non-decreasing order
         for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1])
-                return nums[i];
+            if (nums[i] == nums[i + 1]) // If adjacent elements are equal, it's a duplicate
+                return nums[i]; // Returning the duplicate number
         }
-        return 0;
+        return 0; // Default return value if no duplicate found (assuming 0 is not in the array)
     }
+
 }
