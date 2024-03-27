e0fd63f498a2f8f52fd3b67b0b5947ac3c6737e6
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-03-27 13:52:27 -0400

    Time: 4 ms (100.00%) | Memory: 47.6 MB (63.17%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -3,13 +3,15 @@ class Solution {
         int result = 0;
         int left = 0;
         int product = 1;
-        for (int right = 0; right < nums.length; right++) {
+
+        for(int right = 0; right < nums.length; right++) {
             product *= nums[right];
-            while (left <= right && product >= k) {
-                product = product / nums[left];
-                left++;
+
+            while(left <= right && product >= k) {
+                product /= nums[left++];
             }
-            result += (right - left + 1);
+
+            result += right - left + 1;
         }
 
         return result;
