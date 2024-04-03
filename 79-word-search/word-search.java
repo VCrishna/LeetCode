3d2ddb921a520aed7bfb6cd293375362a3546aa3
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-04-02 20:08:55 -0400

    Time: 135 ms (76.29%) | Memory: 41.3 MB (74.16%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -13,7 +13,7 @@ class Solution {
             }
         }
         // If no match is found, the word does not exist on the board
-        return false; 
+        return false;
     }
 
     // Depth-First Search (DFS) function
@@ -23,24 +23,23 @@ class Solution {
             return true;
         }
 
-        // Base case: If the current cell is out of bounds or does not match the word character, return false
-        if (
-            row < 0 || row >= board.length ||
-            column < 0 || column >= board[0].length ||
-            board[row][column] != word.charAt(count)
-        ) {
+        // Base case: If the current cell is out of bounds or does not match the word
+        // character, return false
+        if (row < 0 || row >= board.length ||
+                column < 0 || column >= board[0].length ||
+                board[row][column] != word.charAt(count)) {
             return false;
         }
         // Storing the current cell's character before modification
         char visited = board[row][column];
         // Marking the cell as visited (using a placeholder character)
-        board[row][column] = ' '; 
+        board[row][column] = ' ';
 
         // Recursively checking neighboring cells in all four directions
         boolean found = dfs(board, row + 1, column, count + 1, word) ||
-                        dfs(board, row - 1, column, count + 1, word) ||
-                        dfs(board, row, column + 1, count + 1, word) ||
-                        dfs(board, row, column - 1, count + 1, word);
+                dfs(board, row - 1, column, count + 1, word) ||
+                dfs(board, row, column + 1, count + 1, word) ||
+                dfs(board, row, column - 1, count + 1, word);
 
         // Restoring the original character after DFS exploration
         board[row][column] = visited;
