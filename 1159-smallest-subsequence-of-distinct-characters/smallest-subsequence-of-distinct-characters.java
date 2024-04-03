6dda0c53ffee7a9ee083ac0b981365ab518a9c5d
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-04-03 11:00:58 -0400

    Time: 2 ms (97.60%) | Memory: 41.7 MB (23.12%) - LeetSync

--- 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
+++ 1159-smallest-subsequence-of-distinct-characters/smallest-subsequence-of-distinct-characters.java
@@ -1,49 +1,57 @@
 class Solution {
     public String smallestSubsequence(String s) {
-        // length of given input string
+        // Length of the given input string
         int strLength = s.length();
-        // array to hold last occurance of indexes of characters in the given string
+        // Array to hold the last occurrence indexes of characters in the given string
         int[] lastIndexArray = new int[26];
-        // updating last occured indexes of characters in the array
+        
+        // Updating the last occurrence indexes of characters in the array
         for (int i = 0; i < strLength; i++) 
             lastIndexArray[s.charAt(i) - 'a'] = i;
-        // visited array is used to handle the duplications
-        // we cannot add duplicate characters to the output string
-        // every character present in string s must be part of output string
-        // and frequency of each character should be 1
-        // and output string should be lexographically smallest
+        
+        // Visited array is used to handle duplications
+        // We cannot add duplicate characters to the output string
+        // Every character present in string s must be part of the output string
+        // The frequency of each character should be 1
+        // The output string should be lexographically smallest
         boolean[] visited = new boolean[26];
         Stack<Integer> stack = new Stack<>();
+        
+        // Iterating through the characters of the string
         for (int i = 0; i < strLength; i++) {
-            // extracting current character
+            // Extracting the current character
             int ch = s.charAt(i) - 'a';
-            // checking if the character is visited already
-            // if it is visited already we are simplify skiping it 
-            // as we donot want to add duplicates to the output string
-            if(visited[ch])
+            
+            // Checking if the character is visited already
+            // If it is visited already, we simply skip it 
+            // as we do not want to add duplicates to the output string
+            if (visited[ch])
                 continue;
-            // if it was not visited we are updating visited array
-            // marking that character as visited
+            
+            // If it was not visited, we update the visited array
+            // Marking that character as visited
             visited[ch] = true;
 
-            while(
-                // stack should not be empty
-                !stack.isEmpty() 
-                // top character in stack is greater than the current character
+            // While loop to remove characters from the stack that are greater than the current character 
+            // and have a later occurrence in the string
+            while (!stack.isEmpty() 
                 && stack.peek() > ch 
-                // last index of my top character is greater than i
-                && lastIndexArray[stack.peek()] > i
-                ) {
-                    // simply delete the element from stack and mark it as un visited
-                   visited[stack.pop()] = false;
-                }
-            // push the character into stack
+                && lastIndexArray[stack.peek()] > i) {
+                // Pop the element from the stack and mark it as unvisited
+                visited[stack.pop()] = false;
+            }
+            
+            // Push the character into the stack
             stack.push(ch);
         }
+        
         StringBuilder sb = new StringBuilder();
-        // iterating over stack in this way will help us to iterate from bottom to top of stack
+        
+        // Iterating over stack to build the result string
+        // Since the stack contains characters in reverse order, we append characters from bottom to top
         for (int i : stack) 
             sb.append((char) (i + 'a'));
+        
         return sb.toString();
     }
 }
