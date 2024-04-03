668dffbb2f22b386ac602e089bc0f930216826c1
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-04-03 10:55:18 -0400

    Time: 5 ms (81.85%) | Memory: 42.6 MB (14.44%) - LeetSync

--- 1123-single-row-keyboard/single-row-keyboard.java
+++ 1123-single-row-keyboard/single-row-keyboard.java
@@ -1,5 +1,21 @@
 class Solution {
     public int calculateTime(String keyboard, String word) {
+        int[] freqArray = new int[26];
+        for (int i = 0; i < keyboard.length(); i++) {
+            freqArray[keyboard.charAt(i) - 'a'] = i;
+        }
+        int result = 0;
+        int previous = 0;
+
+        for (char c : word.toCharArray()) {
+            result += Math.abs(previous - freqArray[c - 'a']);
+            previous = freqArray[c - 'a'];
+        }
+
+        return result;
+    }
+
+    public int calculateTime_MAP(String keyboard, String word) {
         Map<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < keyboard.length(); i++) {
             map.put(keyboard.charAt(i), i);
@@ -7,7 +23,7 @@ class Solution {
         int result = 0;
         int previous = 0;
 
-        for(char c : word.toCharArray()) {
+        for (char c : word.toCharArray()) {
             result += Math.abs(previous - map.get(c));
             previous = map.get(c);
         }
