7b6c04a56271a59d60ba7a4391e2fe24e46d1523
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-04-10 10:22:45 -0400

    Time: 4 ms (86.41%) | Memory: 42.8 MB (99.03%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.java
@@ -1,20 +1,33 @@
 class Solution {
     public int[] deckRevealedIncreasing(int[] deck) {
+        // Sorting the deck in increasing order
         Arrays.sort(deck);
+
+        // Initializing an array to store the result
         int[] result = new int[deck.length];
 
+        // Creating a deque to simulate the revealed order
         Deque<Integer> queue = new ArrayDeque<>();
+
+        // Initializing the deque with indices of the deck
         for (int i = 0; i < deck.length; i++) {
             queue.offer(i);
         }
+
+        // Revealing the cards in the increasing order and update the result array
         for (int d : deck) {
-            int i = queue.pollFirst();
-            result[i] = d;
+            // Get the index of the card to be revealed
+            int i = queue.pollFirst(); // Simulates revealing the top card from the deck
+            result[i] = d; // Assigning the revealed card value to the result array at the index
+
+            // If there are more cards remaining in the deck,
+            // moving the revealed card to the bottom of the deck
             if (!queue.isEmpty()) {
                 queue.offerLast(queue.pollFirst());
             }
         }
 
+        // Returning the result array containing the revealed cards in increasing order
         return result;
     }
 }
