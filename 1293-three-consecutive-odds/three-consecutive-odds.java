a779769f8b81a887fa96ce63c59ff1f2cf769c30
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-06-30 21:08:20 -0500

    Time: 0 ms (100.00%) | Memory: 42.3 MB (19.26%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.java
+++ 1293-three-consecutive-odds/three-consecutive-odds.java
@@ -2,11 +2,10 @@ class Solution {
     public boolean threeConsecutiveOdds(int[] arr) {
         int count = 0;
         for (int i : arr) {
-            if (i % 2 == 0) {
+            if (i % 2 == 0)
                 count = 0;
-            }
             else
-            count++;
+                count++;
             if (count == 3)
                 return true;
         }
