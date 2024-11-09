b1eb4aae2c90984fedb0a12277c7b0f92ba4cd12
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2024-11-09 16:07:32 -0600

    Time: 1753 ms (14.04%) | Memory: 41.5 MB (5.83%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.java
+++ 3394-minimum-array-end/minimum-array-end.java
@@ -1,9 +1,17 @@
 class Solution {
     public long minEnd(int n, int x) {
+        // Initialize the result with the given value x
         long result = x;
+        
+        // Iterate from 1 to n-1 (total n-1 iterations)
         for(int i = 1; i < n; i++) {
+            // Increment the current 'result' by 1.
+            // Apply a bitwise OR operation with 'x' to ensure the result has all bits set as in 'x'.
+            // This OR operation helps preserve the bits from 'x' in the final value.
             result = (result + 1) | x;
         }
+        
+        // Return the final computed result
         return result;
     }
 }
