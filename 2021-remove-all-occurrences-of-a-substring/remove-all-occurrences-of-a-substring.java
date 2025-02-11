7b016479113528e6c7cacba5605aca86576d53f0
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2025-02-10 20:21:01 -0600

    Time: 1 ms (99.46%) | Memory: 42.6 MB (73.57%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -1,14 +1,14 @@
-class Solution 
-{
-    public String removeOccurrences(String s, String part) 
-    {
+class Solution {
+    public String removeOccurrences(String s, String part) {
         // Start index of {part} in {s}
         int index = s.indexOf(part);
-        
-        // If {index == - 1}-> then {part} is not substring of {s} and return {s}
-        if (index == -1) return s;
-        
-        // Otherwise and in a recursive way, take only the parts before and after the 'first' {part} (from left)
+
+        // If {index == - 1} -> then {part} is not substring of {s} and return {s}
+        if (index == -1)
+            return s;
+
+        // Otherwise and in a recursive way, 
+        // take only the parts before and after the 'first' {part} (from left)
         s = s.substring(0, index) + s.substring(index + part.length());
 
         // Recursive return
