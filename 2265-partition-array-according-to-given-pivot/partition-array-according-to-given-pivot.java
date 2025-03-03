2501e69bc8d6d35b94580dc4e5d2e3ce607a6c2f
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2025-03-02 18:22:56 -0600

    Time: 5 ms (85.39%) | Memory: 68.1 MB (44.51%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,6 +1,46 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int[] result = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < pivot) {
+                result[left++] = nums[i];
+            }
+            if (nums[nums.length - 1 - i] > pivot) {
+                result[right--] = nums[nums.length - 1 - i];
+            }
+        }
+        while (left <= right) {
+            result[left++] = pivot;
+            result[right--] = pivot;
+        }
+        return result;
+    }
+
+    public int[] pivotArray_BRUTE_FORCE(int[] nums, int pivot) {
+        int[] result = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < pivot) {
+                result[index++] = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == pivot) {
+                result[index++] = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > pivot) {
+                result[index++] = nums[i];
+            }
+        }
+        return result;
+    }
+
+    public int[] pivotArray_BRUTE_FORCE_2(int[] nums, int pivot) {
+        int[] result = new int[nums.length];
         List<Integer> small = new ArrayList<>();
         List<Integer> large = new ArrayList<>();
         List<Integer> equal = new ArrayList<>();
