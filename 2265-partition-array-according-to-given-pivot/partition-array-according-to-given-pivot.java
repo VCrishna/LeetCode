764e2e3011171e690b624122c33ce80dd003471f
Krishna Varakala <67335876+VCrishna@users.noreply.github.com>
2025-03-02 18:18:12 -0600

    Time: 15 ms (18.06%) | Memory: 65 MB (73.64%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,41 +1,28 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int[] result = new int[nums.length];
-        int left = 0;
-        int right = nums.length - 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < pivot) {
-                result[left++] = nums[i];
-            }
-            if(nums[nums.length - 1 - i] > pivot) {
-                result[right--] = nums[nums.length - 1 - i];
-            }
+        List<Integer> small = new ArrayList<>();
+        List<Integer> large = new ArrayList<>();
+        List<Integer> equal = new ArrayList<>();
+        for (int i : nums) {
+            if (i < pivot)
+                small.add(i);
+            else if (i > pivot)
+                large.add(i);
+            else
+                equal.add(i);
         }
-        while(left <= right) {
-            result[left++] = pivot;
-            result[right--] = pivot;
-        }
-        return result;
-    }
-
-    public int[] pivotArray_BRUTE_FORCE(int[] nums, int pivot) {
-        int[] result = new int[nums.length];
         int index = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < pivot) {
-                result[index++] = nums[i];
-            }
+        for (int i : small) {
+            result[index++] = i;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == pivot) {
-                result[index++] = nums[i];
-            }
+        for (int i : equal) {
+            result[index++] = i;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > pivot) {
-                result[index++] = nums[i];
-            }
+        for (int i : large) {
+            result[index++] = i;
         }
+
         return result;
     }
 }
