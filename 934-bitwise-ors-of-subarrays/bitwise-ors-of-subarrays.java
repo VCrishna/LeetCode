9e296d412a3c8714f887c34358a86b0e481be8b7
Krishna V <67335876+VCrishna@users.noreply.github.com>
2025-07-30 20:32:56 -0500

    Time: 125 ms (93.85%) | Memory: 70.6 MB (93.85%) - LeetSync

---
+++ 934-bitwise-ors-of-subarrays/bitwise-ors-of-subarrays.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int subarrayBitwiseORs(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+            for (int j = i - 1; j >= 0; j--) {
+                if ((arr[i] | arr[j]) == arr[j]) {
+                    break;
+                }
+                arr[j] |= arr[i];
+                set.add(arr[j]);
+            }
+        }
+        return set.size();
+    }
+}
+
+// TC: O(n ^ 2)
+// SC: O(w) - where w is the number of unique OR values
