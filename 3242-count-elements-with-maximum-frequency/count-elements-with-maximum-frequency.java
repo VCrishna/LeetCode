d05b2c3265f8cec5fec31bd04b1a892372e2476e
Krishna V <67335876+VCrishna@users.noreply.github.com>
2025-09-21 19:50:06 -0500

    Time: 3 ms (26.97%) | Memory: 42.6 MB (32.20%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.java
@@ -1,32 +1,16 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        // Creating a HashMap to store the frequency of each element
+        int result = 0, max = -1;
         Map<Integer, Integer> map = new HashMap<>();
-
-        // Initializing the maximum frequency to -1
-        int maxFreq = -1;
-
-        // Iterating through each element in the array
         for (int i : nums) {
-            // Updating the frequency of the current element in the map
             map.put(i, map.getOrDefault(i, 0) + 1);
-
-            // Updating the maximum frequency if the frequency 
-            // of the current element is greater
-            maxFreq = Math.max(maxFreq, map.get(i));
+            max = Math.max(max, map.get(i));
         }
-
-        // Initializing the result variable to 0
-        int result = 0;
-
-        // Iterating through the values (frequencies) in the map
-        for (int i : map.values()) {
-            // If the frequency equals the maximum frequency, 
-            // add it to the result
-            result += i == maxFreq ? i : 0;
+        for (int val : map.values()) {
+            if (max == val) {
+                result += val;
+            }
         }
-
-        // Returning the final result
         return result;
     }
 }
