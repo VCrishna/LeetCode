ede0e6ca64b9694ca2be508d2e7cb98f4dd860fa
Krishna V <67335876+VCrishna@users.noreply.github.com>
2025-11-02 22:54:09 -0600

    Time: 9 ms (59.83%) | Memory: 90.5 MB (5.68%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -2,13 +2,22 @@ class Solution {
     public int minCost(String colors, int[] neededTime) {
         int minCost = 0;
 
+        // Start iterating from the second balloon
         for (int i = 1; i < colors.length(); i++) {
+
+            // Check if the current balloon has the same color as the previous one
             if (colors.charAt(i) == colors.charAt(i - 1)) {
+
+                // Remove the balloon with smaller neededTime (to avoid same-color consecutive balloons)
+                // Add that cost to minCost
                 minCost += Math.min(neededTime[i], neededTime[i - 1]);
+
+                // Keep the balloon with the larger neededTime for future comparisons
                 neededTime[i] = Math.max(neededTime[i], neededTime[i - 1]);
             }
         }
 
+        // Return total minimum cost to remove balloons
         return minCost;
     }
 }
